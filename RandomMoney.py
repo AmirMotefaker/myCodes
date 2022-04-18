59e746cec8a1aac2e4a93eade70eeea15f4cbab3
Amir Motefaker <46513710+AmirMotefaker@users.noreply.github.com>
2022-04-18 16:19:58 +0430

    Create RandomMoney.py

---
+++ RandomMoney.py
@@ -0,0 +1,25 @@
+import random
+random.seed()
+#for i in range (1, 10):
+#    print(random.randrange(0, 2))
+    
+#return 0
+
+people = []
+for i in range (0, 50):
+    people.append(100)
+    
+for beshkan in range (0, 100):
+    for person1 in range (0, 50):
+        
+        person2 = random.randrange(0, 50)
+        while people[person2] == 0:
+            person2 = random.randrange(0, 50)
+        
+        if people[person1] != 0:
+            #print(person1 ,person2)
+            people[person1] = people[person1] - 1
+            people[person2] = people[person2] + 1
+      
+    
+people
