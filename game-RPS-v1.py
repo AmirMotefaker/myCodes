27245f42544855bd934e8661fef205c19d0cf08a
Amir Motefaker <46513710+AmirMotefaker@users.noreply.github.com>
2022-04-19 13:03:46 +0430

    Create game-RPS-v1.py

---
+++ game-RPS-v1.py
@@ -0,0 +1,27 @@
+# Bazi Sang-Kaghaz-Gheychi
+# Game Rock(R)-Paper(P)-Scissors(S) ---> RPS
+# version 1
+
+print("Sang...")
+print("Kaghaz...")
+print("Gheychi...")
+
+player1 = input("Player1, make your move: ")
+player2 = input("Player2, make your move: ")
+
+if player1 == "sang" and player2 == "gheychi":
+    print("Player 1 wins.")
+elif player1 == "sang" and player2 == "kaghaz":
+    print("Player 2 wins.")
+elif player1 == "kaghaz" and player2 == "sang":
+    print("Player 1 wins.")
+elif player1 == "kaghaz" and player2 == "gheychi":
+    print("Player 2 wins.")
+elif player1 == "gheychi" and player2 == "sang":
+    print("Player 2 wins.")
+elif player1 == "gheychi" and player2 == "kaghaz":
+    print("Player 1 wins.")
+elif player1 == player2:
+    print("har do meghdar barabar ast")
+else:
+    print("khataei rokh dadeh ast")
