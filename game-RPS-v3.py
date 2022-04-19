7df95028c865f885e1155136239919220be75f7f
Amir Motefaker <46513710+AmirMotefaker@users.noreply.github.com>
2022-04-19 13:05:07 +0430

    Create game-RPS-v3.py

---
+++ game-RPS-v3.py
@@ -0,0 +1,41 @@
+# Bazi Sang-Kaghaz-Gheychi
+# Game Rock(R)-Paper(P)-Scissors(S) ---> RPS
+# version 3
+
+from random import randint
+
+# print("Sang...")
+# print("Kaghaz...")
+# print("Gheychi...")
+
+#SAng -----> sang
+player = input("Player, make your move: ").lower()
+rand_num = randint(0, 2) # 0 1 2
+if rand_num == 0:
+    computer = "sang"
+elif rand_num == 1:
+    computer = "kaghaz"
+else:
+    computer = "gheychi"
+
+print(f'computer plays "{computer}"')
+
+if player == computer:
+    print("Maghadir mosavi hastand.")
+elif player == "sang":
+    if computer == "gheychi":
+        print("Player 1 wins!")
+    elif computer == "kaghaz":
+        print("Player 2 wins!")
+elif player == "kaghaz":
+    if computer == "gheychi":
+        print("Player 2 wins!")
+    elif computer == "sang":
+        print("Player 1 wins!")
+elif player == "gheychi":
+    if computer == "sang":
+        print("Player 2 wins!")
+    elif computer == "kaghaz":
+        print("Player 1 wins!")
+else:
+    print("Khatei rokh dadeh ast...")
