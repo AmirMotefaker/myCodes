4ec68d160a82f4bef0366db083e64a5c605ba2bb
Amir Motefaker <46513710+AmirMotefaker@users.noreply.github.com>
2022-04-20 17:03:58 +0430

    Update Bitcoin-Price.py

--- Bitcoin-Price.py
+++ Bitcoin-Price.py
@@ -12,6 +12,14 @@ print(data)
 
 # Me
 import requests
+def inform_amir():
+    print('hi there, price is good.')
+
+my_good_price = 18000
 response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD',
                         proxies={'https': 'socks5://127.0.0.1:1080'})
-print ('at this moment, Bitcoin is %i dollars' % float(response.json()['data']['amount']))
+price = float(response.json()['data']['amount'])
+print ('at this moment, Bitcoin is %i dollars' % price)
+
+if (price < my_good_price):
+    inform_amir()
