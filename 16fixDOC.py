8a991f82b2ab79e515d25d7901ff66d63f2767d0
Amir Motefaker <46513710+AmirMotefaker@users.noreply.github.com>
2022-04-24 15:42:03 +0430

    Update 16fixDOC.py

--- 16fixDOC.py
+++ 16fixDOC.py
@@ -1,3 +1,5 @@
+# Code by amotef@gmail.com
+
 #dar in barnameh ba tavajoh be inke dar yek file doc.docx< koliehe adad az rast be chap neveshteh
 #shode boudand. masalan: adad 1370 be sorat 0731 neveshteh shodeh va bayad eslah shavad
 #va tedad adaadi ke eslah shode ro dar payan elam konad
