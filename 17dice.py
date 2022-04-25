53acd72e6802f1bb66a1f3ae0eac7b4d00cbca81
Amir Motefaker <46513710+AmirMotefaker@users.noreply.github.com>
2022-04-25 16:29:50 +0430

    Update 17dice.py

--- 17dice.py
+++ 17dice.py
@@ -7,8 +7,7 @@ img = Image.open('image.png') # loads an image, and displays it using an externa
 img = ImageOps.grayscale(img) # Convert the image to grayscale.
 img = ImageOps.equalize(img)  # Equalize the image histogram. 
 
-#dicew = 100
-dicew = 20
+dicew = 100
 
 dicesize = int(img.width * 1.0 / dicew)
 diceh = int(img.height * 1.0 / img.width * dicew)
@@ -17,6 +16,8 @@ nim = Image.new("L", (img.width, img.height), 'white') #nim=new image
 nimd = ImageDraw.Draw(nim) #nimd=new image draw - ImageDraw.Draw: You can use this module to create new images
 #img.show()
 #print (img.width, img.height, dicew, diceh, dicesize)
+
+print (' you will need %s dices for this!\n' % (dicew * diceh))
 
 for y in range(0, img.height-dicesize, dicesize):
     for x in range(0, img.width-dicesize, dicesize):
@@ -28,12 +29,12 @@ for y in range(0, img.height-dicesize, dicesize):
         thisSectorColor /= (dicesize ** 2)
         nimd.rectangle([(x, y), (x+dicesize,  y+dicesize)], int(thisSectorColor))
 
-        diceNumber = (255 - thisSectorColor) * 6 / 255 + 1 # this is normal method convert scale: multiply to max (6 in dice) divid in max start(255) plus 1(for 1,2,3,4,5,6 in Dice)
-        print (int(diceNumber)),
+        diceNumber = (255-thisSectorColor) * 6 / 255 + 1 # this is normal method convert scale: multiply to max (6 in dice) divid in max start(255) plus 1(for 1,2,3,4,5,6 in Dice)
+        print (int(diceNumber)) ,
     print
 
 
-nim.show()
+#nim.show()
         
         #print (x, y, thisSectorColor)
         
