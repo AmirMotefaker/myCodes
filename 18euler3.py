4880208aa8b2fd51e9bf9dd88066a4cf9d9a197d
Amir Motefaker <amotef@gmail.com>
2022-04-27 15:21:55 +0430

    Update 18euler3.py

--- 18euler3.py
+++ 18euler3.py
@@ -7,20 +7,24 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-def findLargestPrimeFactor(n):
-  primeFactor = 1
-  i = 2
+# Prime Factor: any of the prime numbers that can be multiplied to give the original number.
+# Example: The prime factors of 15 are 3 and 5 (because 3×5=15, and 3 and 5 are prime numbers).
 
-  while i <= n / i:
-    if n % i == 0:
-      primeFactor = i
-      n /= i
-    else:
-      i += 1
 
-  if primeFactor < n: primeFactor = int(n)
+def FLPF(n): # FLPF: Find Largest Prime Factor
+   primeFactor = 1 # A factor that is a prime number
+   i = 2
 
-  return primeFactor
+   while i <= n / i:
+     if n % i == 0:
+       primeFactor = i
+       n /= i
+     else:
+       i += 1
 
-print("Largest prime factor 13195 is: ", findLargestPrimeFactor(13195))
-print("Largest prime factor 600851475143 is: ", findLargestPrimeFactor(600851475143))
+   if primeFactor < n: primeFactor = int(n)
+
+   return primeFactor
+
+print("Largest prime factor 13195 is: ", FLPF(13195))
+print("Largest prime factor 600851475143 is: ", FLPF(600851475143))
