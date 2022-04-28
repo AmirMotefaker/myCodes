bf6719420361586df06fdb7f76d5d61566a67f30
Amir Motefaker <amotef@gmail.com>
2022-04-28 12:22:32 +0430

    Update 21euler6.py

--- 21euler6.py
+++ 21euler6.py
@@ -17,4 +17,4 @@
 def sum_square_difference(n):
       return (((n**2) * (n + 1)**2) / 4) - (n * (n + 1) * (2*n + 1) / 6)
 n = int(input("Enter Number: "))
-print(sum_square_difference(n))
+print(int(sum_square_difference(n)))
