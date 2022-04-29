4bb896da9746a82c445a5ec19ae20636e6032d07
Amir Motefaker <amotef@gmail.com>
2022-04-29 17:25:36 +0430

    Create 22euler7.py

---
+++ 22euler7.py
@@ -0,0 +1,15 @@
+# Code by amotef@gmail.com
+
+# projecteuler.net
+# Problem 7
+
+# 10001st prime
+
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + 3 + .... + 10)**2 = 55*55 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is:
+# 3025 - 385 = 2640
+
