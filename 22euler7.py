1c47e1a3a578e97e80db0174e764baea94c3196f
Amir Motefaker <amotef@gmail.com>
2022-04-29 18:56:48 +0430

    Update 22euler7.py

--- 22euler7.py
+++ 22euler7.py
@@ -8,8 +8,8 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10001st prime number?
 
-# A prime sieve or prime number sieve is a fast type of algorithm for finding primes.
 
+# A prime sieve or prime number sieve is a fast type of algorithm for finding primes.
 
 # Algorithm: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 # The sieve of Eratosthenes can be expressed in pseudocode:
@@ -49,11 +49,11 @@ def Sieve_of_Eratosthenes(n):  # Sieve of Eratosthenes: method for finding all p
 			prime.append(i)
 	return prime
 
-nth_prime = 10001   # the 10001st prime number
+nth_prime = 100001   # the 10001st prime number
 
-Maximum_bound = nth_prime*math.log(nth_prime) + nth_prime*math.log(math.log(nth_prime))   # nlogn+n(loglogn−1)<prime_number<nlogn+nlognlogn
+Max_bound = nth_prime*math.log(nth_prime) + nth_prime*math.log(math.log(nth_prime))   # nlogn+n(loglogn−1)<prime_number<nlogn+nlognlogn
 
-print ("the 10001st prime number is:", Sieve_of_Eratosthenes(int(Maximum_bound))[10000])
+print ("the 10001st prime number is:", Sieve_of_Eratosthenes(int(Max_bound))[10000])
 
 end_time = time.time()   #Time at the end of execution
 
