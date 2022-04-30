34804073cbb439aafb4f49b8b19f69e4368e16b2
Amir Motefaker <amotef@gmail.com>
2022-04-30 09:02:19 +0430

    Update 20euler5.py

--- 20euler5.py
+++ 20euler5.py
@@ -14,11 +14,12 @@
 
 
 import time
+start_time = time.time()   #Time at the start of program execution
 
 from math import gcd  # gcd: Return the greatest common divisor of the specified integer arguments.
 from functools import reduce  # reduce: Apply function of two arguments cumulatively to the items of iterable, from left to right, so as to reduce the iterable to a single value.
 
-start_time = time.time()   #Time at the start of program execution
+
 
 def LCM(a, b):  # LCM: Return the least common multiple of the specified integer arguments.
     return a // gcd(a, b) * b  
@@ -27,5 +28,4 @@ N_reduce = reduce(LCM, range(N//2+1, N+1))
 print ("The LCM for numbers 1 through" ,N, "is", N_reduce)
 
 end_time = time.time()   #Time at the end of execution
-
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
