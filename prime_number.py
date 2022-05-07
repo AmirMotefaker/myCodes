a9bfb09b591b88199adacaf616ddfd0928477a65
Amir Motefaker <amotef@gmail.com>
2022-05-07 16:33:08 +0430

    Final prime_number.py
    The fastest finding Prime  Numbers under 10000.

--- prime_number.py
+++ prime_number.py
@@ -14,11 +14,16 @@ def is_prime(n):
     return avval
 
 prime_count=0
+last_prime_number=0
+
 for i in range(1,10001):
     if is_prime(i):
+        last_prime_number = i
         prime_count = prime_count + 1
-        print(i)
+
+print("")
 print ("we had", prime_count, "prime numbers")
+print("last prime number was", last_prime_number)
 
 end_time = time.time()   #Time at the end of execution
 print ("Time of program execution:", (end_time - start_time))   # Time of program execution
