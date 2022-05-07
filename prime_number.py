4fdfcbe4efdaf92d2ea1d9ce5f45c40a8f987332
Amir Motefaker <amotef@gmail.com>
2022-05-07 16:28:11 +0430

    Faster prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -2,9 +2,12 @@
 
 # Prime Number
 
+import time
+start_time = time.time()   #Time at the start of program execution
+
 def is_prime(n):
     avval = True
-    for i in range(2, n):
+    for i in range(2, n // 2+1):
         if n % i == 0 :
             avval = False
     return avval
@@ -15,3 +18,6 @@ for i in range(1,10001):
         prime_count = prime_count + 1
         print(i)
 print ("we had", prime_count, "prime numbers")
+
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution
