605a7d936477659650a43932f704c9a9b8c2be57
Amir Motefaker <amotef@gmail.com>
2022-05-07 16:26:13 +0430

    Create prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,17 @@
+# Code by amotef@gmail.com
+
+# Prime Number
+
+def is_prime(n):
+    avval = True
+    for i in range(2, n):
+        if n % i == 0 :
+            avval = False
+    return avval
+
+prime_count=0
+for i in range(1,10001):
+    if is_prime(i):
+        prime_count = prime_count + 1
+        print(i)
+print ("we had", prime_count, "prime numbers")
