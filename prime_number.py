b4dda43090055b09e846d1d7ae8d29a59d8687d6
Amir Motefaker <amotef@gmail.com>
2022-05-07 16:29:07 +0430

    Fastest prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -7,7 +7,7 @@ start_time = time.time()   #Time at the start of program execution
 
 def is_prime(n):
     avval = True
-    for i in range(2, n // 2+1):
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0 :
             avval = False
     return avval
