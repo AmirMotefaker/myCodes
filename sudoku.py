9861c874284b7ab3d14820375e15fb9eafad101f
Amir Motefaker <amotef@gmail.com>
2022-05-08 10:49:14 +0430

    Update and rename 30Sudoku-Backtracking.py to sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,7 @@
 # Code by amotef@gmail.com
 
 # Sudoku solver with Backtracking
+
 # Backtracking is an algorithmic-technique for solving problems recursively by trying to build a solution incrementally,
 # one piece at a time, removing those solutions that fail to satisfy the constraints of the problem at any point of time.
 
