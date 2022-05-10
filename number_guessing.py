564685dc962016198ef8cae46786106351b4c09f
Amir Motefaker <amotef@gmail.com>
2022-05-10 11:10:33 +0430

    Create number_guessing.py

---
+++ number_guessing.py
@@ -0,0 +1,44 @@
+# Code by amotef@gmail.com
+
+# Number Guessing
+
+import random
+
+num = random.randint(0, 1000)
+print(f'''
+---------------------------
+Correct answer: {num}
+*************
+***********
+*********
+*******
+*****
+*******
+*********
+***********
+*************
+---------------------------
+''')
+
+attempt = 10
+msg = ''
+while attempt > 0:
+   
+    user_input = int(input('Enter Number: '))
+    if user_input == num:
+        msg = '**** You Won! ****'
+        break
+    elif user_input > num:
+        print(f'{user_input} is greater.\nRemaining attempts: {attempt}.')
+        attempt -= 1
+        
+
+    elif user_input < num:
+        print(f'{user_input} is smaller.\nRemaining attempts: {attempt}.')
+        attempt -= 1
+
+    else:
+        print('Something went wrong!')
+        break
+    
+print(msg)
