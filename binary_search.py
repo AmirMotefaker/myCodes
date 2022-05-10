3958e6ceb7caffc1715b96300517b42a4237e091
Amir Motefaker <amotef@gmail.com>
2022-05-10 15:26:47 +0430

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -6,6 +6,8 @@
 # finds the position of a target value within a sorted array. The binary search algorithm 
 # can be classified as a dichotomies divide-and-conquer search algorithm and executes in logarithmic time.
 
+# https://tinyurl.com/ycktx5aw
+
 def binary_search(item_list,item):
 	first = 0
 	last = len(item_list)-1
