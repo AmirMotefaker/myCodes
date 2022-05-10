876dd8b093d932ba1dd4e331132a217e55be1f63
Amir Motefaker <amotef@gmail.com>
2022-05-10 09:34:34 +0430

    Update determinant.py
    Add = Instead : and added Time of program execution

--- determinant.py
+++ determinant.py
@@ -8,9 +8,15 @@
 
 # => det(A) = aei + bfg + cdh - ceg - bdi - afh
 
+import time
+start_time = time.time()   #Time at the start of program execution
+
 import numpy as np
 A = np.array([[6, 1, 1],
               [4, -2, 5],
               [2, 8, 7]])
 
-print("Det of A: ", np.linalg.det(A))
+print("Det of A =", np.linalg.det(A))
+
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution
