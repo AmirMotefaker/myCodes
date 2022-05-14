fe1a3738bc6b586ac1ca2e7561b6551f4192a110
Amir Motefaker <amotef@gmail.com>
2022-05-14 15:25:25 +0430

    Update qr_code.py
    Added Styled QR Code.

--- qr_code.py
+++ qr_code.py
@@ -4,7 +4,6 @@
 
 # https://pypi.org/project/qrcode/
 
-
 # Generator QRcode(Basic)
 
 # import qrcode
@@ -17,20 +16,20 @@
 
 # Generator QRcode(Advanced)
 
-import qrcode
+# import qrcode
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('Some data')
-qr.make(fit=True)
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=10,
+#     border=4,
+# )
+# qr.add_data('Some data')
+# qr.make(fit=True)
 
-# img = qr.make_image(fill_color="black", back_color="white")
-img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
-img.save("some_file.png")
+# # img = qr.make_image(fill_color="black", back_color="white")
+# img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+# img.save("some_file.png")
 
 
 # Example
@@ -43,3 +42,68 @@ img.save("some_file.png")
 # qr.print_ascii(out=f)
 # f.seek(0)
 # print(f.read())
+
+
+# QRCode for URL link
+
+# import qrcode
+
+# URL of the homepage
+# We'll encode this URL to a QR code
+# URL = "https://github.com/AmirMotefaker"
+
+# qr = qrcode.QRCode(version=4, box_size=10, border=4, \
+# error_correction=qrcode.constants.ERROR_CORRECT_L)
+
+# qr.add_data(URL)
+# qr.make(fit=True)
+
+# img = qr.make_image(fill_color='green', back_color='white')
+# img.save('github_AmirMotefaker.png')
+
+
+## Styled QR Code - QR Code with rounded corners
+
+# import qrcode
+# from qrcode.image.styledpil import StyledPilImage
+# from  qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+
+# qr = qrcode.QRCode(version=4, box_size=10, border=4, \
+# error_correction=qrcode.constants.ERROR_CORRECT_H)
+# qr.add_data('https://github.com/AmirMotefaker')
+
+# img  = qr.make_image(image_factory=StyledPilImage, 
+# module_drawer = RoundedModuleDrawer())
+# img.save('github_AmirMotefaker.png')
+
+
+## Styled QR Code - QR Code with radial gradiant style
+
+# import qrcode
+# from qrcode.image.styledpil import StyledPilImage
+# from qrcode.image.styles.colormasks import RadialGradiantColorMask
+
+
+# qr = qrcode.QRCode(version=4, box_size=10, border=4, \
+# error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+# qr.add_data('https://github.com/AmirMotefaker')
+# img = qr.make_image(image_factory=StyledPilImage, 
+#                     color_mask=RadialGradiantColorMask())
+# img.save('github_AmirMotefaker.png')
+
+
+## Styled QR Code - Add or Embed an image to a QR Code
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+
+qr = qrcode.QRCode(version=4, box_size=10, border=4, \
+error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+qr.add_data('https://github.com/AmirMotefaker')
+img = qr.make_image(image_factory=StyledPilImage, \
+embeded_image_path="image.png")
+
+img.save('github_AmirMotefaker.png')
+
+
