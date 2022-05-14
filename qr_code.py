69749f2eded79260bb72be187dc824709917a24c
Amir Motefaker <amotef@gmail.com>
2022-05-14 14:50:51 +0430

    Update qr_code.py
    Added QRcode  Generator advanced.

--- qr_code.py
+++ qr_code.py
@@ -4,8 +4,25 @@
 
 # Generator QRcode(Basic)
 
-import qrcode
+# import qrcode
 
-img = qrcode.make('Some data here')
-type(img)  # qrcode.image.pil.PilImage
+# img = qrcode.make('Some data here')
+# type(img)  # qrcode.image.pil.PilImage
+# img.save("some_file.png")
+
+
+# Generator QRcode(Advanced)
+
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+# img = qr.make_image(fill_color="black", back_color="white")
+img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
 img.save("some_file.png")
