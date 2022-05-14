13172b837038051675561fcf5900c2c5d5872521
Amir Motefaker <amotef@gmail.com>
2022-05-14 15:09:27 +0430

    Update qr_code.py
    Added example

--- qr_code.py
+++ qr_code.py
@@ -2,6 +2,8 @@
 
 # QR Code generator
 
+# https://pypi.org/project/qrcode/
+
 # Generator QRcode(Basic)
 
 # import qrcode
@@ -11,18 +13,32 @@
 # img.save("some_file.png")
 
 
+
 # Generator QRcode(Advanced)
 
-import qrcode
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('Some data')
-qr.make(fit=True)
+#import qrcode
 
-# img = qr.make_image(fill_color="black", back_color="white")
-img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
-img.save("some_file.png")
+# qr = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=10,
+#     border=4,
+# )
+# qr.add_data('Some data')
+# qr.make(fit=True)
+
+# # img = qr.make_image(fill_color="black", back_color="white")
+# img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+# img.save("some_file.png")
+
+
+# Example
+
+import io
+import qrcode
+qr = qrcode.QRCode()
+qr.add_data("Some text")
+f = io.StringIO()
+qr.print_ascii(out=f)
+f.seek(0)
+print(f.read())
