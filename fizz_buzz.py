c5cafb8e316d4d272317ee5b325d570a0ee98baa
Amir Motefaker <amotef@gmail.com>
2022-05-16 11:31:40 +0430

    Create fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,21 @@
+# Code by amotef@gmail.com
+
+# FizzBuzz
+
+# Iterates the integers from 1 to n. 
+# For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+# For numbers which are multiples of both three and five print "FizzBuzz".
+
+n = int(input("Enter n number:"))
+
+for fizzbuzz in range(n):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
