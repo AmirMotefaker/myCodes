60101c75e2f53b459238e263684ecb2c92d28e11
Amir Motefaker <amotef@gmail.com>
2022-05-17 09:45:29 +0430

    Update make_pattern.py
    Added pyramid pattern with 180 degree rotation

--- make_pattern.py
+++ make_pattern.py
@@ -22,14 +22,15 @@
 
 ### Simple pyramid pattern with using list
 
-def pyramid_pattern(n):
-    myList = []
-    for i in range(1,n+1):
-        myList.append("*"*i)
-    print("\n".join(myList))
+# def pyramid_pattern(n):
+#     myList = []
+#     for i in range(1,n+1):
+#         myList.append("*"*i)
+#     print("\n".join(myList))
  
-n = int(input("Enter the number of rows : ")) 
-pyramid_pattern(n)
+# n = int(input("Enter the number of rows : ")) 
+# pyramid_pattern(n)
+
 
 ## Output (row = 5)
 # *
@@ -37,4 +38,32 @@ pyramid_pattern(n)
 # ***
 # ****
 # *****
+
+
+
+### Simple pyramid pattern with 180 degree rotation
+
+def pyr_pat_deg(n):
+     
+    k = 2*n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+     
+        k = k - 2           # decrementing k after each loop
+     
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+ 
+n = int(input("Enter the number of rows : "))
+pyr_pat_deg(n)
+
+
+## Output (row = 5)
+#         * 
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
 
