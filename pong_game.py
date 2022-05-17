b25becea542d070f2d9f6247c4e0ec9f0930ba0e
Amir Motefaker <amotef@gmail.com>
2022-05-17 14:37:06 +0430

    Update pong_game.py
    Added Left and Right paddles

--- pong_game.py
+++ pong_game.py
@@ -15,4 +15,24 @@ sc = turtle.Screen()
 sc.title("Pong game")
 sc.bgcolor("white")
 sc.setup(width=1000, height=600)
-
+ 
+ 
+# Left paddle
+left_pad = turtle.Turtle()
+left_pad.speed(0)
+left_pad.shape("square")
+left_pad.color("black")
+left_pad.shapesize(stretch_wid=6, stretch_len=2)
+left_pad.penup()
+left_pad.goto(-400, 0)
+ 
+ 
+# Right paddle
+right_pad = turtle.Turtle()
+right_pad.speed(0)
+right_pad.shape("square")
+right_pad.color("black")
+right_pad.shapesize(stretch_wid=6, stretch_len=2)
+right_pad.penup()
+right_pad.goto(400, 0)
+ 
