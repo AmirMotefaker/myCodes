78065f47e9073cf82534ce9edf20a6ebe2589204
Amir Motefaker <amotef@gmail.com>
2022-05-17 10:49:17 +0430

    Update make_pattern.py
    Added Left Start Pattern

--- make_pattern.py
+++ make_pattern.py
@@ -125,18 +125,18 @@
 
 ### Right Start Pattern
 
-def pattern_right(n):
-      for i in range(0, n):
-            for j in range(0, i + 1):
-                print("* ", end="")
-            print()
-      for i in range(n, 0 , -1):
-            for j in range(0, i + 1):
-               print("* ", end="")
-            print()
+# def pattern_right(n):
+#       for i in range(0, n):
+#             for j in range(0, i + 1):
+#                 print("* ", end="")
+#             print()
+#       for i in range(n, 0 , -1):
+#             for j in range(0, i + 1):
+#                print("* ", end="")
+#             print()
  
-n = int(input("Enter the number of rows : "))
-pattern_right(n)
+# n = int(input("Enter the number of rows : "))
+# pattern_right(n)
 
 
 ## Output (row = 5)
@@ -150,6 +150,43 @@ pattern_right(n)
 # * * * *
 # * * *
 # * *
+
+
+
+### Left Start Pattern
+
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(0, n-1):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 2
+        for j in range(0, i + 1):
+            print("* ", end="")
+        print()
+    k = -1
+    for i in range(n-1,-1,-1):
+        for j in range(k,-1,-1):
+            print(end=" ")
+        k = k + 2
+        for j in range(0, i + 1):
+            print("* ", end="")
+        print()
+ 
+n = int(input("Enter the number of rows : ")) 
+pattern(n)
+
+
+## Output (row = 5)
+#         * 
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
 
 
 
