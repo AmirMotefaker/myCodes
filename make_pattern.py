b92ed7c1c430fc8ff6f2c37de2cb811bf5850aab
Amir Motefaker <amotef@gmail.com>
2022-05-17 09:34:28 +0430

    Update make_pattern.py
    Added Simple pyramid pattern with using list.

--- make_pattern.py
+++ make_pattern.py
@@ -9,6 +9,46 @@ for i in range(0, n):
     for j in range(0, i + 1):  
         print("* ", end="")       
     print()  
+# Code by amotef@gmail.com
+
+# Make Pattern
+
+### Simple pyramid pattern
+
+# n = int(input("Enter the number of rows : "))  
+# for i in range(0, n):  
+#     for j in range(0, i + 1):  
+#         print("* ", end="")       
+#     print()  
+
+
+## Output (row = 5)
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+
+
+
+### Simple pyramid pattern with using list
+
+def pyramid_pattern(n):
+    myList = []
+    for i in range(1,n+1):
+        myList.append("*"*i)
+    print("\n".join(myList))
+ 
+n = int(input("Enter the number of rows : ")) 
+pyramid_pattern(n)
+
+## Output (row = 5)
+# *
+# **
+# ***
+# ****
+# *****
+
 
 
 ## Output ( for row = 5)
