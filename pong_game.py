9f4c61d1ea1e0f754a5a873699d9a62c20262d32
Amir Motefaker <amotef@gmail.com>
2022-05-17 14:37:39 +0430

    Update pong_game.py
    Added Ball of circle shape

--- pong_game.py
+++ pong_game.py
@@ -36,3 +36,14 @@ right_pad.shapesize(stretch_wid=6, stretch_len=2)
 right_pad.penup()
 right_pad.goto(400, 0)
  
+ 
+# Ball of circle shape
+hit_ball = turtle.Turtle()
+hit_ball.speed(40)
+hit_ball.shape("circle")
+hit_ball.color("blue")
+hit_ball.penup()
+hit_ball.goto(0, 0)
+hit_ball.dx = 5
+hit_ball.dy = -5
+
