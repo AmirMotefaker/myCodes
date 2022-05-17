22c01fabb1b588b7fb65727cabbcd4da72d6cc08
Amir Motefaker <amotef@gmail.com>
2022-05-17 11:09:16 +0430

    Update make_pattern.py
    Added Diamond Shaped Pattern

--- make_pattern.py
+++ make_pattern.py
@@ -192,28 +192,28 @@
 
 ### Hourglass Pattern 
 
-def pattern_hourglass(n):
+# def pattern_hourglass(n):
     
-    k = n - 2
-    for i in range(n, -1 , -1):
-        for j in range(k , 0 , -1):
-            print(end=" ")
-        k = k + 1    
-        for j in range(0, i+1):
-            print("* " , end="")
-        print()
+#     k = n - 2
+#     for i in range(n, -1 , -1):
+#         for j in range(k , 0 , -1):
+#             print(end=" ")
+#         k = k + 1    
+#         for j in range(0, i+1):
+#             print("* " , end="")
+#         print()
     
-    k = 2 * n  - 2
-    for i in range(0 , n+1):
-        for j in range(0 , k):
-            print(end=" ")
-        k = k - 1
-        for j in range(0, i + 1):
-            print("* ", end="")
-        print()
+#     k = 2 * n  - 2
+#     for i in range(0 , n+1):
+#         for j in range(0 , k):
+#             print(end=" ")
+#         k = k - 1
+#         for j in range(0, i + 1):
+#             print("* ", end="")
+#         print()
  
-n = int(input("Enter the number of rows : "))
-pattern_hourglass(n)
+# n = int(input("Enter the number of rows : "))
+# pattern_hourglass(n)
 
 
 ## Output (row = 5)
@@ -229,6 +229,45 @@ pattern_hourglass(n)
 #      * * * *
 #     * * * * *
 #    * * * * * *
+
+
+
+### Diamond Shaped Pattern
+
+def pattern_diamond(n):
+     k = 2 * n - 2
+     for i in range(0, n):
+          for j in range(0 , k):
+               print(end=" ")
+          k = k - 1
+          for j in range(0 , i + 1 ):
+               print("* ", end="")
+          print()
+     k = n - 2
+     for i in range(n , -1, -1):
+        for j in range(k , 0 , -1): 
+            print(end=" ")
+        k = k + 1
+        for j in range(0 , i + 1):
+            print("* ", end="")
+        print()
+ 
+n = int(input("Enter the number of rows : "))
+pattern_diamond(n)
+
+
+## Output (row = 5)
+#         * 
+#        * *
+#       * * *
+#      * * * *
+#     * * * * *
+#    * * * * * *
+#     * * * * *
+#      * * * *
+#       * * * 
+#        * *
+#         *
 
 
 
