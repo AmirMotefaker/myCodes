684643d98817bd1eee31515adbb874dba835ac10
Amir Motefaker <amotef@gmail.com>
2022-05-18 15:47:56 +0430

    Update youtube_downloader.py
    Added youtube downloader with API

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -64,5 +64,33 @@
 
 # Solution 6 - fastest way to download a Youtube video with Python
 
+# from pytube import YouTube
+# YouTube('https://www.youtube.com/watch?v=wHBBoUtJHhA').streams.first().download()
+
+
+# Solution 7 - with API
+
+from tkinter import *
 from pytube import YouTube
-YouTube('https://www.youtube.com/watch?v=wHBBoUtJHhA').streams.first().download()
+
+# Create Display Window
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("YouYube Video Downloader - Amir Motefaker -")
+
+Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
+
+# Create Field to Enter Link
+link = StringVar()
+Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').place(x= 160 , y = 60)
+link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
+
+# Create Function to Start Downloading
+def Downloader():     
+    url =YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180 , y = 210)  
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+root.mainloop()
