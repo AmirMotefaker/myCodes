45768b1a0f871042a0327d796b8968cf7eb6c27d
Amir Motefaker <amotef@gmail.com>
2022-05-18 15:52:35 +0430

    Update youtube_downloader.py
    Added comment

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -92,5 +92,5 @@ def Downloader():
     video = url.streams.first()
     video.download()
     Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180 , y = 210)  
-Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloader).place(x=180 ,y = 150)
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = '#1e81b0', padx = 2, command = Downloader).place(x=180 ,y = 150)
 root.mainloop()
