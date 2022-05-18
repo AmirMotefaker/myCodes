d1ca44954dce8f7bab8e1fd190a4a3b0fc43290f
Amir Motefaker <amotef@gmail.com>
2022-05-18 16:52:29 +0430

    Update language_translator.py
    Added Specifying Source and Destination Languages code

--- language_translator.py
+++ language_translator.py
@@ -6,14 +6,26 @@
  
 # print(googletrans.LANGUAGES)
 
+# from googletrans import Translator
+
+# translator = Translator()
+
+# result = translator.translate('ich liebe dich')
+
+# print(result.src)
+# print(result.dest)
+# print(result.origin)
+# print(result.text)
+# print(result.pronunciation)
+
+
+# Specifying Source and Destination Languages
+
 from googletrans import Translator
 
 translator = Translator()
-
-result = translator.translate('ich liebe dich')
+result = translator.translate('Mikä on nimesi', src='fi', dest='fr')
 
 print(result.src)
 print(result.dest)
-print(result.origin)
 print(result.text)
-print(result.pronunciation)
