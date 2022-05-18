fcba3bc2ec9a00f3967cbafa0d8ea298d2bbaada
Amir Motefaker <amotef@gmail.com>
2022-05-18 13:37:32 +0430

    Update text_to_speech.py
    Added Spanish audio

--- text_to_speech.py
+++ text_to_speech.py
@@ -7,6 +7,12 @@ from playsound import playsound
 # gTTS (Google Text-to-Speech), a Python library and CLI tool to interface with Google Translate's text-to-speech API.
 # playsound: Pure Python, cross platform, single function module with no dependencies for playing sounds.
 
-tts = gTTS('hello world')
-tts.save('hello.mp3')
-playsound("hello.mp3")
+# in english
+# tts = gTTS('hello world')
+# tts.save('hello.mp3')
+# playsound("hello.mp3")
+
+# in spanish
+tts = gTTS('Hola Mundo', lang='es')
+tts.save('hola.mp3')
+playsound("hola.mp3")
