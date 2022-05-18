86bd5b30352249773de625fd0dbf73404741cbe7
Amir Motefaker <amotef@gmail.com>
2022-05-18 09:41:53 +0430

    Update mad_libs_game.py
    Rename filename and check it again

--- mad_libs_game.py
+++ mad_libs_game.py
@@ -21,7 +21,7 @@ verb1 = input("Enter a verb in the past tense: ")
 verb2 = input("Enter another verb in the past tense: ")
 verb3 = input("Enter a third verb in the past tense: ")
 noun1 = input("Enter a noun: ")
-story = "It was " + food + " day at school, and " + jamie + " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + jamie + " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and " + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped! Luckily, " + jamie + "'s friends were willing to share their " + food + " with her."
+story = "It was " + food + " day at school, and " + girl + " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + girl + " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and " + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped! Luckily, " + girl + "'s friends were willing to share their " + food + " with her."
  
 print(story)
 
