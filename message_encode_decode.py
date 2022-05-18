6736810247d25e9ced9c37c34a9d64dc69b64194
Amir Motefaker <amotef@gmail.com>
2022-05-18 10:58:43 +0430

    Update message_encode_decode.py
    Added solution 2 with base64 library

--- message_encode_decode.py
+++ message_encode_decode.py
@@ -1,8 +1,40 @@
 # Code by amotef@gmail.com
 
-# Message Encode
+# Message Encode - Decode
 
 # Solution 1 - basic
+# def encode(key, string):
+#     encoded_chars = []
+#     for i in range(len(string)):
+#         key_c = key[i % len(key)]
+#         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+#         encoded_chars.append(encoded_c)
+#     encoded_string = ''.join(encoded_chars)
+#     return encoded_string
+
+# def decode(key, string):
+#     encoded_chars = []
+#     for i in range(len(string)):
+#         key_c = key[i % len(key)]
+#         encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
+#         encoded_chars.append(encoded_c)
+#     encoded_string = ''.join(encoded_chars)
+#     return encoded_string
+
+
+# e = encode('Amir', 'Motefaker')
+# d = decode('Amir', e)
+# print([e])
+# print([d])
+
+
+
+# Solution 2 - advanced
+
+import six, base64
+# six: Provides simple utilities for wrapping over differences between Python 2 and Python 3.
+# base64: This module provides functions for encoding binary data to printable ASCII characters and decoding such encodings back to binary data.
+
 def encode(key, string):
     encoded_chars = []
     for i in range(len(string)):
@@ -10,9 +42,12 @@ def encode(key, string):
         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
         encoded_chars.append(encoded_c)
     encoded_string = ''.join(encoded_chars)
-    return encoded_string
+    encoded_string = encoded_string.encode('latin') if six.PY3 else encoded_string
+    return base64.urlsafe_b64encode(encoded_string).rstrip(b'=')
 
 def decode(key, string):
+    string = base64.urlsafe_b64decode(string + b'===')
+    string = string.decode('latin') if six.PY3 else string
     encoded_chars = []
     for i in range(len(string)):
         key_c = key[i % len(key)]
@@ -20,7 +55,6 @@ def decode(key, string):
         encoded_chars.append(encoded_c)
     encoded_string = ''.join(encoded_chars)
     return encoded_string
-
 
 e = encode('Amir', 'Motefaker')
 d = decode('Amir', e)
