2aa93698ab6ec83c3e78b16353eb439c197285f5
Amir Motefaker <amotef@gmail.com>
2022-05-18 08:54:24 +0430

    Create email_slicer.py

---
+++ email_slicer.py
@@ -0,0 +1,9 @@
+# Code by amotef@gmail.com
+
+# Email Slicer 
+
+email = input("Enter Your Email: ").strip()
+username = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
+print(format_)
