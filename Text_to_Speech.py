830ab49c1dbd640b07b14575fd6a7d03cf58c555
Amir Motefaker <amotef@gmail.com>
2022-05-18 13:07:19 +0430

    Create Text_to_Speech.py
    Convert Text to Speech

---
+++ Text_to_Speech.py
@@ -0,0 +1,12 @@
+# Code by amotef@gmail.com
+
+# Convert Text to Speech
+
+from gtts import gTTS
+from playsound import playsound
+# gTTS (Google Text-to-Speech), a Python library and CLI tool to interface with Google Translate's text-to-speech API.
+# playsound: Pure Python, cross platform, single function module with no dependencies for playing sounds.
+
+tts = gTTS('hello world')
+tts.save('hello.mp3')
+playsound("hello.mp3")
