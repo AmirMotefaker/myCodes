b35087918440af0b24ae6b0078087da4045a73bd
Amir Motefaker <amotef@gmail.com>
2022-05-18 14:24:29 +0430

    Update desktop_notifier.py
    Added Solution 2 with win10toast library.

--- desktop_notifier.py
+++ desktop_notifier.py
@@ -3,16 +3,33 @@
 # Desktop Notifier
 
 # Solution 1 - basic
-import time
-from plyer import notification
-# plyer: Platform-independent wrapper for platform-dependent APIs
+# import time
+# from plyer import notification
+# # plyer: Platform-independent wrapper for platform-dependent APIs
 
-if __name__ == "__main__":
-    while True:
-        notification.notify(
-            title = "ALERT!!!",
-            message = "Rest! Now is the time to walk!",
-            timeout = 10
-        )
-        time.sleep(15)
+# if __name__ == "__main__":
+#     while True:
+#         notification.notify(
+#             title = "ALERT!!!",
+#             message = "Rest! Now is the time to walk!",
+#             timeout = 10
+#         )
+#         time.sleep(15)
     
+
+# Solution 2 - advanced
+
+import time
+from win10toast import ToastNotifier
+# win10toast: An easy-to-use Python library for displaying Windows 10 Toast Notifications
+
+reminder_Time = input("Input Time in 24hr format(HH:MM:SS) to set reminder-> ")
+rem_message = input("Enter your message:> ")
+
+while True:
+    current_time = time.strftime("%H:%M:%S")
+    if current_time == reminder_Time:
+        print(current_time)
+        break;
+notify = ToastNotifier()
+notify.show_toast("Notification",rem_message)
