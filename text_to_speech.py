5e5eb81f20293878d327fbf2a111136cb55eb456
Amir Motefaker <amotef@gmail.com>
2022-05-18 13:48:33 +0430

    Update text_to_speech.py
    Added Solution 2 with os library(play the converted audio)

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,8 +2,10 @@
 
 # Convert Text to Speech
 
-from gtts import gTTS
-from playsound import playsound
+# Solution 1
+
+# from gtts import gTTS
+# from playsound import playsound
 # gTTS (Google Text-to-Speech), a Python library and CLI tool to interface with Google Translate's text-to-speech API.
 # playsound: Pure Python, cross platform, single function module with no dependencies for playing sounds.
 
@@ -13,6 +15,22 @@ from playsound import playsound
 # playsound("hello.mp3")
 
 # in spanish
-tts = gTTS('Hola Mundo', lang='es')
-tts.save('hola.mp3')
-playsound("hola.mp3")
+# tts = gTTS('Hola Mundo', lang='es')
+# tts.save('hola.mp3')
+# playsound("hola.mp3")
+
+
+
+# Solution 2
+
+from gtts import gTTS
+import os   # os: play the converted audio
+  
+text = 'Welcome to Visual Studio Code!'
+  
+language = 'en'
+
+my_object = gTTS(text=text, lang=language, slow=False)
+my_object.save("welcome.mp3")
+  
+os.system("welcome.mp3")
