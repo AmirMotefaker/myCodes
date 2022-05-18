d030e4d4c398d8b35698e48844e1bf9e3a22b704
Amir Motefaker <amotef@gmail.com>
2022-05-18 10:43:27 +0430

    Create message_encode_decode.py

---
+++ message_encode_decode.py
@@ -0,0 +1,28 @@
+# Code by amotef@gmail.com
+
+# Message Encode
+
+# Solution 1 - basic
+def encode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    return encoded_string
+
+def decode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    return encoded_string
+
+
+e = encode('Amir', 'Motefaker')
+d = decode('Amir', e)
+print([e])
+print([d])
