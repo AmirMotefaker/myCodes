7fb47019c493ad9d6a586814a176b5d55c270e11
Amir Motefaker <amotef@gmail.com>
2022-05-23 15:20:23 +0430

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,30 @@
+# Code by @AmirMotefaker
+
+# Weather Information
+
+import requests
+
+api_key = "2cf366b47b474b66d3cb60449bd6f658"  # Enter the API key you got from the OpenWeatherMap website
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+city_name = input("Enter city name : ")
+complete_url = base_url + "appid=" + 'd850f7f52bf19300a9eb4b0aa6b80f0d' + "&q=" + city_name  # This is to complete the base_url, you can also do this manually to checkout other weather data available
+response = requests.get(complete_url)
+x = response.json()
+
+if x["cod"] != "404":
+    y = x["main"]
+
+    current_temperature = y["temp"]
+    z = x["weather"]
+
+    weather_description = z[0]["description"]
+
+    print(" Temperature (in kelvin unit) = " +
+                    str(current_temperature) +
+          "\n description = " +
+                    str(weather_description))
+
+else:
+    print(" City Not Found ")
+    
