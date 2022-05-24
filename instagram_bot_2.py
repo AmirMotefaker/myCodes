0c87c5f70a0cb5d8fed8c011c7c8e6f57aa1cac0
Amir Motefaker <amotef@gmail.com>
2022-05-24 12:48:44 +0430

    Create instagram_bot_2.py

---
+++ instagram_bot_2.py
@@ -0,0 +1,129 @@
+# Code by @AmirMotefaker
+
+# Instagram Bot (with InstaPy)
+
+# pip install InstaPy
+# InstaPy: Tool for automated Instagram interactions
+
+# https://bit.ly/3wHOWgp
+
+## Automate a Browser
+from time import sleep
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+browser.get('https://www.instagram.com/')
+
+sleep(5)
+
+browser.close()
+
+
+## add login link 
+from time import sleep
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.implicitly_wait(5)
+
+browser.get('https://www.instagram.com/')
+
+login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+login_link.click()
+
+sleep(5)
+
+browser.close()
+
+
+## enters credentials
+from time import sleep
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.implicitly_wait(5)
+
+browser.get('https://www.instagram.com/')
+
+login_link = browser.find_element_by_xpath("//a[text()='Log in']")
+login_link.click()
+
+sleep(2)
+
+username_input = browser.find_element_by_css_selector("input[name='username']")
+password_input = browser.find_element_by_css_selector("input[name='password']")
+
+username_input.send_keys("<your username>")
+password_input.send_keys("<your password>")
+
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+
+sleep(5)
+
+browser.close()
+
+
+## test code
+def test_login_page(browser):
+    browser.get('https://www.instagram.com/accounts/login/')
+    username_input = browser.find_element_by_css_selector("input[name='username']")
+    password_input = browser.find_element_by_css_selector("input[name='password']")
+    username_input.send_keys("<your username>")
+    password_input.send_keys("<your password>")
+    login_button = browser.find_element_by_xpath("//button[@type='submit']")
+    login_button.click()
+
+    errors = browser.find_elements_by_css_selector('#error_message')
+    assert len(errors) == 0
+
+
+## page object classes
+from time import sleep
+
+class LoginPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def login(self, username, password):
+        username_input = self.browser.find_element_by_css_selector("input[name='username']")
+        password_input = self.browser.find_element_by_css_selector("input[name='password']")
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        login_button = browser.find_element_by_xpath("//button[@type='submit']")
+        login_button.click()
+        sleep(5)
+
+class HomePage:
+    def __init__(self, browser):
+        self.browser = browser
+        self.browser.get('https://www.instagram.com/')
+
+    def go_to_login_page(self):
+        self.browser.find_element_by_xpath("//a[text()='Log in']").click()
+        sleep(2)
+        return LoginPage(self.browser)
+
+
+## Page Object Pattern
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.implicitly_wait(5)
+
+home_page = HomePage(browser)
+login_page = home_page.go_to_login_page()
+login_page.login("<your username>", "<your password>")
+
+browser.close()
+
+
+## test Page Object Pattern
+def test_login_page(browser):
+    home_page = HomePage(browser)
+    login_page = home_page.go_to_login_page()
+    login_page.login("<your username>", "<your password>")
+
+    errors = browser.find_elements_by_css_selector('#error_message')
+    assert len(errors) == 0
