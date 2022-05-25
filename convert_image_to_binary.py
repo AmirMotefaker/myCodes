96686dc188e55973450782f6ba48273c5fe3ab59
Amir Motefaker <amotef@gmail.com>
2022-05-25 12:40:12 +0430

    Update convert_image_to_binary.py
    Added solution2. Convert color image to black and white.

--- convert_image_to_binary.py
+++ convert_image_to_binary.py
@@ -15,18 +15,35 @@
 # 3- Set up a Threshold mark, pixels above the given mark
 #    will turn white, and below the mark will turn black.
 
+# import cv2
+  
+# # read the image file
+# img = cv2.imread('image.png', 2)
+  
+# ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+  
+# # converting to its binary form
+# bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+  
+
+# cv2.imshow("Binary", bw_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+## Solution 2
+
+## Convert color image to black and white
 import cv2
-  
-# read the image file
-img = cv2.imread('image.png', 2)
-  
-ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-  
-# converting to its binary form
-bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-  
 
-cv2.imshow("Binary", bw_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#read image
+img_grey = cv2.imread('image2.png', cv2.IMREAD_GRAYSCALE)
 
+# define a threshold, 128 is the middle of black and white in grey scale
+thresh = 128
+
+# threshold the image
+img_binary = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)[1]
+
+#save image
+cv2.imwrite('image2-binary.png',img_binary) 
