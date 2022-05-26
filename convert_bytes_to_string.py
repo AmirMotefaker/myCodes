3c1f662bcc680177d816c4d78d7ae1950633d16a
Amir Motefaker <amotef@gmail.com>
2022-05-26 12:50:10 +0430

    Update convert_bytes_to_string.py
    Added comment.

--- convert_bytes_to_string.py
+++ convert_bytes_to_string.py
@@ -2,4 +2,8 @@
 
 # Convert Bytes to a String
 
+# Using decode(), you can convert bytes into string.
+# Here, we have used utf-8 for decoding.
+# \xE2\x9C\x85 is the utf-8 code for ✅.
+
 print(b'Amir Motefaker \xE2\x9C\x85'.decode("utf-8"))
