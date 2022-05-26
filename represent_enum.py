d327475a5c070a43243fad067d1a81b8c416f97d
Amir Motefaker <amotef@gmail.com>
2022-05-26 13:01:24 +0430

    Create represent_enum.py

---
+++ represent_enum.py
@@ -0,0 +1,23 @@
+# Code by @AmirMotefaker
+
+# Represent enum
+
+from enum import Enum
+
+class Day(Enum):
+    SUNDAY =  1
+    MONDAY = 2
+    TUESDAY = 3
+    WEDNESDAY = 4
+    THURSDAY = 5
+    FRIDAY = 6
+    SATURDAY = 7
+
+# print the enum member
+print(Day.WEDNESDAY)
+
+# get the name of the enum member
+print(Day.WEDNESDAY.name)
+
+# get the value of the enum member
+print(Day.WEDNESDAY.value)
