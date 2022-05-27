35e7877e573c223a721be64fd9171e60d58bb9ae
Amir Motefaker <amotef@gmail.com>
2022-05-27 06:37:23 +0430

    Update power_number.py
    Added solution 2 by using pow() function.

--- power_number.py
+++ power_number.py
@@ -6,25 +6,36 @@
 
 # base = 3
 # exponent = 4
-# 
+#
 # result = 1
-# 
+#
 # while exponent != 0:
 #     result *= base
 #     exponent-=1
-# 
+#
 # print("Answer = " + str(result))
 
 
 
 # Solution 2: Calculate power of a number using a for loop
 
+# base = 3
+# exponent = 4
+
+# result = 1
+
+# for exponent in range(exponent, 0, -1):
+#     result *= base
+
+# print("Answer = " + str(result))
+
+
+
+# Solution 3: Calculate the power of a number using pow() function
+
 base = 3
-exponent = 4
+exponent = -4
 
-result = 1
-
-for exponent in range(exponent, 0, -1):
-    result *= base
+result = pow(base, exponent)
 
 print("Answer = " + str(result))
