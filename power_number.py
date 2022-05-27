1550b8df45ecbf41e9ed7b0147f4ec7162c30806
Amir Motefaker <amotef@gmail.com>
2022-05-27 06:35:14 +0430

    Update power_number.py
    Added solution 2 by using a for a loop.

--- power_number.py
+++ power_number.py
@@ -4,13 +4,27 @@
 
 # Solution 1: Calculate power of a number using a while loop
 
+# base = 3
+# exponent = 4
+# 
+# result = 1
+# 
+# while exponent != 0:
+#     result *= base
+#     exponent-=1
+# 
+# print("Answer = " + str(result))
+
+
+
+# Solution 2: Calculate power of a number using a for loop
+
 base = 3
 exponent = 4
 
 result = 1
 
-while exponent != 0:
+for exponent in range(exponent, 0, -1):
     result *= base
-    exponent-=1
 
 print("Answer = " + str(result))
