a4fdc6f872db2048d99f0ffa0b3ec5c776b73221
Amir Motefaker <amotef@gmail.com>
2022-05-27 06:14:14 +0430

    Create multiplication_table.py

---
+++ multiplication_table.py
@@ -0,0 +1,14 @@
+# Code by @AmirMotefaker
+
+# Display the multiplication Table
+
+# Multiplication table (from 1 to 10) in Python
+
+num = 13
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
