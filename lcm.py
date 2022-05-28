d86ef52ee6f269da8fbfece947f79faf20b021d6
Amir Motefaker <amotef@gmail.com>
2022-05-28 12:35:05 +0430

    Create lcm.py

---
+++ lcm.py
@@ -0,0 +1,31 @@
+# Code by @AmirMotefaker
+
+# Find LCM
+
+# The Least Common Multiple (LCM) is also referred to as the
+# Lowest Common Multiple (LCM) and Least Common Divisor (LCD). 
+# For two integers a and b, denoted LCM(a,b), 
+# the LCM is the smallest positive integer that is evenly divisible by both a and b.
+
+# For example, LCM(2,3) = 6 and LCM(6,10) = 30.
+
+
+def lcm(x, y):
+
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+
+print("The L.C.M. is", lcm(num1, num2))
