53c2a6b738815d74172b60c2a824cdb38bd3fa5b
Amir Motefaker <amotef@gmail.com>
2022-05-28 12:30:46 +0430

    Update gcd.py
    Added comment

--- gcd.py
+++ gcd.py
@@ -1,9 +1,21 @@
 # Code by @AmirMotefaker
 
 # Find GCD of two numbers
+
 # GCD: The greatest common divisor (GCD),
-#  also called the greatest common factor,
-#  of two numbers is the largest number that divides them both.
+# also called the greatest common factor,
+# of two numbers is the largest number that divides them both.
+
+# Example: Find the greatest common divisor of 13 and 48.
+# Solution: We will use the below steps to determine the greatest common divisor of (13, 48).
+
+# Divisors of 13 are 1, and 13.
+# Divisors of 48 are 1, 2, 3, 4, 6, 8, 12, 16, 24 and 48.
+
+# The common divisor of 13 and 48 is 1.
+# The greatest common divisor of 13 and 48 is 1.
+
+# Thus, GCD(13, 48) = 1.
 
 def gcd(x, y):
 
@@ -21,4 +33,3 @@ num1 = int(input("Enter number1: "))
 num2 = int(input("Enter number2: "))
 
 print("The GCD is", gcd(num1, num2))
-
