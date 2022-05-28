90e1e23c866e958b9b56863d52a7e2bc66c448a8
Amir Motefaker <amotef@gmail.com>
2022-05-28 12:12:51 +0430

    Update sum_of_natural_numbers.py
    Added comment

--- sum_of_natural_numbers.py
+++ sum_of_natural_numbers.py
@@ -2,6 +2,10 @@
 
 # Find the Sum of Natural Numbers
 
+# We could have solved the above problem without using a loop
+# by using the following formula:
+#     n*(n+1)/2
+# For example, if n = 16, the sum would be (16*17)/2 = 136.
 
 num = int(input("Enter number: "))
 
@@ -14,4 +18,5 @@ else:
        sum += num
        num -= 1
    print("The sum is", sum)
-  
+   
+ 
