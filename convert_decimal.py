7491635c607456d91c55f2b61218bcf8ce38c8ba
Amir Motefaker <amotef@gmail.com>
2022-05-28 12:21:52 +0430

    Create convert_decimal.py

---
+++ convert_decimal.py
@@ -0,0 +1,11 @@
+# Code by @AmirMotefaker
+
+# Convert Decimal to Binary, Octal and Hexadecimal
+
+dec = int(input("Enter number: "))
+
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
