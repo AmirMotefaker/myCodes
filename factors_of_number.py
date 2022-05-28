276ed1aec369ae0462608e240e4945d8cdd010e5
Amir Motefaker <amotef@gmail.com>
2022-05-28 12:37:14 +0430

    Create factors_of_number.py

---
+++ factors_of_number.py
@@ -0,0 +1,13 @@
+# Code by @AmirMotefaker
+
+# Find the Factors of a Number
+
+def factors_number(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("Enter number: "))
+
+factors_number(num)
