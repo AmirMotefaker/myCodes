ee1a46d5b971ffad50917fa5c27c29cb687d6daf
Amir Motefaker <amotef@gmail.com>
2022-05-29 15:35:03 +0430

    Create multiply_two_matrices.py

---
+++ multiply_two_matrices.py
@@ -0,0 +1,26 @@
+# Code by @AmirMotefaker
+
+# Multiply Two Matrices
+
+# X = 3x3 matrix
+# Y = 3x4 matrix
+# result is 3x4
+
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+Y = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+for i in range(len(X)):
+   for j in range(len(Y[0])):
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
