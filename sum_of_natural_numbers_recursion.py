36eb1813f2a93866b3c886b0a3159de0fe35593f
Amir Motefaker <amotef@gmail.com>
2022-05-29 13:22:05 +0430

    Create sum_of_natural_numbers_recursion.py

---
+++ sum_of_natural_numbers_recursion.py
@@ -0,0 +1,17 @@
+# Code by @AmirMotefaker
+
+# Find the Sum of Natural Numbers Using Recursion
+
+def sum_recursion(n):
+   if n <= 1:
+       return n
+   else:
+       return n + sum_recursion(n-1)
+
+num = int(input("Enter number: "))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   print("The sum is",sum_recursion(num))
+   
