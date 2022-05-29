1354f46f1ba660672ef4b6d41475c4d27f876628
Amir Motefaker <amotef@gmail.com>
2022-05-29 12:47:00 +0430

    Create display_calendar.py

---
+++ display_calendar.py
@@ -0,0 +1,12 @@
+# Code by @AmirMotefaker
+
+# Display Calendar
+
+
+import calendar
+
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
