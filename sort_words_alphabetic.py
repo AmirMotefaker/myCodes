9ad6dcf4b8d3d1f8fe8556f645f137c38e064c3a
Amir Motefaker <amotef@gmail.com>
2022-05-30 13:34:35 +0430

    Create sort_words_alphabetic.py

---
+++ sort_words_alphabetic.py
@@ -0,0 +1,13 @@
+# Code by @AmirMotefaker
+
+# Sort Words in Alphabetic Order
+
+my_str = input("Enter a string: ")
+
+words = [word.lower() for word in my_str.split()]
+
+words.sort()
+
+print("The sorted words are:")
+for word in words:
+   print(word)
