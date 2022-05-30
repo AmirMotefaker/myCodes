235ba72b00cdfc1a074ea78ee25a1dbfeafbe789
Amir Motefaker <amotef@gmail.com>
2022-05-30 14:31:10 +0430

    Update index_list.py
    Added solution 3 without using enumerate().

--- index_list.py
+++ index_list.py
@@ -12,7 +12,16 @@
 
 # Solution 2 - Start the indexing with non zero value
 
+# my_list = [21, 44, 35, 11]
+
+# for index, val in enumerate(my_list, start=1):
+#     print(index, val)
+
+
+# Solution 3 - Without using enumerate()
+
 my_list = [21, 44, 35, 11]
 
-for index, val in enumerate(my_list, start=1):
-    print(index, val)
+for index in range(len(my_list)):
+    value = my_list[index]
+    print(index, value)
