b59bd2cd1ded3fb3da7cfb71a313e81252a18b73
Amir Motefaker <amotef@gmail.com>
2022-05-30 14:29:59 +0430

    Update index_list.py
    Added solution 2 with Start the indexing with non zero value.

--- index_list.py
+++ index_list.py
@@ -4,7 +4,15 @@
 
 # Solution 1 - Using enumerate
 
+# my_list = [21, 44, 35, 11]
+
+# for index, val in enumerate(my_list):
+#     print(index, val)
+
+
+# Solution 2 - Start the indexing with non zero value
+
 my_list = [21, 44, 35, 11]
 
-for index, val in enumerate(my_list):
+for index, val in enumerate(my_list, start=1):
     print(index, val)
