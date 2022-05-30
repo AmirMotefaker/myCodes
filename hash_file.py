a52e5ecd03d768ae088f3128c61dc159054a7305
Amir Motefaker <amotef@gmail.com>
2022-05-30 14:14:57 +0430

    Create hash_file.py

---
+++ hash_file.py
@@ -0,0 +1,27 @@
+# Code by @AmirMotefaker
+
+# Find Hash of File
+
+import hashlib
+
+# This function returns the SHA-1 hash of the file passed into it
+def hash_file(filename):
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
+message = hash_file("filename.txt")
+print(message)
