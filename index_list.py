0012de55f77d6642a8009cf272f6aaede07c112f
Amir Motefaker <amotef@gmail.com>
2022-05-30 14:28:35 +0430

    Create index_list.py

---
+++ index_list.py
@@ -0,0 +1,10 @@
+# Code by @AmirMotefaker
+
+# Access Index of a List Using for Loop
+
+# Solution 1 - Using enumerate
+
+my_list = [21, 44, 35, 11]
+
+for index, val in enumerate(my_list):
+    print(index, val)
