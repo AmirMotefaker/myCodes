b50340d0ef35f645cf46d2788c1ba42ee24bb4bf
Amir Motefaker <amotef@gmail.com>
2022-05-30 13:36:43 +0430

    Create different_set_operations.py

---
+++ different_set_operations.py
@@ -0,0 +1,15 @@
+# Code by @AmirMotefaker
+
+# Illustrate Different Set Operations
+
+
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+print("Union of E and N is",E | N)
+
+print("Intersection of E and N is",E & N)
+
+print("Difference of E and N is",E - N)
+
+print("Symmetric difference of E and N is",E ^ N)
