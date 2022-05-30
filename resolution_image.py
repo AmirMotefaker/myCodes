16023d44f7d42c22e3141ede2f01edcc95768d35
Amir Motefaker <amotef@gmail.com>
2022-05-30 13:58:25 +0430

    Update resolution_image.py
    Added solution 2 with Using OpenCV.

--- resolution_image.py
+++ resolution_image.py
@@ -4,14 +4,33 @@
 
 
 # Solution 1 - Using PIL
-import PIL
-from PIL import Image
+
+# import PIL
+# from PIL import Image
+  
+# # loading the image
+# img = PIL.Image.open("image.png")
+  
+# # fetching the dimensions
+# wid, hgt = img.size
+  
+# # displaying the dimensions
+# print(str(wid) + "x" + str(hgt))
+
+
+
+# solution 2 - Using OpenCV
+
+import cv2
   
 # loading the image
-img = PIL.Image.open("image.png")
+img = cv2.imread("image.png")
   
 # fetching the dimensions
-wid, hgt = img.size
+wid = img.shape[1]
+hgt = img.shape[0]
   
 # displaying the dimensions
 print(str(wid) + "x" + str(hgt))
+
+
