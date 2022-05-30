41141a2eba78bf55fc28af428d449222a88c55db
Amir Motefaker <amotef@gmail.com>
2022-05-30 14:19:34 +0430

    Update merge_two_dictionaries.py
    Added solution 2 with Using the ** Operator

--- merge_two_dictionaries.py
+++ merge_two_dictionaries.py
@@ -2,7 +2,17 @@
 
 # Merge Two Dictionaries
 
+# Solution 1 - Using the | Operator
+
 dict_1 = {1: 'a', 2: 'b'}
 dict_2 = {2: 'c', 4: 'd'}
 
 print(dict_1 | dict_2)
+
+
+# Solution 2 - Using the ** Operator
+
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print({**dict_1, **dict_2})
