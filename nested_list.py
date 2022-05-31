f699b4dc66e2f612d7abe95913ab99fde2702ae2
Amir Motefaker <amotef@gmail.com>
2022-05-31 13:55:55 +0430

    Update nested_list.py
    Added solution 2 with Using Nested for Loops (non pythonic way)

--- nested_list.py
+++ nested_list.py
@@ -2,7 +2,19 @@
 
 # Flatten a Nested List
 
+# Solution 1 - Using List Comprehension
+# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+# flat_list = [num for sublist in my_list for num in sublist]
+# print(flat_list)
+
+
+# Solution 2 - Using Nested for Loops (non pythonic way)
 my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-flat_list = [num for sublist in my_list for num in sublist]
+flat_list = []
+for sublist in my_list:
+    for num in sublist:
+        flat_list.append(num)
+
 print(flat_list)
