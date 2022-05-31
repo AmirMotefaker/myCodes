e87199787a58d050ef42321f8183af76f02acd00
Amir Motefaker <amotef@gmail.com>
2022-05-31 15:17:25 +0430

    Update list_empty.py
    Added solution 3 with Comparing with []

--- list_empty.py
+++ list_empty.py
@@ -9,6 +9,12 @@
 
 
 # Solution 2 - Using len()
+# my_list = []
+# if not len(my_list):
+#     print("the list is empty")
+
+
+# Solution 3 - Comparing with []
 my_list = []
-if not len(my_list):
-    print("the list is empty")
+if my_list == []:
+    print("The list is empty")
