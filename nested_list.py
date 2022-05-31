44cfb5ee83ced184f8d86e18a494f02867173cde
Amir Motefaker <amotef@gmail.com>
2022-05-31 13:57:06 +0430

    Update nested_list.py
    Added solution3 with Using itertools package

--- nested_list.py
+++ nested_list.py
@@ -10,11 +10,21 @@
 
 
 # Solution 2 - Using Nested for Loops (non pythonic way)
+# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+
+# flat_list = []
+# for sublist in my_list:
+#     for num in sublist:
+#         flat_list.append(num)
+
+# print(flat_list)
+
+
+# Solution 3 - Using itertools package
+import itertools
+
 my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-flat_list = []
-for sublist in my_list:
-    for num in sublist:
-        flat_list.append(num)
-
+flat_list = list(itertools.chain(*my_list))
 print(flat_list)
+
