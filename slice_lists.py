ec4cb24bc68ac7ad403fc673db2d6dcdf423d733
Amir Motefaker <amotef@gmail.com>
2022-05-31 14:33:02 +0430

    Update slice_lists.py
    Added solution4 with Get all the Items from One Position to Another Position

--- slice_lists.py
+++ slice_lists.py
@@ -9,13 +9,19 @@
 
 
 # Solution 2 - Get all the Items After a Specific Position
+# my_list = [1, 2, 3, 4, 5]
+
+# print(my_list[2:])
+
+
+
+# Solution 3 - Get all the Items Before a Specific Position 2
+# my_list = [1, 2, 3, 4, 5]
+
+# print(my_list[:2])
+
+
+# Solution 4 - Get all the Items from One Position to Another Position
 my_list = [1, 2, 3, 4, 5]
 
-print(my_list[2:])
-
-
-
-# Solution 3 -Get all the Items Before a Specific Position 2
-my_list = [1, 2, 3, 4, 5]
-
-print(my_list[:2])
+print(my_list[2:4])
