bc2c18c52ecd659f3da95a19c8ae2b679e2b7118
Amir Motefaker <amotef@gmail.com>
2022-05-31 15:01:53 +0430

    Update dictionaries_iterate .py
    Added solution 2 with Access both key and value without using items()

--- dictionaries_iterate .py
+++ dictionaries_iterate .py
@@ -2,7 +2,15 @@
 
 # Iterate Over Dictionaries Using for Loop
 
+# Solution 1 - Access both key and value using items()
+# dt = {'a': 'juice', 'b': 'grill', 'c': 'corn'}
+
+# for key, value in dt.items():
+#     print(key, value)
+
+
+# Solution 2 - Access both key and value without using items()
 dt = {'a': 'juice', 'b': 'grill', 'c': 'corn'}
 
-for key, value in dt.items():
-    print(key, value)
+for key in dt:
+    print(key, dt[key])
