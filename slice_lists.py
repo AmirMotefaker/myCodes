b0a4e5dfe51ddd50bfa388d5e8b737c03cc88f0f
Amir Motefaker <amotef@gmail.com>
2022-05-31 14:30:46 +0430

    Update slice_lists.py
    Added solution2 with Get all the Items After a Specific Position

--- slice_lists.py
+++ slice_lists.py
@@ -3,6 +3,12 @@
 # Slice Lists
 
 # Solution 1 - Get all the Items
+# my_list = [1, 2, 3, 4, 5]
+
+# print(my_list[:])
+
+
+# Solution 2 - Get all the Items After a Specific Position
 my_list = [1, 2, 3, 4, 5]
 
-print(my_list[:])
+print(my_list[2:])
