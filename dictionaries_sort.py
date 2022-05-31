822d879c90a28f9a31be0853f2a04fb76e895989
Amir Motefaker <amotef@gmail.com>
2022-05-31 15:13:38 +0430

    Update dictionaries_sort.py
    Added solution 2 with Sort only the values

--- dictionaries_sort.py
+++ dictionaries_sort.py
@@ -3,9 +3,16 @@
 # Sort a Dictionary by Value
 
 # Solution 1 - Sort the dictionary based on values
+# dt = {5:4, 1:6, 6:3}
+
+# sorted_dt = {key: value for key, value in sorted(dt.items(), key=lambda item: item[1])}
+
+# print(sorted_dt)
+
+
+
+# Solution 2 - Sort only the values
 dt = {5:4, 1:6, 6:3}
 
-sorted_dt = {key: value for key, value in sorted(dt.items(), key=lambda item: item[1])}
-
-print(sorted_dt)
-
+sorted_dt_value = sorted(dt.values())
+print(sorted_dt_value)
