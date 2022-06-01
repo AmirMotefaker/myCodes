c7d918ee911cd96532c02795bd4a386438a1c2ba
Amir Motefaker <amotef@gmail.com>
2022-06-01 12:35:25 +0430

    Create string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,17 @@
+# Code by @AmirMotefaker
+
+# String Formatting
+
+color = 'yellow'
+print("{name}'s {object} is {color}".format(color = "blue", object = "bag", name = "john"))
+
+print("My score is {score:10.3f}". format(score = 10 / 3))
+
+
+# f-strings
+name = 'John'
+item = 'car'
+color = 'blue'
+
+print(f"{name}'s {item} is {color}.")
+
