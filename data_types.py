db493e0550eaeee8d9af53eaa9e7fc3fed684dd4
Amir Motefaker <amotef@gmail.com>
2022-06-01 11:47:15 +0430

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,23 @@
+# Code by @AmirMotefaker
+
+# Data Types
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+my_int = 21
+my_float = 20.5
+my_string = "hello"
+my_list = [1, 2, 'hello']
+my_dict = {'key1': 'value1'}
+my_tuple = (1 ,2, 'hello')
+my_boolean = True
+
+print(type(my_tuple))
+
