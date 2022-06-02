80fa48287a6ffe778e674b4fb3d1fdf7d71bcda4
Amir Motefaker <amotef@gmail.com>
2022-06-02 12:10:50 +0430

    Update multiple_exceptions.py
    Added comment

--- multiple_exceptions.py
+++ multiple_exceptions.py
@@ -4,6 +4,9 @@
 
 # Multiple exceptions can be caught using a tuple.
 
+from re import A
+
+
 string = input("Enter :")
 
 try:
@@ -11,3 +14,16 @@ try:
     print(string+num)
 except (TypeError, ValueError) as e:
     print(e)
+
+
+# Input:
+# a
+# 2
+# Output:
+# can only concatenate str (not "int") to str
+
+# Input:
+# a
+# b
+# Output:
+# invalid literal for int() with base 10: 'b'
