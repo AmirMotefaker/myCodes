d6eeaa64514df37e08ff22436667b4ad95591e17
Amir Motefaker <amotef@gmail.com>
2022-06-03 01:22:05 +0430

    Update string_parse.py
    Added solution 2 with Parse string into float

--- string_parse.py
+++ string_parse.py
@@ -3,12 +3,22 @@
 # Parse a String to a Float or Int
 
 # Solution 1 - Parse string into integer
-balance_str = "1500"
-balance_int = int(balance_str)
+# balance_str = "1500"
+# balance_int = int(balance_str)
+
+# # print the type
+# print(type(balance_int))
+
+# # print the value
+# print(balance_int)
+
+
+# Solution 2 - Parse string into float
+balance_str = "1500.4"
+balance_float = float(balance_str)
 
 # print the type
-print(type(balance_int))
+print(type(balance_float))
 
 # print the value
-print(balance_int)
-
+print(balance_float)
