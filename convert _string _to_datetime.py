efbb54c692a003de513c9c2db04139e73dfc4d50
Amir Motefaker <amotef@gmail.com>
2022-06-03 01:45:26 +0430

    Update convert _string _to_datetime.py
    Added solution 2 with Using dateutil module

--- convert _string _to_datetime.py
+++ convert _string _to_datetime.py
@@ -3,11 +3,20 @@
 # Convert String to Datetime
 
 # Solution 1 - Using datetime module
-from datetime import datetime
+# from datetime import datetime
 
-my_date_string = "Mar 11 2011 11:31AM"
+# my_date_string = "Mar 11 2011 11:31AM"
 
-datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
+# datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
 
-print(type(datetime_object))
-print(datetime_object)
+# print(type(datetime_object))
+# print(datetime_object)
+
+
+# Solution 2 - Using dateutil module
+from dateutil import parser
+
+date_time = parser.parse("Mar 11 2011 11:31AM")
+
+print(date_time)
+print(type(date_time))
