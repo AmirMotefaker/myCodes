8899be52dbb7ecc9c8dbabc8c95d226bd964da08
Amir Motefaker <amotef@gmail.com>
2022-06-03 01:20:50 +0430

    Create string_parse.py

---
+++ string_parse.py
@@ -0,0 +1,14 @@
+# Code by @AmirMotefaker
+
+# Parse a String to a Float or Int
+
+# Solution 1 - Parse string into integer
+balance_str = "1500"
+balance_int = int(balance_str)
+
+# print the type
+print(type(balance_int))
+
+# print the value
+print(balance_int)
+
