3c00a01c8b5146292f143e2c3983ee568e21befe
Amir Motefaker <amotef@gmail.com>
2022-06-02 12:18:41 +0430

    Create concatenate_two_lists.py

---
+++ concatenate_two_lists.py
@@ -0,0 +1,12 @@
+# Code by @AmirMotefaker
+
+# Concatenate Two Lists
+
+
+# Solution 1 - Using + operator
+list_1 = [1, 'a']
+list_2 = [3, 4, 5]
+
+list_joined = list_1 + list_2
+print(list_joined)
+
