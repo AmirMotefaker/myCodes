87ab0d4325b888b70baed83b31e9be41edeabb2b
Amir Motefaker <amotef@gmail.com>
2022-06-02 12:19:54 +0430

    Update concatenate_two_lists.py
    Added solution 2 with Using iterable unpacking operator *

--- concatenate_two_lists.py
+++ concatenate_two_lists.py
@@ -4,9 +4,17 @@
 
 
 # Solution 1 - Using + operator
+# list_1 = [1, 'a']
+# list_2 = [3, 4, 5]
+
+# list_joined = list_1 + list_2
+# print(list_joined)
+
+
+
+# Solution 2 - Using iterable unpacking operator *
 list_1 = [1, 'a']
-list_2 = [3, 4, 5]
+list_2 = range(2, 4)
 
-list_joined = list_1 + list_2
+list_joined = [*list_1, *list_2]
 print(list_joined)
-
