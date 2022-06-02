3d889e276e0df9776e12c3bcb136fd352ac5ff9e
Amir Motefaker <amotef@gmail.com>
2022-06-02 12:26:49 +0430

    Update split_list.py
    Added solution 2

--- split_list.py
+++ split_list.py
@@ -11,3 +11,10 @@ def split(list_a, chunk_size):
 chunk_size = 2
 my_list = [1,2,3,4,5,6,7,8,9]
 print(list(split(my_list, chunk_size)))
+
+
+# Solution 2
+chunk_size = 2
+list_chunked = [my_list[i:i + chunk_size] for i in range(0, len(my_list), chunk_size)]
+print(list_chunked)
+
