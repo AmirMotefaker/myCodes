cf9706451aaae67bd97523ebb5279982c9b39bd6
Amir Motefaker <amotef@gmail.com>
2022-06-10 01:41:07 +0430

    Update read_file.py
    Added solution 2 with Using for loop and list comprehension

--- read_file.py
+++ read_file.py
@@ -3,13 +3,26 @@
 # Read a File Line by Line Into a List
 
 # Solution 1 - Using readlines()
-with open("data_file.txt") as f:
-    content_list = f.readlines()
+# with open("data_file.txt") as f:
+#     content_list = f.readlines()
 
-# print the list
+# # print the list
+# print(content_list)
+
+# # remove new line characters
+# content_list = [x.strip() for x in content_list]
+# print(content_list)
+
+
+# Solution 2 - Using for loop and list comprehension
+with open('data_file.txt') as f:
+    content_list = [line for line in f]
+
 print(content_list)
 
-# remove new line characters
-content_list = [x.strip() for x in content_list]
+# removing the characters
+with open('data_file.txt') as f:
+    content_list = [line.rstrip() for line in f]
+
 print(content_list)
 
