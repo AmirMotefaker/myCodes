071933e21bae9ff378762b976bbdad8f348836d6
Amir Motefaker <amotef@gmail.com>
2022-06-10 01:39:40 +0430

    Create read_file.py

---
+++ read_file.py
@@ -0,0 +1,15 @@
+# Code by @AmirMotefaker
+
+# Read a File Line by Line Into a List
+
+# Solution 1 - Using readlines()
+with open("data_file.txt") as f:
+    content_list = f.readlines()
+
+# print the list
+print(content_list)
+
+# remove new line characters
+content_list = [x.strip() for x in content_list]
+print(content_list)
+
