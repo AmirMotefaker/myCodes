7a6141cd4da44913c52fd14e2de8e6295aac5c31
Amir Motefaker <amotef@gmail.com>
2022-06-10 01:49:37 +0430

    Create list_randomly_select.py

---
+++ list_randomly_select.py
@@ -0,0 +1,9 @@
+# Code by @AmirMotefaker
+
+# Randomly Select an Element From the List
+
+# Solution 1 - Using random module
+import random
+
+my_list = [1, 'a', 32, 'c', 'd', 31]
+print(random.choice(my_list))
