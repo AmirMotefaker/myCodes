9c116fa2c87c018556ab51b1872e7cd3b0600b43
Amir Motefaker <amotef@gmail.com>
2022-06-13 10:02:48 +0430

    Update elapsed_time.py
    Added solution 2 with Using timeit module

--- elapsed_time.py
+++ elapsed_time.py
@@ -2,33 +2,39 @@
 
 # Measure the Elapsed Time
 
-## Solution 1
+# Solution 1 - Using time module
 
 # Save the timestamp at the beginning of the code start using time().
 # Save the timestamp at the end of the code end.
 # Find the difference between the end and start, which gives the execution time.
 
-# import time
+import time
 
-# start = time.time()
+start = time.time()
 
-# print(22*1.4)
+print(22*2.4)
 
-# end = time.time()
-# print(end - start)
+end = time.time()
+print(end - start)
+
+# output:
+# 52.8
+# 0.001001119613647461
 
 
 
-## Solution 2
-
+# Solution 2 - Using timeit module
 
 from timeit import default_timer as timer
 # timeit provides the most accurate results.
 
 start = timer()
 
-print(22*1.4)
+print(22*2.4)
 
 end = timer()
 print(end - start)
 
+# output:
+# 52.8
+# 0.001001119613647461
