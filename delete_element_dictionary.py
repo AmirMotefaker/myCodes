0e5ef4dc1257ea4d2f7ceeee3d4279a005e2e0f9
Amir Motefaker <amotef@gmail.com>
2022-06-13 09:25:09 +0430

    Update delete_element_dictionary.py
    Added solution 2 with Using pop()

--- delete_element_dictionary.py
+++ delete_element_dictionary.py
@@ -3,6 +3,7 @@
 # Delete an Element From a Dictionary
 
 
+# Solution 1  -Using del keyword
 my_dict = {31: 'a', 21: 'b', 14: 'c'}
 
 del my_dict[31]
@@ -10,4 +11,16 @@ del my_dict[31]
 print(my_dict)
 
 # output:
+# {21: 'b', 14: 'c'}
+
+
+# Solution 2 - Using pop()
+my_dict = {31: 'a', 21: 'b', 14: 'c'}
+
+print(my_dict.pop(31))
+
+print(my_dict)
+
+# output:
+# a
 # {21: 'b', 14: 'c'}
