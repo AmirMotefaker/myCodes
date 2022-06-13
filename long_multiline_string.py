b273ffc0a64146f5256af7fdf10fbeb1d805ef6c
Amir Motefaker <amotef@gmail.com>
2022-06-13 09:45:00 +0430

    Create long_multiline_string.py

---
+++ long_multiline_string.py
@@ -0,0 +1,14 @@
+# Code by @AmirMotefaker
+
+# Create a Long Multiline String
+
+my_string = '''The only way to
+learn to program is
+by writing code.'''
+
+print(my_string)
+
+# output:
+# The only way to
+# learn to program is
+# by writing code.
