9bf9bb250abac96fceb97a5d8dd8c5e227d2b165
Amir Motefaker <amotef@gmail.com>
2022-06-13 09:16:28 +0430

    Create append_to_file.py

---
+++ append_to_file.py
@@ -0,0 +1,16 @@
+# Code by @AmirMotefaker
+
+# Append to a File
+# my_file.txt:
+# honda 1948
+# mercedes 1926
+# ford 1903
+
+with open("my_file.txt", "a") as f:
+   f.write("new text")
+
+# output:
+# change my_file.txt:
+# honda 1948
+# mercedes 1926
+# ford 1903new text
