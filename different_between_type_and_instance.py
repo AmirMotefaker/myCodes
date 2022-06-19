ada0c773bbf4432bfcfdb675abb7b7bf5ab4da87
Amir Motefaker <amotef@gmail.com>
2022-06-19 13:30:59 +0430

    Create different_between_type_and_instance.py

---
+++ different_between_type_and_instance.py
@@ -0,0 +1,27 @@
+# Code by @AmirMotefaker
+
+# Differentiate Between type() and isinstance()
+
+class Polygon:
+    def sides_no(self):
+        pass
+
+class Triangle(Polygon):
+    def area(self):
+        pass
+
+obj_polygon = Polygon()
+obj_triangle = Triangle()
+
+print(type(obj_triangle) == Triangle)   	# true
+print(type(obj_triangle) == Polygon)    	# false
+
+print(isinstance(obj_polygon, Polygon)) 	# true
+print(isinstance(obj_triangle, Polygon))	# true
+
+
+# Output:
+# True
+# False
+# True 
+# True 
