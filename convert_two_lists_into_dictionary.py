a06ea5ce40ba776f8308e5be7ad3f656f5ccd3cc
Amir Motefaker <amotef@gmail.com>
2022-06-19 13:27:12 +0430

    Update convert_two_lists_into_dictionary.py
    Added solution 2 with list comprehension

--- convert_two_lists_into_dictionary.py
+++ convert_two_lists_into_dictionary.py
@@ -3,12 +3,23 @@
 # Convert Two Lists Into a Dictionary
 
 # Solution 1 - Using zip and dict methods
-index = [1, 2, 3]
-languages = ['python', 'c', 'c++']
+# index = [1, 2, 3]
+# languages = ['python', 'c', 'c++']
 
-dictionary = dict(zip(index, languages))
-print(dictionary)
+# dictionary = dict(zip(index, languages))
+# print(dictionary)
 
 # output:
 # {1: 'python', 2: 'c', 3: 'c++'}
 
+
+
+# Solution 2 - Using list comprehension
+index = [1, 2, 3]
+languages = ['python', 'c', 'c++']
+
+dictionary = {k: v for k, v in zip(index, languages)}
+print(dictionary)
+
+# output:
+# {1: 'python', 2: 'c', 3: 'c++'}
