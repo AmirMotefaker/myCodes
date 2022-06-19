a1256d3ef2a46a1e88317cf2ab2cbba75a09940b
Amir Motefaker <amotef@gmail.com>
2022-06-19 13:22:45 +0430

    Update class_name_instance.py
    Added solution 2 with Using type() and __name__ attribute

--- class_name_instance.py
+++ class_name_instance.py
@@ -2,12 +2,25 @@
 
 # Get the Class Name of an Instance
 
+# Solution 1 - Using __class__.__name__
+# class Vehicle:
+#     def name(self, name):
+#         return name
+
+# v = Vehicle()
+# print(v.__class__.__name__)
+
+# Output:
+# Vehicle
+
+
+# Solution 2 - Using type() and __name__ attribute
 class Vehicle:
     def name(self, name):
         return name
 
 v = Vehicle()
-print(v.__class__.__name__)
+print(type(v).__name__)
 
 # Output:
 # Vehicle
