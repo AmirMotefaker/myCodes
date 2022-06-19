8c114239d6e0c971ccc77ab6ceb67ce8d92d8ae0
Amir Motefaker <amotef@gmail.com>
2022-06-19 13:35:19 +0430

    Update trim_whitespace_string.py
    Added solution 2 with regular expression

--- trim_whitespace_string.py
+++ trim_whitespace_string.py
@@ -3,10 +3,29 @@
 # Trim Whitespace From a String
 
 # Solution 1 - Using strip()
-my_string = " Python "
+# my_string = " Python "
 
-print(my_string.strip())
+# print(my_string.strip())
 
 # Output:
 # Python
 
+# my_string = " \nPython "
+
+# print(my_string.strip(" "))
+
+
+# Output:
+# Python
+
+
+# Solution 2 - Using regular expression
+import re
+
+my_string  = " Hello Python "
+output = re.sub(r'^\s+|\s+$', '', my_string)
+
+print(output)
+
+# Output:
+# Hello Python
