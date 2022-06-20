88f9034f7dab08635d08227c333f69fbd7eb48f3
Amir Motefaker <amotef@gmail.com>
2022-06-20 17:20:38 +0430

    Update rock_paper_scissors.py
    Added solution 4. solution 3 is the BEST.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -74,7 +74,64 @@
 
 
 
-# Solution 3 - CodingYar with AI and Result
+# Solution 3 - CodingYar with AI and Result **BEST**
+# import random
+
+# choices = ['r', 'p', 's']
+
+# choice_meaning = {
+#     'r': 'Rock',
+#     's': 'Scissors',
+#     'p': 'Paper'
+# }
+
+# user_score = 0
+# ai_score = 0
+
+# i = 0
+
+# while i < 5:
+#     user_choice = input('Select from Rock, Paper, Scissors: (r, p, s) ')
+
+#     ai_choice = random.choice(choices)
+
+#     if user_choice in choices:
+#         print(f'Your choice is {choice_meaning[user_choice]}. AI choice is {choice_meaning[ai_choice]}.')
+#         # r > s - p > r - s > p
+#         if user_choice == ai_choice:
+#             print('Tie')
+#         elif user_choice == 'r' and ai_choice == 's':
+#             print('user+1!')
+#             user_score += 1
+#         elif user_choice == 'p' and ai_choice == 'r':
+#             print('user+1!')
+#             user_score += 1
+#         elif user_choice == 's' and ai_choice == 'p':
+#             print('user+1!')
+#             user_score += 1
+#         else:
+#             print('AI+1!')
+#             ai_score += 1
+#     else:
+#         print('Invalid input')
+#         i -= 1
+
+#     print(f'User score: {user_score} / AI score: {ai_score}')
+    
+#     print('\n', '-'*15, '\n')
+    
+#     i += 1
+
+# if user_score > ai_score:
+#     print(f'User won with score: {user_score}')
+# elif user_score < ai_score:
+#     print(f'AI won with score: {ai_score}')
+# else:
+#     print(f'It\'s a tie. score: {user_score}')
+
+
+
+# Solution 4 - CodingYar with AI and Result
 import random
 
 choices = ['r', 'p', 's']
@@ -87,10 +144,9 @@ choice_meaning = {
 
 user_score = 0
 ai_score = 0
+final_number = 5
 
-i = 0
-
-while i < 5:
+while True:
     user_choice = input('Select from Rock, Paper, Scissors: (r, p, s) ')
 
     ai_choice = random.choice(choices)
@@ -114,18 +170,17 @@ while i < 5:
             ai_score += 1
     else:
         print('Invalid input')
-        i -= 1
 
     print(f'User score: {user_score} / AI score: {ai_score}')
     
+    if user_score == final_number or ai_score == final_number:
+        break
+
     print('\n', '-'*15, '\n')
     
-    i += 1
-
-if user_score > ai_score:
+    
+if user_score == final_number:
     print(f'User won with score: {user_score}')
-elif user_score < ai_score:
-    print(f'AI won with score: {ai_score}')
 else:
-    print(f'It\'s a tie. score: {user_score}')
+    print(f'AI won with score: {ai_score}')
 
