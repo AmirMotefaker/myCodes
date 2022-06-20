bd1dba314ad5eb51e46f681cfd55a37e98f1faf1
Amir Motefaker <amotef@gmail.com>
2022-06-20 09:53:20 +0430

    Update sum_digits_numbers.py
    Adde solution 2

--- sum_digits_numbers.py
+++ sum_digits_numbers.py
@@ -2,6 +2,7 @@
 
 # Calculate the sum of digits of numbers
 
+# Solution 1
 # 1234 -> 10
 
 # 1234 % 10 -> 4
@@ -13,13 +14,33 @@
 # 1 % 10 -> 1
 # 1 // 10 -> 0
 
-user_input = int(input('Enter your number: '))
+# user_input = int(input('Enter your number: '))
+
+# result = 0
+
+# while user_input != 0:
+#     result += user_input % 10
+#     user_input = user_input // 10
+
+# print(result)
+
+# Output:
+# Enter your number: 1234
+# 10
+
+# Output:
+# Enter your number: 99991
+# 37
+
+
+
+# Solution 2
+user_input = input('Enter your number: ') 
 
 result = 0
 
-while user_input != 0:
-    result += user_input % 10
-    user_input = user_input // 10
+for i in user_input: # '1234'
+    result += int(i)
 
 print(result)
 
