475f843b8b9e8a2d168c50d223223bfd9af05e48
Amir Motefaker <amotef@gmail.com>
2022-06-20 17:10:41 +0430

    Update rock_paper_scissors.py
    Added Solution 3 - CodingYar with AI and Result

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -46,29 +46,86 @@
 
 # Solution 2 - CodingYar
 
+# import random
+
+# choices = ['r', 'p', 's']
+
+# choice_meaning = {
+#     'r' : 'Rock',
+#     'p': 'Paper',
+#     's': 'Scissors'
+# }
+
+# user_choice = input("Select from Rock, Paper, Scissors: (r, p, s) ")
+
+# ai_choice = random.choice(choices)
+
+# if user_choice in choices:
+#     print(f'Your choice is {choice_meaning[user_choice]}. AI choice is {choice_meaning[ai_choice]}.')
+#     # r > s - p > r - s > p
+#     if user_choice == ai_choice:
+#         print('Tie')
+#     elif (user_choice == 'r' and ai_choice == 's') or (user_choice == 'p' and ai_choice == 'r') or (user_choice == 's' and ai_choice == 'p'):
+#         print('You Won!')
+#     else:
+#         print('You Lost!')
+# else:
+#     print("Invalid Input")
+
+
+
+# Solution 3 - CodingYar with AI and Result
 import random
 
 choices = ['r', 'p', 's']
 
 choice_meaning = {
-    'r' : 'Rock',
-    'p': 'Paper',
-    's': 'Scissors'
+    'r': 'Rock',
+    's': 'Scissors',
+    'p': 'Paper'
 }
 
-user_choice = input("Select from Rock, Paper, Scissors: (r, p, s) ")
+user_score = 0
+ai_score = 0
 
-ai_choice = random.choice(choices)
+i = 0
 
-if user_choice in choices:
-    print(f'Your choice is {choice_meaning[user_choice]}. AI choice is {choice_meaning[ai_choice]}.')
-    # r > s - p > r - s > p
-    if user_choice == ai_choice:
-        print('Tie')
-    elif (user_choice == 'r' and ai_choice == 's') or (user_choice == 'p' and ai_choice == 'r') or (user_choice == 's' and ai_choice == 'p'):
-        print('You Won!')
+while i < 5:
+    user_choice = input('Select from Rock, Paper, Scissors: (r, p, s) ')
+
+    ai_choice = random.choice(choices)
+
+    if user_choice in choices:
+        print(f'Your choice is {choice_meaning[user_choice]}. AI choice is {choice_meaning[ai_choice]}.')
+        # r > s - p > r - s > p
+        if user_choice == ai_choice:
+            print('Tie')
+        elif user_choice == 'r' and ai_choice == 's':
+            print('user+1!')
+            user_score += 1
+        elif user_choice == 'p' and ai_choice == 'r':
+            print('user+1!')
+            user_score += 1
+        elif user_choice == 's' and ai_choice == 'p':
+            print('user+1!')
+            user_score += 1
+        else:
+            print('AI+1!')
+            ai_score += 1
     else:
-        print('You Lost!')
+        print('Invalid input')
+        i -= 1
+
+    print(f'User score: {user_score} / AI score: {ai_score}')
+    
+    print('\n', '-'*15, '\n')
+    
+    i += 1
+
+if user_score > ai_score:
+    print(f'User won with score: {user_score}')
+elif user_score < ai_score:
+    print(f'AI won with score: {ai_score}')
 else:
-    print("Invalid Input")
+    print(f'It\'s a tie. score: {user_score}')
 
