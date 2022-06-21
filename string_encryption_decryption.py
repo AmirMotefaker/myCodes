7a4629e340b75130b652979206abd924d239c60e
Amir Motefaker <amotef@gmail.com>
2022-06-21 11:51:55 +0430

    Update string_encryption_decryption.py
    Added String Decryption

--- string_encryption_decryption.py
+++ string_encryption_decryption.py
@@ -2,16 +2,26 @@
 
 # String Encryption and Decryption
 
+
 # String Encryption
-my_string = 'Hello. My name is Hadi.'
+my_string = input('Enter string: ')
 
 nums = []
 
 for char in my_string:
     nums.append(str(ord(char) + 12))
 
-print(','.join(nums))
+secret_string = ','.join(nums)
+print(secret_string)
+
+# secret_string = '84,113,120,120,123,58,44,89,133,44,122,109,121,113,44,117,127,44,84,109,112,117,58'
 
 
-secret_string = '84,113,120,120,123,58,44,89,133,44,122,109,121,113,44,117,127,44,84,109,112,117,58'
+# String Decryption
+string_list = []
+
+for num_str in secret_string.split(','):
+    string_list.append(chr(int(num_str)-12))
+
+print(''.join(string_list))
 
