1b4a472b854fb8066da7d68da79ba3bbdb7f4c95
Amir Motefaker <amotef@gmail.com>
2022-06-21 08:58:45 +0430

    Create nested_loop.py

---
+++ nested_loop.py
@@ -0,0 +1,44 @@
+# Code by amotef@gmail.com
+
+# Nested Loops
+
+for i in range(3):
+    print('i loop start')
+    
+    for j in range(4):
+        print('j loop start')
+        print(i, j)
+
+    print('-'*10)
+
+# Output:
+# i loop start
+# j loop start
+# 0 0
+# j loop start
+# 0 1
+# j loop start
+# 0 2
+# j loop start
+# 0 3
+# ----------
+# i loop start
+# j loop start
+# 1 0
+# j loop start
+# 1 1
+# j loop start
+# 1 2
+# j loop start
+# 1 3
+# ----------
+# i loop start
+# j loop start
+# 2 0
+# j loop start
+# 2 1
+# j loop start
+# 2 2
+# j loop start
+# 2 3
+# ----------
