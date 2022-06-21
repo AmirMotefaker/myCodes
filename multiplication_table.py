beb8090f33df9668951f561e5ed2bd698fed6881
Amir Motefaker <amotef@gmail.com>
2022-06-21 09:13:23 +0430

    Update multiplication_table.py
    Added solution 2 - the BEST

--- multiplication_table.py
+++ multiplication_table.py
@@ -4,11 +4,31 @@
 
 # Multiplication table (from 1 to 10) in Python
 
-num = 13
 
-# To take input from the user
-# num = int(input("Display multiplication table of? "))
+# Solution 1 
+# num = 13
+# # To take input from the user
+# # num = int(input("Display multiplication table of? "))
 
-# Iterate 10 times from i = 1 to 10
-for i in range(1, 11):
-   print(num, 'x', i, '=', num*i)
+# # Iterate 10 times from i = 1 to 10
+# for i in range(1, 11):
+#    print(num, 'x', i, '=', num*i)
+
+
+
+# Solution 2 - CodingYar
+for i in range(1, 10):
+   print()
+   for j in range(1, 10):
+      print(f'{i*j:3}', end=' ')
+
+# Output:
+#   1   2   3   4   5   6   7   8   9 
+#   2   4   6   8  10  12  14  16  18 
+#   3   6   9  12  15  18  21  24  27 
+#   4   8  12  16  20  24  28  32  36 
+#   5  10  15  20  25  30  35  40  45 
+#   6  12  18  24  30  36  42  48  54 
+#   7  14  21  28  35  42  49  56  63 
+#   8  16  24  32  40  48  56  64  72
+#   9  18  27  36  45  54  63  72  81
