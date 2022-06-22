2fe03a71a7dd6dacec51b7a8b635ed828c8ce000
Amir Motefaker <amotef@gmail.com>
2022-06-22 12:02:34 +0430

    Create find_file_in_directory.py

---
+++ find_file_in_directory.py
@@ -0,0 +1,30 @@
+# Code by @AmirMotefaker
+
+# Find All File with .txt Extension Present Inside a Directory
+
+# Solution 1 - Using glob
+import glob, os
+
+os.chdir("mydir")
+for file in glob.glob("*.txt"):
+    print(file)
+
+# Output:
+# a.txt
+# b.txt
+# c.txt
+
+
+
+# Solution 2 - Using os
+import os
+
+for file in os.listdir("mydir"):
+    if file.endswith(".txt"):
+        print(os.path.join("mydir", file))
+
+
+# Output:
+# mydir\a.txt
+# mydir\b.txt
+# mydir\c.txt
