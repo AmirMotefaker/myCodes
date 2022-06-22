577cb4e63089ac9a347bf97afbb6ff62d6e310b6
Amir Motefaker <amotef@gmail.com>
2022-06-22 09:39:13 +0430

    Update return_multiple_values.py
    Added Solution 2 -  Using a dictionary

--- return_multiple_values.py
+++ return_multiple_values.py
@@ -2,7 +2,7 @@
 
 # Return Multiple Values From a Function
 
-# Solution 1: Return values using comma
+# Solution 1 - Return values using comma
 def name():
     return "Amir","Motefaker"
 
@@ -17,3 +17,19 @@ print(name_1, name_2)
 # Output:
 # ('Amir', 'Motefaker')
 # Amir Motefaker
+
+
+
+# Solution 2 -  Using a dictionary
+def name():
+    n1 = "Amir"
+    n2 = "Motefaker"
+
+    return {1:n1, 2:n2}
+
+names = name()
+print(names)
+
+
+# Output:
+# {1: 'Amir', 2: 'Motefaker'}
