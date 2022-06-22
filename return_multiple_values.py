e8b99eeafff76cc979877ecc9996b4c332551708
Amir Motefaker <amotef@gmail.com>
2022-06-22 09:36:53 +0430

    Create return_multiple_values.py

---
+++ return_multiple_values.py
@@ -0,0 +1,19 @@
+# Code by @AmirMotefaker
+
+# Return Multiple Values From a Function
+
+# Solution 1: Return values using comma
+def name():
+    return "Amir","Motefaker"
+
+# print the tuple with the returned values
+print(name())
+
+# get the individual items
+name_1, name_2 = name()
+print(name_1, name_2)
+
+
+# Output:
+# ('Amir', 'Motefaker')
+# Amir Motefaker
