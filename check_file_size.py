b49eb622a2a7f7038f6fd484ad0e4f7aa793a2d2
Amir Motefaker <amotef@gmail.com>
2022-06-23 09:28:59 +0430

    Create check_file_size.py

---
+++ check_file_size.py
@@ -0,0 +1,12 @@
+# Code by @AmirMotefaker
+
+# Iterate Through Two Lists in Parallel
+
+# Solution 1 - Using os module
+import os
+
+file_stat = os.stat('my_file.txt')
+print(file_stat.st_size)
+
+# Output:
+# 44
