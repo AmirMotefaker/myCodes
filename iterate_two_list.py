d4f0670d535d3a34f3f61cc1dbbcb86d5ffb8138
Amir Motefaker <amotef@gmail.com>
2022-06-23 09:19:48 +0430

    Update iterate_two_list.py
    Added solution 2 with Using itertools

--- iterate_two_list.py
+++ iterate_two_list.py
@@ -14,3 +14,32 @@ for i, j in zip(list_1, list_2):
 # 2 b
 # 3 c
 
+
+
+# Solution 2 - Using itertools 
+import itertools
+
+list_1 = [1, 2, 3, 4]
+list_2 = ['a', 'b', 'c']
+
+# loop until the short loop stops
+for i,j in zip(list_1,list_2):
+    print(i,j)
+
+print("\n")
+
+# loop until the longer list stops
+for i,j in itertools.zip_longest(list_1,list_2):
+    print(i,j)
+
+
+# Output:
+# 1 a
+# 2 b
+# 3 c
+
+
+# 1 a
+# 2 b
+# 3 c
+# 4 None
