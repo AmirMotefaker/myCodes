31c255d996b0ff961d6a5bdb64c02e2615cfc997
Amir Motefaker <amotef@gmail.com>
2022-06-23 09:11:45 +0430

    Update full_path_directory.py
    Added solution 2 with Using os module

--- full_path_directory.py
+++ full_path_directory.py
@@ -2,6 +2,8 @@
 
 # Get the Full Path of the Current Working Directory
 
+
+# Solution 1 - Using pathlib module
 import pathlib
 
 # path of the given file
@@ -9,6 +11,20 @@ print(pathlib.Path("my_file.txt").parent.absolute())
 
 # current working directory
 print(pathlib.Path().absolute())
+
+# Output:
+# E:\A.Motefaker\مدارک\Python\MyCode
+# E:\A.Motefaker\مدارک\Python\MyCode
+
+
+# Solution 2 - Using os module
+import os
+
+# path of the given file
+print(os.path.dirname(os.path.abspath("my_file.txt")))
+
+# current working directory
+print(os.path.abspath(os.getcwd()))
 
 # Output:
 # E:\A.Motefaker\مدارک\Python\MyCode
