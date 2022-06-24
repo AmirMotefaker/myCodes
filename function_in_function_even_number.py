85e18f9e5851ef597eaea65b45c313c6da81ed80
Amir Motefaker <amotef@gmail.com>
2022-06-24 13:25:48 +0430

    Create function_in_function_even_number.py

---
+++ function_in_function_even_number.py
@@ -0,0 +1,24 @@
+# Code by @AmirMotefaker
+
+# Function in Function Even Number
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
+def return_all_even_numbers_in_list(num_list):
+    result = []
+
+    for num in num_list:
+        if is_even(num):
+            result.append(num)
+
+    return(result)
+
+
+print(return_all_even_numbers_in_list([1,2,3,4,6,-123,8,3,13,122,11,29,-2]))
+
+# Output:
+# [2, 4, 6, 8, 122, -2]
