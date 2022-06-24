33c14705395f90788fe89cf23271f73952cef845
Amir Motefaker <amotef@gmail.com>
2022-06-24 11:12:34 +0430

    Create tuples_unpacking.py

---
+++ tuples_unpacking.py
@@ -0,0 +1,15 @@
+# Code by @AmirMotefaker
+
+# Tuples Unpacking
+
+def calc_sum_sub(num1, num2):
+    result_sum = num1 + num2
+    result_sub = num1 - num2
+    return result_sum, result_sub
+
+result_sum, result_sub = calc_sum_sub(10, 4)
+
+print(result_sum, result_sub)
+
+# Output:
+# 14 6
