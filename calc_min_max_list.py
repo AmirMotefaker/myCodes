3d573d6046c41608e340a3fc2594fe4d541c2835
Amir Motefaker <amotef@gmail.com>
2022-06-24 11:45:13 +0430

    Create calc_min_max_list.py

---
+++ calc_min_max_list.py
@@ -0,0 +1,14 @@
+# Code by @AmirMotefaker
+
+# Calculate Min and Max in list
+
+def calc_min_max(num_list):
+    return min(num_list), max(num_list)
+
+
+result_min, result_max = calc_min_max([1,2,3,4,10,9,-2,-9])
+
+print(result_min, result_max)
+
+# Output:
+# -9 10
