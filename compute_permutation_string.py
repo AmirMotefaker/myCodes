5ca8d1ced94538418556950274e2f3fcdf28e912
Amir Motefaker <amotef@gmail.com>
2022-06-25 13:21:00 +0430

    Update compute_permutation_string.py
    Added solution 2 with Using itertools

--- compute_permutation_string.py
+++ compute_permutation_string.py
@@ -3,21 +3,21 @@
 # Compute all the Permutation of the String
 
 # Solution 1 - Using recursion
-def get_permutation(string, i=0):
+# def get_permutation(string, i=0):
 
-    if i == len(string):   	 
-        print("".join(string))
+#     if i == len(string):   	 
+#         print("".join(string))
 
-    for j in range(i, len(string)):
+#     for j in range(i, len(string)):
 
-        words = [c for c in string]
+#         words = [c for c in string]
    
-        # swap
-        words[i], words[j] = words[j], words[i]
+#         # swap
+#         words[i], words[j] = words[j], words[i]
    	 
-        get_permutation(words, i + 1)
+#         get_permutation(words, i + 1)
 
-print(get_permutation('amir'))
+# print(get_permutation('amir'))
 
 # Output:
 # amir
@@ -45,3 +45,18 @@ print(get_permutation('amir'))
 # raim
 # rami
 # None
+
+
+
+# Solution 2 - Using itertools
+from itertools import permutations
+
+words = [''.join(p) for p in permutations('amir')]
+
+print(words)
+
+# Output:
+# ['amir', 'amri', 'aimr', 'airm', 'armi', 'arim',
+#  'mair', 'mari', 'miar', 'mira', 'mrai', 'mria',
+#  'iamr', 'iarm', 'imar', 'imra', 'iram', 'irma',
+#  'rami', 'raim', 'rmai', 'rmia', 'riam', 'rima']
