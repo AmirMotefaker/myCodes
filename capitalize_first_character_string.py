87dee13842dc07dfb6a18db371851361111de1ec
Amir Motefaker <amotef@gmail.com>
2022-06-25 13:15:24 +0430

    Create capitalize_first_character_string.py

---
+++ capitalize_first_character_string.py
@@ -0,0 +1,11 @@
+# Code by @AmirMotefaker
+
+# Capitalize the First Character of a String
+
+# Solution 1 - Using list slicing
+my_string = "amir motefaker"
+
+print(my_string[0].upper() + my_string[1:])
+
+# Output:
+# Amir motefaker
