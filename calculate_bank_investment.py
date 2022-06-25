03e5325b3bea349d18baa4b0fd29e9b6761c4c52
Amir Motefaker <amotef@gmail.com>
2022-06-25 16:26:02 +0430

    Create calculate_bank_investment.py

---
+++ calculate_bank_investment.py
@@ -0,0 +1,23 @@
+# Code by @AmirMotefaker
+
+# Calculate Bank Investment
+
+# r = 0.2
+
+# 1000$ 
+# 1200$ = 1000 + (r * 1000) = 1.2 * 1000 =  1200
+# 1440$ = 1200 * (1 + r) = 1440
+def calc_bank_investment(initial_money, num_of_years, rate=0.2):
+    result = initial_money
+    
+    for i in range(num_of_years):
+        result *= (1 + rate)
+
+    return result
+
+print(calc_bank_investment(num_of_years=3, initial_money=1000))
+print(1440 * 1.2) # test
+
+# Output:
+# 1728.0
+# 1728.0
