e9afa78d0bd1f7498e590367507ebcb8fee5e9e9
Amir Motefaker <amotef@gmail.com>
2022-06-25 13:53:10 +0430

    Update remove_duplicate_element_from_list.py
    Added solution 2 with Remove the items that are duplicated in two lists

--- remove_duplicate_element_from_list.py
+++ remove_duplicate_element_from_list.py
@@ -9,3 +9,15 @@ print(list(set(list_1)))
 
 # Output:
 # [1, 2, 4, 6]
+
+
+
+# Solution 2 - Remove the items that are duplicated in two lists
+list_1 = [1, 2, 1, 4, 6]
+list_2 = [7, 8, 2, 1]
+
+print(list(set(list_1) ^ set(list_2)))
+
+# Output:
+[4, 6, 7, 8]
+
