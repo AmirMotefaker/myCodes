182039b4b88155acda5e1033daee055678776d45
Amir Motefaker <amotef@gmail.com>
2022-06-25 15:33:29 +0430

    Update unlimited_number_inputs_function.py
    Added (*args, **kwargs)

--- unlimited_number_inputs_function.py
+++ unlimited_number_inputs_function.py
@@ -2,15 +2,31 @@
 
 # Get an unlimited number of inputs in the function
 
-def sum_numbers(*args):
-    result_sum = 0
-    for n in args:
-        result_sum += n
+# def sum_numbers(*args):
+#     result_sum = 0
+#     for n in args:
+#         result_sum += n
 
-    return result_sum
+#     return result_sum
 
-print(sum_numbers(1,2,3,4,5,9))
+# print(sum_numbers(1,2,3,4,5,9))
 
 # Output:
 # 24
 
+
+def sum_numbers(*args, **kwargs):  # Keyword Arguments
+    # print(type(kwargs))
+    # <class 'dict'>
+    print(kwargs)
+    print(args)
+    print(kwargs.keys())
+    print(kwargs.values())
+
+sum_numbers(10, 1, 'hello', a='bye', b=10, c='night')
+
+# Output:
+# {'a': 'bye', 'b': 10, 'c': 'night'}
+# (10, 1, 'hello')
+# dict_keys(['a', 'b', 'c'])
+# dict_values(['bye', 10, 'night'])
