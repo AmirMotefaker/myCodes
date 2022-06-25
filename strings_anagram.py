fb32107df188bce493c4c215324c95aff7ac12eb
Amir Motefaker <amotef@gmail.com>
2022-06-25 12:57:47 +0430

    Update strings_anagram.py
    Added solution 3 with Count characters

--- strings_anagram.py
+++ strings_anagram.py
@@ -31,7 +31,7 @@
 
 
 
-# Solution 2
+# Solution 2 - count characters using one array
 NO_OF_CHARS = 256
  
 # function to check if two strings
@@ -77,7 +77,59 @@ else:
     print("The two strings are not anagram of each other")
      
      
-# This code is contributed by patel2127
+# Output:
+# The two strings are anagram of each other
+
+
+
+
+# Solution 3 - Count characters
+# Python program to check if two strings are anagrams of
+# each other
+NO_OF_CHARS = 256
+ 
+# Function to check whether two strings are anagram of
+# each other
+ 
+ 
+def areAnagram(str1, str2):
+ 
+    # Create two count arrays and initialize all values as 0
+    count1 = [0] * NO_OF_CHARS
+    count2 = [0] * NO_OF_CHARS
+ 
+    # For each character in input strings, increment count
+    # in the corresponding count array
+    for i in str1:
+        count1[ord(i)] += 1
+ 
+    for i in str2:
+        count2[ord(i)] += 1
+ 
+    # If both strings are of different length. Removing this
+    # condition will make the program fail for strings like
+    # "aaca" and "aca"
+    if len(str1) != len(str2):
+        return 0
+ 
+    # Compare count arrays
+    for i in range(NO_OF_CHARS):
+        if count1[i] != count2[i]:
+            return 0
+ 
+    return 1
+ 
+ 
+# Driver code
+str1 = "listen"
+str2 = "silent"
+ 
+# Function call
+if areAnagram(str1, str2):
+    print("The two strings are anagram of each other")
+else:
+    print("The two strings are not anagram of each other")
+ 
 
 # Output:
 # The two strings are anagram of each other
