cc8b560845d0902f9c894bd78a3866fd2ac434b7
Amir Motefaker <amotef@gmail.com>
2022-06-25 14:15:36 +0430

    Create function_inputs.py

---
+++ function_inputs.py
@@ -0,0 +1,16 @@
+# Code by @AmirMotefaker
+
+# Function inputs
+
+def my_func(language, eye_color, age):
+    print('my language is {}'.format(language))
+    print('my eye color is {}'.format(eye_color))
+    print('my age is {}'.format(age))
+
+my_func(age =100, language = 'persian', eye_color='yellow')
+
+
+# Output:
+# my language is persian
+# my eye color is yellow
+# my age is 100
