3abe8f3e42468cf00dd924aecf314422f2ae1038
Amir Motefaker <amotef@gmail.com>
2022-06-26 10:17:46 +0430

    Update URL_shortener.py
    Added docstring

--- URL_shortener.py
+++ URL_shortener.py
@@ -2,10 +2,11 @@
 
 # URL Shortener
 
-import pyshorteners   # pyshorteners: A simple URL shortening API wrapper Python library.
+import pyshorteners
+   """ pyshorteners: A simple URL shortening API wrapper Python library. """
 long_url = input("Enter the URL to shorten: ")
  
-#TinyURL shortener service
+   """ TinyURL shortener service """
 type_tiny = pyshorteners.Shortener()
 short_url = type_tiny.tinyurl.short(long_url)
  
