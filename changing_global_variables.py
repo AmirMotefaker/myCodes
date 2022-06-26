a2d71d94de4938c413c24cd055f6abc2e0b63e91
Amir Motefaker <amotef@gmail.com>
2022-06-26 11:51:54 +0430

    Create changing_global_variables.py

---
+++ changing_global_variables.py
@@ -0,0 +1,19 @@
+# Code by @AmirMotefaker
+
+# Changing Global Variables
+
+a = 2
+ 
+def custom_function(num):
+    global a
+    a = a + num
+    print(a)
+
+print(a)
+custom_function(100)
+print(a)
+
+# Output:
+# 2
+# 102
+# 102
