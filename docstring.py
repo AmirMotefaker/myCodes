1c0f6cc8384cd3d287d4fc3c48d6b7f4dcaed23b
Amir Motefaker <amotef@gmail.com>
2022-06-26 10:00:33 +0430

    Create docstring.py

---
+++ docstring.py
@@ -0,0 +1,23 @@
+# Code by @AmirMotefaker
+
+# Docstring Conventions - PEP 257
+
+def calc_sum_two_numbers(num1, num2):
+    """ this function calculates sum of two input
+    num1: the first number
+    num2: the second number """
+
+    return num1 + num2
+
+# print(calc_sum_two_numbers(8,6))
+
+help(calc_sum_two_numbers)
+
+
+# Output:
+# Help on function calc_sum_two_numbers in module __main__:
+
+# calc_sum_two_numbers(num1, num2)
+#     this function calculates sum of two input
+#     num1: the first number
+#     num2: the second number
