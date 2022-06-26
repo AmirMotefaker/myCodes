14a13f3f2b680499b43c867a2476afac08cffdda
Amir Motefaker <amotef@gmail.com>
2022-06-26 10:20:35 +0430

    Create scope.py

---
+++ scope.py
@@ -0,0 +1,28 @@
+# Code by @AmirMotefaker
+
+# Scope in Python
+
+a = 1
+
+def my_function1():
+    a = 2
+    print(a)
+
+def my_function2():
+    a = 3
+    print(a)
+
+print(a)
+my_function1()
+print(a)
+my_function2()
+print(a)
+
+# Output:
+# 1
+# 2
+# 1
+# 3
+# 1
+
+
