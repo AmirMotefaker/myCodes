dd1a52f144b0d58cb0fe299fa309baaa4cbc03c2
Amir Motefaker <amotef@gmail.com>
2022-06-27 15:18:33 +0430

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,19 @@
+# Code by @AmirMotefaker
+
+# Factorial
+
+# 3! = 1 * 2 * 3 = 6
+# 5! = 1 * 2 * 3 * 4 * 5 = 125
+
+def factorial(n):
+    result = 1
+
+    for i in range(2, n+1):
+            result *= i
+
+    return result
+
+print(factorial(5))
+
+# Output:
+# 120
