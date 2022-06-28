18e0bee1106cf09f5a4ce8135b0d43f06cbeeeee
Amir Motefaker <amotef@gmail.com>
2022-06-28 14:02:36 +0430

    Update draw_diamond.py
    Added solution 3

--- draw_diamond.py
+++ draw_diamond.py
@@ -23,15 +23,31 @@
 
 
 
-# Solution 2 - Diamond
+# # Solution 2 - Diamond
+# def draw_diamond(num):
+
+#     for i in range(num//2+1):
+#         print(i*2 + 1)
+
+
+# draw_diamond(7) 
+
+
+# # Output:
+# # 1
+# # 3
+# # 5
+# # 7
+
+    
+
+# Solution 3 - Diamond
 def draw_diamond(num):
-
-    for i in range(num//2+1):
-        print(i*2 + 1)
-
+    for i in range(num):
+        if i < num / 2:
+            print(i*2 + 1)
 
 draw_diamond(7) 
-
 
 # Output:
 # 1
