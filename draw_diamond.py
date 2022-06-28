dc72b1e0b81bd5a0163526520cc5e1cd96b11731
Amir Motefaker <amotef@gmail.com>
2022-06-28 14:22:48 +0430

    Update draw_diamond.py
    Added solution 6

--- draw_diamond.py
+++ draw_diamond.py
@@ -79,22 +79,50 @@
 
 
 
-# Solution 5 - Diamond
+# # Solution 5 - Diamond
+# def draw_diamond(num):
+#     for i in range(num):
+#         if i < num / 2:
+#             print((i*2 + 1) * '*')
+#         else:
+#             print(((num-i)*2 - 1) * '*')
+
+
+# draw_diamond(7)
+
+# # Output:
+# # *
+# # ***
+# # *****
+# # *******
+# # *****
+# # ***
+# # *
+
+
+
+# Solution 6 - Diamond
+def print_star_line(number_of_stars, total_stars):
+    number_of_spaces = (total_stars - number_of_stars) // 2
+    print(f"{' ' * number_of_spaces}{'*' * number_of_stars}{' ' * number_of_spaces}")
+    
+
 def draw_diamond(num):
+    print()
     for i in range(num):
         if i < num / 2:
-            print((i*2 + 1) * '*')
+            print_star_line(i*2 + 1, num)
         else:
-            print(((num-i)*2 - 1) * '*')
+            print_star_line((num-i)*2 - 1, num)
 
 
 draw_diamond(7)
 
 # Output:
-# *
-# ***
-# *****
+#    *   
+#   ***
+#  *****
 # *******
-# *****
-# ***
-# *
+#  *****
+#   ***
+#    *
