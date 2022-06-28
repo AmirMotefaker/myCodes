1cd3ebb490a61f7e064f2a32e65b42ab00cbf18a
Amir Motefaker <amotef@gmail.com>
2022-06-28 12:37:53 +0430

    Update calculate_min_max_sum_len.py
    Added solution with Keyword arguments

--- calculate_min_max_sum_len.py
+++ calculate_min_max_sum_len.py
@@ -32,18 +32,112 @@
 
 
 
-# Solution 3
-def calc_min_max_sum_len(*args):
-    if len(args) == 0:
-        print('Error: At least enter one number')
-        return
+# # Solution 3
+# def calc_min_max_sum_len(*args):
+#     if len(args) == 0:
+#         print('Error: At least enter one number')
+#         return
 
+#     sum_result = 0
+#     max_result = args[0]
+#     min_result = args[0]
+#     len_result = 0
+
+#     for i in args:
+#         len_result += 1
+#         sum_result += i
+
+#         if i > max_result:
+#             max_result = i
+
+#         if i < min_result:
+#             max_result = i
+    
+#     return min_result, max_result, sum_result, len_result
+
+
+# print(calc_min_max_sum_len(1,2,3,4,5,6))
+
+# # Output:
+# # (1, 6, 21, 6)
+
+
+
+# # Solution 4 - Tuple Unpacking
+# def calc_min_max_sum_len(*args):
+#     if len(args) == 0:
+#         print('Error: At least enter one number')
+#         return
+
+#     sum_result = 0
+#     max_result = args[0]
+#     min_result = args[0]
+#     len_result = 0
+
+#     for i in args:
+#         len_result += 1
+#         sum_result += i
+
+#         if i > max_result:
+#             max_result = i
+
+#         if i < min_result:
+#             max_result = i
+    
+#     return min_result, max_result, sum_result, len_result
+
+
+# a,b,c,d = calc_min_max_sum_len(1,2,3,4,5)
+
+# print(a,b,c,d)
+
+# # Output:
+# # 1 5 15 5
+
+
+
+# # Solution 5 - Keyword arguments
+# def calc_min_max_sum_len(*args, list_of_nums):
+#     sum_result = 0
+#     max_result = args[0]
+#     min_result = args[0]
+#     len_result = 0
+
+#     for i in args:
+#         list_of_nums.append(i)
+#         len_result += 1
+#         sum_result += i
+
+#         if i > max_result:
+#             max_result = i
+
+#         if i < min_result:
+#             max_result = i
+    
+#     return min_result, max_result, sum_result, len_result
+
+# a= list()
+# print(calc_min_max_sum_len(1,2,3,4,5,10,list_of_nums=a))
+
+# print(a)
+
+# # Output:
+# # (1, 10, 25, 6)
+# # [1, 2, 3, 4, 5, 10]
+
+
+
+
+# Solution 6 - Keyword arguments - list_of_nums.clear()
+def calc_min_max_sum_len(*args, list_of_nums):
+    list_of_nums.clear()
     sum_result = 0
     max_result = args[0]
     min_result = args[0]
     len_result = 0
 
     for i in args:
+        list_of_nums.append(i)
         len_result += 1
         sum_result += i
 
@@ -55,8 +149,11 @@ def calc_min_max_sum_len(*args):
     
     return min_result, max_result, sum_result, len_result
 
+a= [0,0,0]
+print(calc_min_max_sum_len(1,2,3,4,5,10,list_of_nums=a))
 
-print(calc_min_max_sum_len(1,2,3,4,5,6))
+print(a)
 
 # Output:
-# (1, 6, 21, 6)
+# (1, 10, 25, 6)
+# [1, 2, 3, 4, 5, 10]
