98b14e672f0575198944d5cfe59775259e6c2b73
Amir Motefaker <amotef@gmail.com>
2022-06-28 13:29:50 +0430

    Update find_divisors.py
    Fix error

--- find_divisors.py
+++ find_divisors.py
@@ -6,7 +6,7 @@ def divisors(num):
     all_div = [1]
 
     for i in range(2, num+1):
-        if num % 2 == 0:
+        if num % i == 0:
             all_div.append(i)
 
     return all_div
@@ -15,4 +15,4 @@ print(divisors(16))
 
 
 # Output:
-# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+# [1, 2, 4, 8, 16]
