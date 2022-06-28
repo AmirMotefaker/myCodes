b3a934fd4c1f0576402ac9568e98511dd56d11d5
Amir Motefaker <amotef@gmail.com>
2022-06-28 13:59:52 +0430

    Update draw_diamond.py
    Added solution 2

--- draw_diamond.py
+++ draw_diamond.py
@@ -2,20 +2,39 @@
 
 # Draw Diamond
 
+# # Solution 1
+# def draw_diamond(num):
+#     for i in range(1, num, 2):
+#         print(i)
+
+#     for i in range(0, num, 2):
+#         print(num-i)
+
+# draw_diamond(7)
+
+# # Output:
+# # 1
+# # 3
+# # 5
+# # 7
+# # 5
+# # 3
+# # 1
+
+
+
+# Solution 2 - Diamond
 def draw_diamond(num):
-    for i in range(1, num, 2):
-        print(i)
 
-    for i in range(0, num, 2):
-        print(num-i)
+    for i in range(num//2+1):
+        print(i*2 + 1)
 
-draw_diamond(7)
+
+draw_diamond(7) 
+
 
 # Output:
 # 1
 # 3
 # 5
 # 7
-# 5
-# 3
-# 1
