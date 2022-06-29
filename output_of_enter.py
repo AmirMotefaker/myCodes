6206ba07f49a33781db578764ff03d3c84ec87e3
Amir Motefaker <amotef@gmail.com>
2022-06-29 12:53:55 +0430

    Create output_of_enter.py

---
+++ output_of_enter.py
@@ -0,0 +1,20 @@
+# Code by @AmirMotefaker
+
+# Output of Enter 
+
+user_input = input('Enter something: ')
+
+#print(f'--{user_input}--')
+
+if user_input == ' ':
+    print('space')
+elif user_input == '':
+    print('empty')
+elif user_input == '\n':
+    print('nnnnn')
+else:
+    print('something else')
+
+# Output:
+# Enter something: 
+# empty
