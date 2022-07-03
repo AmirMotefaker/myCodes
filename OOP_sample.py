feea0845f970f87d62c53147e694db7d99df8d63
Amir Motefaker <amotef@gmail.com>
2022-07-03 12:51:56 +0430

    Update OOP_sample.py
    Added solution 2

--- OOP_sample.py
+++ OOP_sample.py
@@ -2,17 +2,40 @@
 
 # object-oriented Programming (OOPs)
 
-class Car:
-    def __init__(self, car_color):
-        self.color = car_color  # instance attribute
+# # Solution 1
+# class Car:
+#     def __init__(self, car_color):
+#         self.color = car_color  # instance attribute
 
-car1= Car('blue')  # instance
-car2= Car('yellow')
-car3= Car('red')   # instance
+# car1= Car('blue')  # instance
+# car2= Car('yellow')
+# car3= Car('red')   # instance
+
+# print(car1.color)
+# print(car2.color)
+
+# # Output:
+# # blue
+# # yellow
+
+
+# Solution 2
+class Car:
+    def __init__(self, color, model):  # constructor
+        self.color = color  # instance attribute
+        self.model = model
+
+car1= Car('blue', 'BMW')  # instance
+car2= Car('yellow', 'AUDI')
+car3= Car('red', 'BMW')   # instance
 
 print(car1.color)
+print(car1.model)
 print(car2.color)
+print(car2.model)
 
 # Output:
 # blue
+# BMW
 # yellow
+# AUDI
