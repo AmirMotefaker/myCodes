ca1241cea2376eb5dde0cbb7eceb2eb69d0b449a
Amir Motefaker <amotef@gmail.com>
2022-07-03 13:54:32 +0430

    Update OOP_sample.py
    Added solution 6 with self method

--- OOP_sample.py
+++ OOP_sample.py
@@ -7,9 +7,9 @@
 #     def __init__(self, car_color):
 #         self.color = car_color  # instance attribute
 
-# car1= Car('blue')  # instance
-# car2= Car('yellow')
-# car3= Car('red')   # instance
+# car1 = Car('blue')  # instance
+# car2 = Car('yellow')
+# car3 = Car('red')   # instance
 
 # print(car1.color)
 # print(car2.color)
@@ -25,9 +25,9 @@
 #         self.color = color  # instance attribute
 #         self.model = model
 
-# car1= Car('blue', 'BMW')  # instance
-# car2= Car('yellow', 'AUDI')
-# car3= Car('red', 'BMW')   # instance
+# car1 = Car('blue', 'BMW')  # instance
+# car2 = Car('yellow', 'AUDI')
+# car3 = Car('red', 'BMW')   # instance
 
 # print(car1.color)
 # print(car1.model)
@@ -50,7 +50,7 @@
 #     def print_details(self):
 #         return f'color: {self.color} model: {self.model}'
 
-# car1= Car('blue', 'BMW') 
+# car1 = Car('blue', 'BMW') 
 
 # print(car1.print_details())
 
@@ -67,9 +67,9 @@
 #     def print_details(self):
 #         return f'color: {self.color}, model: {self.model}'
 
-# car1= Car('blue', 'BMW')  
-# car2= Car('yellow', 'AUDI')
-# car3= Car('red', 'BMW')   # 
+# car1 = Car('blue', 'BMW')  
+# car2 = Car('yellow', 'AUDI')
+# car3 = Car('red', 'BMW')   # 
 
 # print(car1.print_details())
 # print(car2.print_details())
@@ -81,19 +81,38 @@
 # # color: red, model: BMW
 
 
-# Solution 5
+# # Solution 5
+# class Car:
+#     def __init__(self, color, model):  # constructor
+#         self.color = color  
+#         self.model = model
+
+#     def print_details(self):
+#         return f'color: {self.color}, model: {self.model}'
+
+# car1 = Car('blue', 'BMW')  
+# print(car1.color, car1.model)
+# print(car1.print_details())
+
+# # Output:
+# # blue BMW
+# # color: blue, model: BMW
+
+
+# Solution 6 - self
 class Car:
     def __init__(self, color, model):  # constructor
         self.color = color  
         self.model = model
 
-    def print_details(self):
-        return f'color: {self.color}, model: {self.model}'
+    def print_details(self, a):
+        return f'color: {self.color}, model: {self.model} {a}'
 
-car1= Car('blue', 'BMW')  
-print(car1.color, car1.model)
-print(car1.print_details())
+
+car1 = Car('blue', 'BMW')  
+print(car1.print_details(20))
+print(Car.print_details(car1, 20))
 
 # Output:
-# blue BMW
-# color: blue, model: BMW
+# color: blue, model: BMW 20
+# color: blue, model: BMW 20
