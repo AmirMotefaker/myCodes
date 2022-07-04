eb72aab4a17defbddcddbd49fb0c3c91b5156b71
Amir Motefaker <amotef@gmail.com>
2022-07-04 09:58:28 +0430

    Update OOP_circle.py
    Added solution 4 with calculate Area and circumference

--- OOP_circle.py
+++ OOP_circle.py
@@ -55,7 +55,37 @@
 
 
 
-# Solution 3 - circumference
+# # Solution 3 - circumference
+# class Circle:
+#     pi = 3.14  # Class Attribute
+
+#     def __init__(self, r):
+#         self.r = r
+
+#     def calc_diameter(self):
+#         return self.r * 2
+
+#     def calc_area(self):
+#         # pi * (r ** 2)
+#         return self.pi * (self.r ** 2)
+
+#     def calc_circumference(self):
+#         # 2 * pi * r
+#         return 2 * self.pi * self.r 
+
+# circle1 = Circle(14)
+
+# print(circle1.calc_diameter())
+# print(circle1.calc_area())
+# print(circle1.calc_circumference())
+
+# # Output:
+# # 28
+# # 615.44
+# # 87.92
+
+
+# Solution 4 - Area and circumference
 class Circle:
     pi = 3.14  # Class Attribute
 
@@ -66,20 +96,16 @@ class Circle:
         return self.r * 2
 
     def calc_area(self):
-        # pi * (r ** 2)
         return self.pi * (self.r ** 2)
 
     def calc_circumference(self):
-        # 2 * pi * r
-        return 2 * self.pi * self.r 
+        return 2 * Circle.pi * self.r 
 
-circle1 = Circle(14)
-
-print(circle1.calc_diameter())
+circle1 = Circle(10)
 print(circle1.calc_area())
-print(circle1.calc_circumference())
+circle1.pi = 3.1415
+print(circle1.calc_area())
 
 # Output:
-# 28
-# 615.44
-# 87.92
+# 314.0
+# 314.15000000000003
