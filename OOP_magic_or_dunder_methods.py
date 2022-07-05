eb8970ac928257ff392c5549fdeab16eb725ee87
Amir Motefaker <amotef@gmail.com>
2022-07-05 15:35:47 +0430

    Update OOP_magic_or_dunder_methods.py
    Adde solution 2 with Magic or Dunder Methods

--- OOP_magic_or_dunder_methods.py
+++ OOP_magic_or_dunder_methods.py
@@ -2,16 +2,35 @@
 
 # OOP - Magic or Dunder Methods
 
-# Solution 1
+# # Solution 1
+# class Square:
+
+#     def __init__(self, side_length):
+#         self.s = side_length
+
+    
+# square = Square(10)
+
+# print(square.s)
+
+# # Output:
+# # 10
+
+
+
+# Solution 2
 class Square:
 
     def __init__(self, side_length):
         self.s = side_length
 
+    def __str__(self):
+        return f'Square with side length of {self.s}'
+
     
 square = Square(10)
 
-print(square.s)
+print(square)
 
 # Output:
-# 10
+# Square with side length of 10
