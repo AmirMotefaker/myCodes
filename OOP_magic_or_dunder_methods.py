0dcf08249455d70ed997a9112e467f2918f0d0ef
Amir Motefaker <amotef@gmail.com>
2022-07-05 15:32:57 +0430

    Create OOP_magic_or_dunder_methods.py

---
+++ OOP_magic_or_dunder_methods.py
@@ -0,0 +1,17 @@
+# Code by @AmirMotefaker
+
+# OOP - Magic or Dunder Methods
+
+# Solution 1
+class Square:
+
+    def __init__(self, side_length):
+        self.s = side_length
+
+    
+square = Square(10)
+
+print(square.s)
+
+# Output:
+# 10
