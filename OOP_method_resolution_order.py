ad241b69c5441b5a707895ddc1cdffb106aa11a1
Amir Motefaker <amotef@gmail.com>
2022-07-05 13:10:55 +0430

    Create OOP_method_resolution_order.py

---
+++ OOP_method_resolution_order.py
@@ -0,0 +1,67 @@
+# Code by @AmirMotefaker
+
+# Method Resolution Order(MRO)
+
+ 
+# Solution 1
+class Person:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def fullname(self): # title(): john -> John
+        return self.firstname.title() + ' ' + self.lastname.title()
+
+class Student(Person): # Inheritance
+    def __init__(self, firstname, lastname, major, uni):
+        super().__init__(firstname, lastname)
+        self.major = major
+        self.university = uni
+
+    def fullname(self):
+        return f'{self.firstname} {self.lastname}. I am studying in {self.major}.'
+
+    def education_info(self):
+        return f'{self.university}: {self.major} '
+
+
+class Teacher(Person):
+    def __init__(self, firstname, lastname, uni, department):
+        super().__init__(firstname, lastname)
+        self.uni = uni
+        self.dept = department 
+
+    def working_info(self):
+        return f'I am working in {self.uni} university at {self.dept} department.'
+
+help(Teacher)
+
+# Output:
+# Help on class Teacher in module __main__:
+
+# class Teacher(Person)
+#  |  Teacher(firstname, lastname, uni, department)
+#  |  
+#  |  Method resolution order:
+#  |      Teacher
+#  |      Person
+#  |      builtins.object
+#  |
+#  |  Methods defined here:
+#  |
+#  |  __init__(self, firstname, lastname, uni, department)
+#  |      Initialize self.  See help(type(self)) for accurate signature.
+#  |
+#  |  working_info(self)
+#  |
+#  |
+#  |  fullname(self)
+#  |
+#  |  ----------------------------------------------------------------------
+#  |  Data descriptors inherited from Person:
+#  |
+#  |  __dict__
+#  |      dictionary for instance variables (if defined)
+#  |
+#  |  __weakref__
+#  |      list of weak references to the object (if defined)
