d6b14adc739ad4b77f604cb23b889363f512d7f4
Amir Motefaker <amotef@gmail.com>
2022-07-05 15:08:50 +0430

    Update OOP_Polymorphism.py
    Added solution 4 for Area(Square1, Circle1, Circle2)

--- OOP_Polymorphism.py
+++ OOP_Polymorphism.py
@@ -67,7 +67,50 @@
 
 
 
-# Solution 3 - Area(Square, Circle)
+# # Solution 3 - Area(Square, Circle)
+# class Shape:  #  Abstract Class
+#     def __init__(self, kind, name):
+#         self.kind = kind
+#         self.name = name
+
+#     def area(self):
+#         raise NotImplementedError('All children of Shape class should redefine the area method.')
+
+# class Circle(Shape):
+#     pi = 3.14
+
+#     def __init__(self, kind, name, r):
+#         super().__init__(kind, name)
+#         self.radius = r
+
+#     def area(self):  # Override
+#         return Circle.pi * (self.radius ** 2) 
+
+# class Square(Shape):
+#     def __init__(self, kind, name, side_length):
+#         super().__init__(kind, name)
+#         self.side_length = side_length
+
+#     def area(self):
+#         return self.side_length ** 2
+
+
+# def show_area(s):
+#     print(s.area())
+
+# square = Square('square', 'a', 10 )
+# circle = Circle('circle', 'b', 10)
+
+# show_area(square)
+# show_area(circle)
+
+# # Output:
+# # 100
+# # 314.0
+
+
+
+# Solution 4 - Area(Square1, Circle1, Circle2)
 class Shape:  #  Abstract Class
     def __init__(self, kind, name):
         self.kind = kind
@@ -98,12 +141,16 @@ class Square(Shape):
 def show_area(s):
     print(s.area())
 
-square = Square('square', 'a', 10 )
-circle = Circle('circle', 'b', 10)
 
-show_area(square)
-show_area(circle)
+square1 = Square('square', 'a', 10 )
+circle1 = Circle('circle', 'b', 10)
+circle2 = Circle('circle', 'c', 4)
+
+for shape in [square1, circle1, circle2]:
+    show_area(shape)
+
 
 # Output:
 # 100
 # 314.0
+# 50.24
