42ebe70c81a35fec33b420a74783c52b3a2d6b50
Amir Motefaker <amotef@gmail.com>
2022-07-05 16:59:41 +0430

    Update OOP_magic_or_dunder_methods.py
    Added solution 5 with __del__

--- OOP_magic_or_dunder_methods.py
+++ OOP_magic_or_dunder_methods.py
@@ -59,7 +59,28 @@
 
 
 
-# Solution 4
+# # Solution 4
+# class Square:
+
+#     def __init__(self, side_length):
+#         self.s = side_length
+
+#     def __str__(self):
+#         return f'Square with side length of {self.s}'
+
+#     def __len__(self):
+#         return 4 * self.s
+    
+# square = Square(10)
+# #s1 = Square(99)
+# print(len(square))
+
+# # Output:
+# # 40
+
+
+
+# Solution 5
 class Square:
 
     def __init__(self, side_length):
@@ -70,10 +91,13 @@ class Square:
 
     def __len__(self):
         return 4 * self.s
+
+    def __del__(self):
+        print( f'Square with {self.s} side length is deleted.')
+
     
 square = Square(10)
-#s1 = Square(99)
-print(len(square))
+del square
 
 # Output:
-# 40
+# Square with 10 side length is deleted.
