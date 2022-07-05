43e76f08756adb99ee769bba0407dcecfc4a14d9
Amir Motefaker <amotef@gmail.com>
2022-07-05 08:36:29 +0430

    Update OOP_Inheritance.py
    Remove # from codes

--- OOP_Inheritance.py
+++ OOP_Inheritance.py
@@ -3,20 +3,20 @@
 # OOP - Inheritance 
 
 # # Solution 1
-# class Student:
-#     def __init__(self, firstname, lastname):
-#         self.firstname = firstname
-#         self.lastname = lastname
+class Student:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
 
-#     def who_am_i(self):
-#         return f'{self.firstname} {self.lastname}'
+    def who_am_i(self):
+        return f'{self.firstname} {self.lastname}'
 
 
-# john_stu = Student('john', 'doe')
-# david_stu = Student('david', 'stuart')
+john_stu = Student('john', 'doe')
+david_stu = Student('david', 'stuart')
 
-# print(john_stu.who_am_i())
-# print(david_stu.who_am_i())
+print(john_stu.who_am_i())
+print(david_stu.who_am_i())
 
 # # Output:
 # # john doe
@@ -25,29 +25,29 @@
 
 
 # # Solution 2
-# class Student:
-#     def __init__(self, firstname, lastname, major, uni):
-#         self.firstname = firstname
-#         self.lastname = lastname
-#         self.major = major
-#         self.university = uni
+class Student:
+    def __init__(self, firstname, lastname, major, uni):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.major = major
+        self.university = uni
 
-#     def who_am_i(self):
-#         return f'{self.firstname} {self.lastname}'
+    def who_am_i(self):
+        return f'{self.firstname} {self.lastname}'
 
-# class Teacher:
-#     def __init__(self, firstname, lastname, department, uni):
-#         self.firstname = firstname
-#         self.lastname = lastname
-#         self.department = department
-#         self.university = uni
+class Teacher:
+    def __init__(self, firstname, lastname, department, uni):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.department = department
+        self.university = uni
 
-#     def who_am_i(self):
-#         return f'{self.firstname} {self.lastname}. I am a teacher at {self.university} university.'
+    def who_am_i(self):
+        return f'{self.firstname} {self.lastname}. I am a teacher at {self.university} university.'
 
 
-# john = Teacher('john', 'doe', 'math', 'MIT')
-# print(john.who_am_i())
+john = Teacher('john', 'doe', 'math', 'MIT')
+print(john.who_am_i())
 
 # # Output:
 # # john doe. I am a teacher at MIT university.
@@ -55,20 +55,20 @@
 
 
 # # Solution 3
-# class Person:
-#     def __init__(self, firstname, lastname):
-#         self.firstname = firstname
-#         self.lastname = lastname
+class Person:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
 
-#     def fullname(self): # title(): john -> John
-#         return self.firstname.title() + ' ' + self.lastname.title()
+    def fullname(self): # title(): john -> John
+        return self.firstname.title() + ' ' + self.lastname.title()
 
-# class Student(Person): # Inheritance
-#     pass
+class Student(Person): # Inheritance
+    pass
 
 
-# ali_stu = Student('ali', 'alavi')
-# print(ali_stu.fullname())
+ali_stu = Student('ali', 'alavi')
+print(ali_stu.fullname())
 
 # # Output:
 # # Ali Alavi
@@ -76,22 +76,22 @@
 
 
 # # Solution 4
-# class Person:
-#     def __init__(self, firstname, lastname):
-#         self.firstname = firstname
-#         self.lastname = lastname
+class Person:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
 
-#     def fullname(self): # title(): john -> John
-#         return self.firstname.title() + ' ' + self.lastname.title()
+    def fullname(self): # title(): john -> John
+        return self.firstname.title() + ' ' + self.lastname.title()
 
-# class Student(Person): # Inheritance
-#     def __init__(self, firstname, lastname, major):
-#         Person.__init__(self, firstname, lastname)
-#         self.major = major
+class Student(Person): # Inheritance
+    def __init__(self, firstname, lastname, major):
+        Person.__init__(self, firstname, lastname)
+        self.major = major
 
 
-# ali_stu = Student('ali', 'alavi', 'Computer Engineering')
-# print(ali_stu.fullname())
+ali_stu = Student('ali', 'alavi', 'Computer Engineering')
+print(ali_stu.fullname())
 
 # # Output:
 # # Ali Alavi
@@ -99,23 +99,23 @@
 
 
 # # Solution 5
-# class Person:
-#     def __init__(self, firstname, lastname):
-#         self.firstname = firstname
-#         self.lastname = lastname
+class Person:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
 
-#     def fullname(self): # title(): john -> John
-#         return self.firstname.title() + ' ' + self.lastname.title()
+    def fullname(self): # title(): john -> John
+        return self.firstname.title() + ' ' + self.lastname.title()
 
-# class Student(Person): # Inheritance
-#     def __init__(self, firstname, lastname, major):
-#         #Person.__init__(self, firstname, lastname)
-#         super().__init__(firstname, lastname)
-#         self.major = major
+class Student(Person): # Inheritance
+    def __init__(self, firstname, lastname, major):
+        #Person.__init__(self, firstname, lastname)
+        super().__init__(firstname, lastname)
+        self.major = major
 
   
-# ali_stu = Student('ali', 'alavi', 'Computer Engineering')
-# print(ali_stu.fullname())
+ali_stu = Student('ali', 'alavi', 'Computer Engineering')
+print(ali_stu.fullname())
 
 # # Output:
 # # Ali Alavi
@@ -123,25 +123,25 @@
 
 
 # # Solution 6
-# class Person:
-#     def __init__(self, firstname, lastname):
-#         self.firstname = firstname
-#         self.lastname = lastname
+class Person:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
 
-#     def fullname(self): # title(): john -> John
-#         return self.firstname.title() + ' ' + self.lastname.title()
+    def fullname(self): # title(): john -> John
+        return self.firstname.title() + ' ' + self.lastname.title()
 
-# class Student(Person): # Inheritance
-#     def __init__(self, firstname, lastname, major):
-#         super().__init__(firstname, lastname)
-#         self.major = major
+class Student(Person): # Inheritance
+    def __init__(self, firstname, lastname, major):
+        super().__init__(firstname, lastname)
+        self.major = major
 
-#     def fullname(self):
-#         return f'{self.firstname} {self.lastname}. I am studying in {self.major}.'
+    def fullname(self):
+        return f'{self.firstname} {self.lastname}. I am studying in {self.major}.'
 
 
-# ali_stu = Student('ali', 'alavi', 'Computer Engineering')
-# print(ali_stu.fullname())
+ali_stu = Student('ali', 'alavi', 'Computer Engineering')
+print(ali_stu.fullname())
 
 # # Output:
 # # ali alavi. I am studying in Computer Engineering.
