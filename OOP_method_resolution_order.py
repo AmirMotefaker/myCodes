5fca56d4c0eec0240461f8cb61194a4bcda13dc3
Amir Motefaker <amotef@gmail.com>
2022-07-05 13:20:01 +0430

    Update OOP_method_resolution_order.py
    Added solution 3

--- OOP_method_resolution_order.py
+++ OOP_method_resolution_order.py
@@ -67,7 +67,75 @@
 
 
 
-# Solution 2
+# # Solution 2
+# class Person:
+#     def __init__(self, firstname, lastname):
+#         self.firstname = firstname
+#         self.lastname = lastname
+
+#     def fullname(self): # title(): john -> John
+#         return self.firstname.title() + ' ' + self.lastname.title()
+
+# class Student(Person): # Inheritance
+#     def __init__(self, firstname, lastname, major, uni):
+#         super().__init__(firstname, lastname)
+#         self.major = major
+#         self.university = uni
+
+#     def fullname(self):
+#         return f'{self.firstname} {self.lastname}. I am studying in {self.major}.'
+
+#     def education_info(self):
+#         return f'{self.university}: {self.major} '
+
+
+# class Teacher(Person):
+#     def __init__(self, firstname, lastname, uni, department):
+#         super().__init__(firstname, lastname)
+#         self.uni = uni
+#         self.dept = department 
+
+#     def working_info(self):
+#         return f'I am working in {self.uni} university at {self.dept} department.'
+
+# class Sample(Teacher):
+#     pass
+
+# help(Sample)
+
+# # Output:
+# # Help on class Sample in module __main__:
+
+# # class Sample(Teacher)
+# #  |  Sample(firstname, lastname, uni, department)
+# #  |
+# #  |  Method resolution order:
+# #  |      Sample
+# #  |      Teacher
+# #  |      Person
+# #  |      builtins.object
+# #  |
+# #  |  Methods inherited from Teacher:
+# #  |
+# #  |  __init__(self, firstname, lastname, uni, department)
+# #  |      Initialize self.  See help(type(self)) for accurate signature.
+# #  |
+# #  |  working_info(self)
+# #  |
+# #  |  fullname(self)
+# #  |
+# #  |  ----------------------------------------------------------------------
+# #  |  Data descriptors inherited from Person:
+# #  |
+# #  |  __dict__
+# #  |      dictionary for instance variables (if defined)
+# #  |
+# #  |  __weakref__
+# #  |      list of weak references to the object (if defined)
+
+
+
+# Solution 3
 class Person:
     def __init__(self, firstname, lastname):
         self.firstname = firstname
@@ -90,7 +158,7 @@ class Student(Person): # Inheritance
 
 
 class Teacher(Person):
-    def __init__(self, firstname, lastname, uni, department):
+    def __init__(self, firstname, lastname, uni, department='o'):
         super().__init__(firstname, lastname)
         self.uni = uni
         self.dept = department 
@@ -101,34 +169,8 @@ class Teacher(Person):
 class Sample(Teacher):
     pass
 
-help(Sample)
+s = Sample('s', 's', 's')
+print(s.working_info())
 
 # Output:
-# Help on class Sample in module __main__:
-
-# class Sample(Teacher)
-#  |  Sample(firstname, lastname, uni, department)
-#  |
-#  |  Method resolution order:
-#  |      Sample
-#  |      Teacher
-#  |      Person
-#  |      builtins.object
-#  |
-#  |  Methods inherited from Teacher:
-#  |
-#  |  __init__(self, firstname, lastname, uni, department)
-#  |      Initialize self.  See help(type(self)) for accurate signature.
-#  |
-#  |  working_info(self)
-#  |
-#  |  fullname(self)
-#  |
-#  |  ----------------------------------------------------------------------
-#  |  Data descriptors inherited from Person:
-#  |
-#  |  __dict__
-#  |      dictionary for instance variables (if defined)
-#  |
-#  |  __weakref__
-#  |      list of weak references to the object (if defined)
+# I am working in s university at o department.
