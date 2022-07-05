f9ba98e98260d60b52003edcde9388bd8e1ae02e
Amir Motefaker <amotef@gmail.com>
2022-07-05 15:51:16 +0430

    Update OOP_magic_or_dunder_methods.py
    Adde solution 4 with __len__

--- OOP_magic_or_dunder_methods.py
+++ OOP_magic_or_dunder_methods.py
@@ -37,7 +37,29 @@
 
 
 
-# Solution 3
+# # Solution 3
+# class Square:
+
+#     def __init__(self, side_length):
+#         self.s = side_length
+
+#     def __str__(self):
+#         return f'Square with side length of {self.s}'
+
+    
+# square = Square(10)
+# s1 = Square(99)
+
+# print(square)
+# print(s1)
+
+# # Output:
+# # Square with side length of 10
+# # Square with side length of 99 
+
+
+
+# Solution 4
 class Square:
 
     def __init__(self, side_length):
@@ -46,13 +68,12 @@ class Square:
     def __str__(self):
         return f'Square with side length of {self.s}'
 
+    def __len__(self):
+        return 4 * self.s
     
 square = Square(10)
-s1 = Square(99)
-
-print(square)
-print(s1)
+#s1 = Square(99)
+print(len(square))
 
 # Output:
-# Square with side length of 10
-# Square with side length of 99 
+# 40
