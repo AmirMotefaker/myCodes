39dbfc059524194c05c90973920298ee6efdae15
Amir Motefaker <amotef@gmail.com>
2022-07-05 15:38:46 +0430

    Update OOP_magic_or_dunder_methods.py
    Added solution 3

--- OOP_magic_or_dunder_methods.py
+++ OOP_magic_or_dunder_methods.py
@@ -18,7 +18,26 @@
 
 
 
-# Solution 2
+# # Solution 2
+# class Square:
+
+#     def __init__(self, side_length):
+#         self.s = side_length
+
+#     def __str__(self):
+#         return f'Square with side length of {self.s}'
+
+    
+# square = Square(10)
+
+# print(square)
+
+# # Output:
+# # Square with side length of 10
+
+
+
+# Solution 3
 class Square:
 
     def __init__(self, side_length):
@@ -29,8 +48,11 @@ class Square:
 
     
 square = Square(10)
+s1 = Square(99)
 
 print(square)
+print(s1)
 
 # Output:
 # Square with side length of 10
+# Square with side length of 99 
