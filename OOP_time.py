eff038cb5f1297b7371e61a898ec371806b048c8
Amir Motefaker <amotef@gmail.com>
2022-07-06 16:25:18 +0430

    Update OOP_time.py
    Added solution 6

--- OOP_time.py
+++ OOP_time.py
@@ -130,7 +130,47 @@
 
 
 
-# Solution 5 - Operator Overloading
+# # Solution 5 - Operator Overloading
+# import os
+# os.system('cls')
+
+# class Time:
+#     def __init__(self, hours, minutes, seconds):
+#         if hours > 24:
+#             raise ValueError('Hours number should be less than 23.')
+#         if minutes > 59:
+#             raise ValueError('Minutes number should be less than 60.')
+#         if seconds > 59:
+#             raise ValueError('Seconds number should be less than 60.')
+
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+
+#     def __str__(self):
+#         return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}'
+    
+#     def __add__(self, other):
+#         seconds = self.seconds + other.seconds # 46
+#         minutes = self.minutes + other.minutes + (seconds // 60) # 6
+#         hours = self.hours + other.hours + (minutes // 60) # 40
+#         return Time(hours%24, minutes%60, seconds%60)
+     
+
+# time1 = Time(17, 5, 42)
+# gym_time = Time(23, 1, 4)
+
+# time3 = time1 + gym_time
+# print(time3)
+# print(time3 + Time(3,0,0))
+
+# # Output:
+# # 16:06:46
+# # 19:06:46
+
+
+
+# Solution 6 - Operator Overloading
 import os
 os.system('cls')
 
@@ -157,13 +197,10 @@ class Time:
         return Time(hours%24, minutes%60, seconds%60)
      
 
-time1 = Time(17, 5, 42)
-gym_time = Time(23, 1, 4)
+time1 = Time(13, 57, 20)
+time2 = Time(13, 2, 40)
 
-time3 = time1 + gym_time
-print(time3)
-print(time3 + Time(3,0,0))
+print(time1 + time2)
 
 # Output:
-# 16:06:46
-# 19:06:46
+# 03:00:00
