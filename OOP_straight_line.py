13fd7fef173b04960dcabe5cafcf7f7570a53b9e
Amir Motefaker <amotef@gmail.com>
2022-07-06 12:24:37 +0430

    Update OOP_straight_line.py
    Remove # comment

--- OOP_straight_line.py
+++ OOP_straight_line.py
@@ -3,20 +3,20 @@
 # OOP - Construction of straight line class
 
 # # Solution 1
-# class Line:
-#     def __init__(self, x1, y1, x2, y2):
-#         self.x1 = x1
-#         self.y1 = y1
-#         self.x2 = x2
-#         self.y2 = y2
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
 
-#     def __str__(self):
-#         return f'Line: a=({self.x1},{self.y1}) b=({self.x2},{self.y2})'
+    def __str__(self):
+        return f'Line: a=({self.x1},{self.y1}) b=({self.x2},{self.y2})'
 
 
-# line1 = Line(1,1,3,3)
+line1 = Line(1,1,3,3)
 
-# print(line1)
+print(line1)
 
 # # Output:
 # # Line: a=(1,1) b=(3,3)
@@ -24,18 +24,18 @@
 
 
 # # Solution 2
-# class Line:
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
      
-#     def __str__(self):
-#         return f'Line: a=({self.a[0]},{self.a[1]}) b=({self.b[0]},{self.b[1]})'
+    def __str__(self):
+        return f'Line: a=({self.a[0]},{self.a[1]}) b=({self.b[0]},{self.b[1]})'
 
 
-# line1 = Line((1,1), (3,3))
+line1 = Line((1,1), (3,3))
 
-# print(line1)
+print(line1)
 
 # # Output:
 # # Line: a=(1,1) b=(3,3)
@@ -43,24 +43,24 @@
 
 
 # # Solution 3 - with length
-# class Line:
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
      
-#     def __str__(self):
-#         return f'Line: a=({self.a[0]},{self.a[1]}) b=({self.b[0]},{self.b[1]})'
+    def __str__(self):
+        return f'Line: a=({self.a[0]},{self.a[1]}) b=({self.b[0]},{self.b[1]})'
 
-#     def length(self):
-#         return ((self.b[1] - self.a[1]) ** 2 + (self.b[0] - self.a[0]) ** 2) ** 0.5
+    def length(self):
+        return ((self.b[1] - self.a[1]) ** 2 + (self.b[0] - self.a[0]) ** 2) ** 0.5
 
 
-# line1 = Line((0,0), (3,3))
+line1 = Line((0,0), (3,3))
 
-# print(line1.length())
+print(line1.length())
 
-# # for Test
-# print(18 ** 0.5)
+# for Test
+print(18 ** 0.5)
 
 # # Output:
 # # 4.242640687119285
@@ -69,24 +69,24 @@
 
 
 # # Solution 4 - Slope(shib)
-# class Line:
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
      
-#     def __str__(self):
-#         return f'Line: a=({self.a[0]},{self.a[1]}) b=({self.b[0]},{self.b[1]})'
+    def __str__(self):
+        return f'Line: a=({self.a[0]},{self.a[1]}) b=({self.b[0]},{self.b[1]})'
 
-#     def length(self):
-#         return ((self.b[1] - self.a[1]) ** 2 + (self.b[0] - self.a[0]) ** 2) ** 0.5
+    def length(self):
+        return ((self.b[1] - self.a[1]) ** 2 + (self.b[0] - self.a[0]) ** 2) ** 0.5
 
-#     def slope(self):
-#         return (self.b[1] - self.a[1]) / (self.b[0] - self.a[0])
+    def slope(self):
+        return (self.b[1] - self.a[1]) / (self.b[0] - self.a[0])
 
-# line1 = Line((0,0), (3,3))
+line1 = Line((0,0), (3,3))
 
-# print(line1.length())
-# print(line1.slope())
+print(line1.length())
+print(line1.slope())
 
 # # Output:
 # # 4.242640687119285
