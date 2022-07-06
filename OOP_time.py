60731e157cff2f9dfb8892a79b8e9d5605ed5e8c
Amir Motefaker <amotef@gmail.com>
2022-07-06 15:09:12 +0430

    Update OOP_time.py
    Added solution 2 with {self.hours:02}

--- OOP_time.py
+++ OOP_time.py
@@ -1,6 +1,30 @@
 # Code by @AmirMotefaker
 
 # OOP - Time (Operator Overloading)
+
+# # Solution 1
+# import os
+# os.system('cls')
+
+# class Time:
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+
+#     def __str__(self):
+#         return f'{self.hours}:{self.minutes}:{self.seconds}'
+    
+# time1 = Time(17, 5, 42)
+
+# print(time1)
+
+# # Output:
+# # 17:5:42
+
+
+
+# Solution 2
 import os
 os.system('cls')
 
@@ -11,12 +35,15 @@ class Time:
         self.seconds = seconds
 
     def __str__(self):
-        return f'{self.hours}:{self.minutes}:{self.seconds}'
+        return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}'
     
 time1 = Time(17, 5, 42)
+gym_time = Time(23, 1, 4)
 
 print(time1)
+print(gym_time)
+
 
 # Output:
-# 17:5:42
-   
+# 17:05:42
+# 23:01:04
