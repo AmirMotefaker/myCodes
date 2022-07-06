7864e48edf65792c4f7cecf5042b542f1794ceea
Amir Motefaker <amotef@gmail.com>
2022-07-06 17:09:42 +0430

    Udpate OOP_time.py
    Added solution 8 with wrong answer

--- OOP_time.py
+++ OOP_time.py
@@ -207,7 +207,48 @@
 
 
 
-# Solution 7 - Operator Overloading
+# # Solution 7 - Operator Overloading
+# import os
+# os.system('cls')
+
+# class Time:
+#     def __init__(self, hours, minutes, seconds):
+#         if hours > 24:
+#             raise ValueError('Hours number should be less than 23.')
+#         if minutes > 59:
+#             raise ValueError('Minutes number should be less than 60.')
+#         if seconds > 59:
+#             raise ValueError('Seconds number should be less than 60.')
+
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+
+#     def __str__(self):
+#         return f'{self.hours:02}:{self.minutes:02}:{self.seconds:02}'
+    
+#     def __add__(self, other):
+#         seconds = self.seconds + other.seconds 
+#         minutes = self.minutes + other.minutes + (seconds // 60) 
+#         hours = self.hours + other.hours + (minutes // 60) 
+#         return Time(hours%24, minutes%60, seconds%60)
+     
+#     def __gt__(self, other):
+#         return (self.hours > other.hours) \
+#             or (self.hours == other.hours and self.minutes > other.minutes) \
+#             or (self.hours == other.hours and self.minutes > other.minutes and self.seconds > other.seconds)
+
+# time1 = Time(13, 57, 20) # 13 * 60 * 60 + 57 * 60 + 20
+# time2 = Time(13, 2, 40)
+
+# print(time1 > time2)
+
+# # Output:
+# # True
+
+
+
+# Solution 8 - Operator Overloading - Magic or Dunder Methods
 import os
 os.system('cls')
 
@@ -238,10 +279,10 @@ class Time:
             or (self.hours == other.hours and self.minutes > other.minutes) \
             or (self.hours == other.hours and self.minutes > other.minutes and self.seconds > other.seconds)
 
-time1 = Time(13, 57, 20) # 13 * 60 * 60 + 57 * 60 + 20
-time2 = Time(13, 2, 40)
+time1 = Time(13, 1, 20) # 13 * 60 * 60 + 57 * 60 + 20
+time2 = Time(13, 1, 20)
 
-print(time1 > time2)
+print(time1 == time2)
 
 # Output:
-# True
+# False - That is wrong
