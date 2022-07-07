6229a2fa9ba37f97c8e6627545cd5a650f4ea038
Amir Motefaker <amotef@gmail.com>
2022-07-07 09:26:35 +0430

    Update OOP_bank_account.py
    Added solution 6 with __eq__(self, other)

--- OOP_bank_account.py
+++ OOP_bank_account.py
@@ -108,7 +108,47 @@
 
 
 
-# Solution 5 - __iadd__(self, other)
+# # Solution 5 - __iadd__(self, other)
+# import os
+# os.system('cls')
+
+# class Deposit:
+#     def __init__(self, name, amount =0):
+#         self.owner = name
+#         self.amount = amount
+
+#     def __str__(self):
+#         return f'Owner: {self.owner} | amount: {self.amount}'
+
+#     def __repr__(self):
+#         return f'{self.__class__.__name__}(name={self.owner}, amount={self.amount})'  # HardCode
+
+#     def __add__(self, other):
+#         name = f'{self.owner}+{other.owner}'
+#         amount = f'{self.amount}+{other.amount}'
+#         return Deposit(name, amount)
+
+#     def __iadd__(self, other):
+#         self.amount += other.amount
+#         other.amount = 0
+#         return self
+
+
+    
+# john_dep = Deposit('john', 1000)
+# david_dep = Deposit('david', 200)
+
+# john_dep += david_dep
+# print(john_dep)
+# print(david_dep)
+
+# # Output:
+# # Owner: john | amount: 1200
+# # Owner: david | amount: 0
+
+
+
+# Solution 6 - __eq__(self, other) 
 import os
 os.system('cls')
 
@@ -133,6 +173,8 @@ class Deposit:
         other.amount = 0
         return self
 
+    def __eq__(self, other):
+        return self.amount == other.amount
 
     
 john_dep = Deposit('john', 1000)
@@ -142,6 +184,9 @@ john_dep += david_dep
 print(john_dep)
 print(david_dep)
 
+print(john_dep == Deposit('', 1200))
+
 # Output:
 # Owner: john | amount: 1200
-# Owner: david | amount: 0
+# Owner: david | amount: 0  
+# True
