91e1ee3330fa29cbf262f771bf8182b8dc82c334
Amir Motefaker <amotef@gmail.com>
2022-07-07 09:45:09 +0430

    Update OOP_bank_account.py
    Added solution 8 with deposit(self, amount)

--- OOP_bank_account.py
+++ OOP_bank_account.py
@@ -193,7 +193,62 @@ print(john_dep == Deposit('', 1200))
 
 
 
-# Solution 7 - transfer(self, other, amount) 
+# # Solution 7 - transfer(self, other, amount) 
+# import os
+# os.system('cls')
+
+# class Deposit:
+#     def __init__(self, name, amount =0):
+#         self.owner = name
+#         self.amount = amount
+
+#     def __str__(self):
+#         return f'Owner: {self.owner} | amount: {self.amount}'
+
+#     def __repr__(self):
+#         return f'{self.__class__.__name__}(name={self.owner}, amount={self.amount})'  # HardCode
+
+#     def __add__(self, other):
+#         name = f'{self.owner}+{other.owner}'
+#         amount = f'{self.amount}+{other.amount}'
+#         return Deposit(name, amount)
+
+#     def __iadd__(self, other):
+#         self.amount += other.amount
+#         other.amount = 0
+#         return self
+
+#     def __eq__(self, other):
+#         return self.amount == other.amount
+
+#     def transfer(self, other, amount):
+#         if self.amount < amount:
+#             print('Not enough money.')
+#             return
+
+#         self.amount -= amount
+#         other.amount += amount
+
+    
+# john_dep = Deposit('john', 1000)
+# david_dep = Deposit('david', 200)
+
+# print(john_dep)
+# print(david_dep)
+
+# john_dep.transfer(david_dep, 200)
+# print(john_dep)
+# print(david_dep)
+
+# # Output:
+# # Owner: john | amount: 1000
+# # Owner: david | amount: 200
+# # Owner: john | amount: 800
+# # Owner: david | amount: 400
+
+
+
+# Solution 8 - deposit(self, amount)
 import os
 os.system('cls')
 
@@ -229,19 +284,38 @@ class Deposit:
         self.amount -= amount
         other.amount += amount
 
+    def deposit(self, amount):
+        if amount <= 0:
+            print('Deposit amount should be positive.')
+            return
+
+        self.amount += amount
+
     
 john_dep = Deposit('john', 1000)
 david_dep = Deposit('david', 200)
 
 print(john_dep)
 print(david_dep)
+print('-'*25)
 
 john_dep.transfer(david_dep, 200)
 print(john_dep)
 print(david_dep)
+print('-'*25)
+
+
+john_dep.deposit(2000)
+print(john_dep)
+print(david_dep)
+
 
 # Output:
 # Owner: john | amount: 1000
 # Owner: david | amount: 200
+# -------------------------
 # Owner: john | amount: 800
+# Owner: david | amount: 400
+# -------------------------
+# Owner: john | amount: 2800
 # Owner: david | amount: 400
