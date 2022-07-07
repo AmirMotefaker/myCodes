bc678ed8ad39c559eeb6f739c1622ff485f1a8c6
Amir Motefaker <amotef@gmail.com>
2022-07-07 09:23:09 +0430

    Update OOP_bank_account.py
    Added solution 5 with __iadd__(self, other)

--- OOP_bank_account.py
+++ OOP_bank_account.py
@@ -76,7 +76,39 @@
 
 
 
-# Solution 4 - __add__(self, other)
+# # Solution 4 - __add__(self, other)
+# import os
+# os.system('cls')
+
+# class Deposit:
+#     def __init__(self, name, amount =0):
+#         self.owner = name
+#         self.amount = amount
+
+#     def __str__(self):
+#         return f'Owner: {self.owner} | amount: {self.amount}'
+
+#     def __repr__(self):
+#         return f'{self.__class__.__name__}(name={self.owner}, amount={self.amount})'  # HardCode
+
+#     def __add__(self, other):
+#         name = f'{self.owner}+{other.owner}'
+#         amount = f'{self.amount}+{other.amount}'
+#         return Deposit(name, amount)
+
+
+    
+# john_dep = Deposit('john', 1000)
+# david_dep = Deposit('david', 200)
+
+# print(john_dep + david_dep)
+
+# # Output:
+# # Owner: john+david | amount: 1000+200
+
+
+
+# Solution 5 - __iadd__(self, other)
 import os
 os.system('cls')
 
@@ -96,12 +128,20 @@ class Deposit:
         amount = f'{self.amount}+{other.amount}'
         return Deposit(name, amount)
 
+    def __iadd__(self, other):
+        self.amount += other.amount
+        other.amount = 0
+        return self
+
 
     
 john_dep = Deposit('john', 1000)
 david_dep = Deposit('david', 200)
 
-print(john_dep + david_dep)
+john_dep += david_dep
+print(john_dep)
+print(david_dep)
 
 # Output:
-# Owner: john+david | amount: 1000+200
+# Owner: john | amount: 1200
+# Owner: david | amount: 0
