c80dfea3de988fdbacf95540a5384d4cfdfc1e5b
Amir Motefaker <amotef@gmail.com>
2022-07-07 09:10:24 +0430

    Update OOP_bank_account.py
    Added solution 2 with __repr__(self)

--- OOP_bank_account.py
+++ OOP_bank_account.py
@@ -2,6 +2,7 @@
 
 # OOP - Bank Account
 
+# Solution 1
 import os
 os.system('cls')
 
@@ -20,3 +21,29 @@ print (john_dep)
 
 # Output:
 # Owner: john | amount: 1000
+
+
+
+# Solution 2
+import os
+os.system('cls')
+
+class Deposit:
+    def __init__(self, name, amount =0):
+        self.owner = name
+        self.amount = amount
+
+    def __str__(self):
+        return f'Owner: {self.owner} | amount: {self.amount}'
+
+    def __repr__(self):
+        return f'Deposit(name={self.owner}, amount={self.amount})'
+
+    
+john_dep = Deposit('john', 1000)
+david_dep = Deposit('david', 200)
+
+print(repr(john_dep))
+
+# Output:
+# Deposit(name=john, amount=1000)
