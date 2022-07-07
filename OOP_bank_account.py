ed4300e0cb3db9fdc9c2b896581b6107d86cc541
Amir Motefaker <amotef@gmail.com>
2022-07-07 09:17:37 +0430

    Update OOP_bank_account.py
    Added solution 4 with __add__(self, other)

--- OOP_bank_account.py
+++ OOP_bank_account.py
@@ -2,29 +2,81 @@
 
 # OOP - Bank Account
 
-# Solution 1
-import os
-os.system('cls')
+# # Solution 1
+# import os
+# os.system('cls')
 
-class Deposit:
-    def __init__(self, name, amount =0):
-        self.owner = name
-        self.amount = amount
+# class Deposit:
+#     def __init__(self, name, amount =0):
+#         self.owner = name
+#         self.amount = amount
 
-    def __str__(self):
-        return f'Owner: {self.owner} | amount: {self.amount}'
+#     def __str__(self):
+#         return f'Owner: {self.owner} | amount: {self.amount}'
     
-john_dep = Deposit('john', 1000)
-david_dep = Deposit('david', 200)
+# john_dep = Deposit('john', 1000)
+# david_dep = Deposit('david', 200)
 
-print (john_dep)
+# print (john_dep)
 
-# Output:
-# Owner: john | amount: 1000
+# # Output:
+# # Owner: john | amount: 1000
 
 
 
-# Solution 2
+# # Solution 2
+# import os
+# os.system('cls')
+
+# class Deposit:
+#     def __init__(self, name, amount =0):
+#         self.owner = name
+#         self.amount = amount
+
+#     def __str__(self):
+#         return f'Owner: {self.owner} | amount: {self.amount}'
+
+#     def __repr__(self):
+#         return f'Deposit(name={self.owner}, amount={self.amount})'
+
+    
+# john_dep = Deposit('john', 1000)
+# david_dep = Deposit('david', 200)
+
+# print(repr(john_dep))
+
+# # Output:
+# # Deposit(name=john, amount=1000)
+
+
+
+# # Solution 3 - HardCode
+# import os
+# os.system('cls')
+
+# class Deposit:
+#     def __init__(self, name, amount =0):
+#         self.owner = name
+#         self.amount = amount
+
+#     def __str__(self):
+#         return f'Owner: {self.owner} | amount: {self.amount}'
+
+#     def __repr__(self):
+#         return f'{self.__class__.__name__}(name={self.owner}, amount={self.amount})'  # HardCode
+
+    
+# john_dep = Deposit('john', 1000)
+# david_dep = Deposit('david', 200)
+
+# print(repr(john_dep))
+
+# # Output:
+# # Deposit(name=john, amount=1000)
+
+
+
+# Solution 4 - __add__(self, other)
 import os
 os.system('cls')
 
@@ -37,13 +89,19 @@ class Deposit:
         return f'Owner: {self.owner} | amount: {self.amount}'
 
     def __repr__(self):
-        return f'Deposit(name={self.owner}, amount={self.amount})'
+        return f'{self.__class__.__name__}(name={self.owner}, amount={self.amount})'  # HardCode
+
+    def __add__(self, other):
+        name = f'{self.owner}+{other.owner}'
+        amount = f'{self.amount}+{other.amount}'
+        return Deposit(name, amount)
+
 
     
 john_dep = Deposit('john', 1000)
 david_dep = Deposit('david', 200)
 
-print(repr(john_dep))
+print(john_dep + david_dep)
 
 # Output:
-# Deposit(name=john, amount=1000)
+# Owner: john+david | amount: 1000+200
