84279251dabab0c33f8ffaab642c7f09e6e85c12
Amir Motefaker <amotef@gmail.com>
2022-07-13 14:28:01 +0430

    Create error_handling.py

---
+++ error_handling.py
@@ -0,0 +1,21 @@
+# Code by @AmirMotefaker
+
+# Error Handling
+
+# Solution 1
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+
+# Output:
+# Please enter a number: a
+# Oops!  That was no valid number.  Try again...
+# Please enter a number: -d
+# Oops!  That was no valid number.  Try again...
+# Please enter a number: @
+# Oops!  That was no valid number.  Try again...
+# Please enter a number: 1
