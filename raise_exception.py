4e5c3f55589014316dee87b2d4c94642b4a2b505
Amir Motefaker <amotef@gmail.com>
2022-07-13 14:53:11 +0430

    Update raise_exception.py
    Added solution 2

--- raise_exception.py
+++ raise_exception.py
@@ -2,22 +2,45 @@
 
 # Raise Exception
 
-# Solution 1
+# # Solution 1
+# def calc_income(income):
+#     if income < 0:
+#         raise Exception('Income should not be a negative number!')
+        
+#     return income * 2
+
+# user_income = int(input('Enter your income: '))
+# print(calc_income(user_income))
+
+
+# # Output:
+# # Enter your income: -100
+# # Traceback (most recent call last):
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\raise_exception.py", line 12, in <module>
+# #     print(calc_income(user_income))
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\raise_exception.py", line 7, in calc_income
+# #     raise Exception('Income should not be a negative number!')
+# # Exception: Income should not be a negative number!
+
+
+
+# Solution 2
 def calc_income(income):
     if income < 0:
         raise Exception('Income should not be a negative number!')
-        
+    print('first')    
     return income * 2
 
 user_income = int(input('Enter your income: '))
-print(calc_income(user_income))
 
+try:
+    print(calc_income(user_income))
+except:
+    print('EXCEPTION')
+
+print('second')
 
 # Output:
-# Enter your income: -100
-# Traceback (most recent call last):
-#   File "e:\A.Motefaker\ABC\Python\MyCode\raise_exception.py", line 12, in <module>
-#     print(calc_income(user_income))
-#   File "e:\A.Motefaker\ABC\Python\MyCode\raise_exception.py", line 7, in calc_income
-#     raise Exception('Income should not be a negative number!')
-# Exception: Income should not be a negative number!
+# Enter your income: -10
+# EXCEPTION
+# second
