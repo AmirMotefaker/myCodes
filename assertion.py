84c7e415f4cad7ebaf9b98043d057db1096b9660
Amir Motefaker <amotef@gmail.com>
2022-07-13 15:05:48 +0430

    Update assertion.py
    Added solution 3

--- assertion.py
+++ assertion.py
@@ -3,23 +3,23 @@
 # Assert Keyword - Assertion
 
 # # Solution 1
-# def calc_income(income):
-#     # if income < 0:
-#     #     raise Exception('Income should not be a negative number!')
+def calc_income(income):
+    # if income < 0:
+    #     raise Exception('Income should not be a negative number!')
 
-#     assert income>=0, 'Income should not be a negative number!'
+    assert income>=0, 'Income should not be a negative number!'
 
-#     print('first')    
-#     return income * 2
+    print('first')    
+    return income * 2
 
-# user_income = int(input('Enter your income: '))
+user_income = int(input('Enter your income: '))
 
-# try:
-#     print(calc_income(user_income))
-# except:
-#     print('EXCEPTION')
+try:
+    print(calc_income(user_income))
+except:
+    print('EXCEPTION')
 
-# print('second')
+print('second')
 
 # # Output:
 # # Enter your income: 100
@@ -29,7 +29,7 @@
 
 
 
-# Solution 2
+# # Solution 2
 def calc_income(income):
     # if income < 0:
     #     raise Exception('Income should not be a negative number!')
@@ -48,11 +48,26 @@ print(calc_income(user_income))
 
 # print('second')
 
+# # Output:
+# # Enter your income: -10
+# # Traceback (most recent call last):
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\assertion.py", line 45, in <module>
+# #     print(calc_income(user_income))
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\assertion.py", line 37, in calc_income
+# #     assert income>=0, 'Income should not be a negative number!'
+# # AssertionError: Income should not be a negative number!
+
+
+
+# Solution 3
+def square(x):
+    assert x>=0, 'Only positive numbers are allowed'
+    return x*x
+
+try:
+    square(-2)
+except AssertionError as msg:
+    print(msg)
+
 # Output:
-# Enter your income: -10
-# Traceback (most recent call last):
-#   File "e:\A.Motefaker\ABC\Python\MyCode\assertion.py", line 45, in <module>
-#     print(calc_income(user_income))
-#   File "e:\A.Motefaker\ABC\Python\MyCode\assertion.py", line 37, in calc_income
-#     assert income>=0, 'Income should not be a negative number!'
-# AssertionError: Income should not be a negative number!
+# Only positive numbers are allowed
