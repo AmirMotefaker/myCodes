1ffaf755b5f80e62c86027cd3bdda263582562ed
Amir Motefaker <amotef@gmail.com>
2022-07-13 15:17:59 +0430

    Create access_error_content.py

---
+++ access_error_content.py
@@ -0,0 +1,30 @@
+# Code by @AmirMotefaker
+
+# Access to error content
+
+try:
+    print(2/0)
+except ZeroDivisionError as e:
+    print('EXCEPTION', e)
+
+# Output:
+# EXCEPTION division by zero
+
+
+try:
+    import x
+except Exception as e:
+    print('EXCEPTION', e)
+
+# Output:
+# EXCEPTION No module named 'x'
+
+
+
+try:
+    import x
+except ModuleNotFoundError as e:
+    print('EXCEPTION', e)
+
+# Output:
+# EXCEPTION No module named 'x'
