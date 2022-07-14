e580fa5507135ab74f75a675eb353e1a40ff8c3a
Amir Motefaker <amotef@gmail.com>
2022-07-14 11:38:08 +0430

    Update getting_input_from_user.py
    Added solution 7 with "Handling Exceptions"

--- getting_input_from_user.py
+++ getting_input_from_user.py
@@ -3,9 +3,9 @@
 # Getting Input From User
 
 # # Solution 1
-# user_input = input("Enter your number:  ")
+user_input = input("Enter your number:  ")
 
-# print(int(user_input))
+print(int(user_input))
 
 # # Output:
 # # Enter your number:  12
@@ -19,9 +19,9 @@
 
 
 # # Solution 2
-# user_input = input("Enter your number:  ")
+user_input = input("Enter your number:  ")
 
-# print(float(user_input))
+print(float(user_input))
 
 # # Output:
 # # Enter your number:  12.1
@@ -39,9 +39,9 @@
 
 
 # # Solution 3
-# user_input = input("Enter your number:  ")
+user_input = input("Enter your number:  ")
 
-# result = int(user_input)
+result = int(user_input)
 
 # # Output:
 # # Enter your number:  13.1
@@ -53,15 +53,15 @@
 
 
 # # Solution 4
-# user_input = input("Enter your number:  ")
-# result = None
+user_input = input("Enter your number:  ")
+result = None
 
-# try:
-#     result = int(user_input)
-# except ValueError:
-#     print('Not an integer')
+try:
+    result = int(user_input)
+except ValueError:
+    print('Not an integer')
 
-# print(f'Result: {result}')
+print(f'Result: {result}')
 
 # # Output:
 # # Enter your number:  12.1
@@ -70,7 +70,7 @@
 
 
 
-# Solution 5
+# # Solution 5
 user_input = input("Enter your number:  ")
 result = None
 
@@ -81,22 +81,85 @@ except ValueError:
 
 print(f'Result: {result}')
 
+# # Output:
+# # Enter your number:  12
+# # Result: 12
+
+# # Enter your number:  12.1
+# # Result: 12.1
+
+# # Enter your number:  123a
+# # Traceback (most recent call last):
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 78, in <module>
+# #     result = int(user_input)
+# # ValueError: invalid literal for int() with base 10: '123a'
+
+# # During handling of the above exception, another exception occurred:
+
+# # Traceback (most recent call last):
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 80, in <module>
+# #     result = float(user_input)
+# # ValueError: could not convert string to float: '123a'
+
+
+
+# # Solution 6
+user_input = input("Enter your number:  ")
+result = None
+
+try:
+    result = int(user_input)
+except ValueError:
+    try:
+        result = float(user_input)
+    except ValueError:
+        print('You have to enter a number.')
+
+print(type(result))
+print(f'Result: {result}')
+
+# # Output:
+# # Enter your number:  12
+# # <class 'int'>
+# # Result: 12 
+
+# # Enter your number:  12.1
+# # <class 'float'>
+# # Result: 12.1
+
+# # Enter your number:  dsadsa1
+# # You have to enter a number.
+# # <class 'NoneType'>
+# # Result: None
+
+
+
+# Solution 7
+result = None
+
+while True:
+    user_input = input("Enter your number:  ")
+    
+    try:
+        result = int(user_input)
+        break
+    except ValueError:
+        try:
+            result = float(user_input)
+            break
+        except ValueError:
+            print('You have to enter a number. Please try again.')
+
+print(type(result))
+print(f'Result: {result}')
+
 # Output:
-# Enter your number:  12
-# Result: 12
-
-# Enter your number:  12.1
-# Result: 12.1
-
+# Enter your number:  hello
+# You have to enter a number. Please try again.
 # Enter your number:  123a
-# Traceback (most recent call last):
-#   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 78, in <module>
-#     result = int(user_input)
-# ValueError: invalid literal for int() with base 10: '123a'
-
-# During handling of the above exception, another exception occurred:
-
-# Traceback (most recent call last):
-#   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 80, in <module>
-#     result = float(user_input)
-# ValueError: could not convert string to float: '123a'
+# You have to enter a number. Please try again.
+# Enter your number:
+# You have to enter a number. Please try again.
+# Enter your number:  123.2
+# <class 'float'>
+# Result: 123.2
