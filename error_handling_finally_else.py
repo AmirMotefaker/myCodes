d4002a1f25371f5f173ca4bf73e1ad9114372435
Amir Motefaker <amotef@gmail.com>
2022-07-14 09:28:39 +0430

    Update error_handling_finally_else.py
    Added solution 3 with "some other error"

--- error_handling_finally_else.py
+++ error_handling_finally_else.py
@@ -18,12 +18,40 @@
 
 
 
-# Solution 2
+# # Solution 2
+# a = 10
+# b = int(input('Enter a number: '))
+
+# try: # try to run this code
+#     print( a / b)
+# except ZeroDivisionError:
+#     print('EXCEPTION')
+# except ModuleNotFoundError:
+#     print('another exception')
+# except Exception:
+#     print('some other error')
+# else: # if there is no error
+#     print('ELSE')
+
+# print('END')
+
+# # OutPut:
+# # Enter a number: 0
+# # EXCEPTION
+# # END
+
+# # Enter a number: 10
+# # 1.0
+# # ELSE
+# # END
+
+
+# Solution 3
 a = 10
 b = int(input('Enter a number: '))
 
 try: # try to run this code
-    print( a / b)
+    b = c + 10
 except ZeroDivisionError:
     print('EXCEPTION')
 except ModuleNotFoundError:
@@ -35,12 +63,7 @@ else: # if there is no error
 
 print('END')
 
-# OutPut:
-# Enter a number: 0
-# EXCEPTION
-# END
-
-# Enter a number: 10
-# 1.0
-# ELSE
+# Output:
+# nter a number: 10
+# some other error
 # END
