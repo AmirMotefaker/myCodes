fc6cc925c8bc51a6c43f6eca61fb7498baf2a6df
Amir Motefaker <amotef@gmail.com>
2022-07-14 11:28:34 +0430

    Update getting_input_from_user.py
    Added solution 4,5

--- getting_input_from_user.py
+++ getting_input_from_user.py
@@ -18,20 +18,85 @@
 # # ValueError: invalid literal for int() with base 10: '12.1'
 
 
-# Solution 2
-user_input = input("Enter your number:  ")
+# # Solution 2
+# user_input = input("Enter your number:  ")
 
-print(float(user_input))
+# print(float(user_input))
+
+# # Output:
+# # Enter your number:  12.1
+# # 12.1
+
+# # Enter your number:  12
+# # 12.0
+
+# # Enter your number:  hello
+# # Traceback (most recent call last):
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 24, in <module>
+# #     print(float(user_input))
+# # ValueError: could not convert string to float: 'hello'
+
+
+
+# # Solution 3
+# user_input = input("Enter your number:  ")
+
+# result = int(user_input)
+
+# # Output:
+# # Enter your number:  13.1
+# # Traceback (most recent call last):
+# #   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 44, in <module>
+# #     result = int(user_input)
+# # ValueError: invalid literal for int() with base 10: '13.1'
+
+
+
+# # Solution 4
+# user_input = input("Enter your number:  ")
+# result = None
+
+# try:
+#     result = int(user_input)
+# except ValueError:
+#     print('Not an integer')
+
+# print(f'Result: {result}')
+
+# # Output:
+# # Enter your number:  12.1
+# # Not an integer
+# # Result: None
+
+
+
+# Solution 5
+user_input = input("Enter your number:  ")
+result = None
+
+try:
+    result = int(user_input)
+except ValueError:
+    result = float(user_input)
+
+print(f'Result: {result}')
 
 # Output:
-# Enter your number:  12.1
-# 12.1
-
 # Enter your number:  12
-# 12.0
+# Result: 12
 
-# Enter your number:  hello
+# Enter your number:  12.1
+# Result: 12.1
+
+# Enter your number:  123a
 # Traceback (most recent call last):
-#   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 24, in <module>
-#     print(float(user_input))
-# ValueError: could not convert string to float: 'hello'
+#   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 78, in <module>
+#     result = int(user_input)
+# ValueError: invalid literal for int() with base 10: '123a'
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "e:\A.Motefaker\ABC\Python\MyCode\getting_input_from_user.py", line 80, in <module>
+#     result = float(user_input)
+# ValueError: could not convert string to float: '123a'
