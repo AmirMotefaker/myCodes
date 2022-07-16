c6da4aed1ba0daeaa0439c043656d718845259b6
Amir Motefaker <amotef@gmail.com>
2022-07-16 12:44:35 +0430

    Create working_with_files.py

---
+++ working_with_files.py
@@ -0,0 +1,12 @@
+# Code by @AmirMotefaker
+
+# Working with files
+
+# Solution 1
+name = input('Enter your file name: ')
+
+open(f'{name}.txt', 'w') # w: write r: read
+
+# Output:
+# Enter your file name: info 
+# create file(info.txt)
