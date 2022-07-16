376a35107342436757c0b9ad8ad35434145d3964
Amir Motefaker <amotef@gmail.com>
2022-07-16 13:31:48 +0430

    Update working_with_files.py
    Added solution 2

--- working_with_files.py
+++ working_with_files.py
@@ -2,11 +2,22 @@
 
 # Working with files
 
-# Solution 1
-name = input('Enter your file name: ')
+# # Solution 1
+# name = input('Enter your file name: ')
 
-open(f'{name}.txt', 'w') # w: write r: read
+# open(f'{name}.txt', 'w') # w: write r: read
+
+# # Output:
+# # Enter your file name: info 
+# # create file(info.txt)
+
+
+
+# Solution 2
+f = open('names.txt', 'w')
+
+f.write('hello')
+
 
 # Output:
-# Enter your file name: info 
-# create file(info.txt)
+# in file names.txt write hello
