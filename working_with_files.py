32285969154a020c5d05bc1ce59b0029b077115b
Amir Motefaker <amotef@gmail.com>
2022-07-16 15:07:02 +0430

    Update working_with_files.py
    Added solution 4 for read files

--- working_with_files.py
+++ working_with_files.py
@@ -13,11 +13,56 @@
 
 
 
-# Solution 2
+# # Solution 2 - write
+# f = open('names.txt', 'w')
+
+# f.write('hello')
+
+
+# # Output:
+# # in file names.txt write hello
+
+
+
+# # Solution 3 - write
+# f = open('names.txt', 'w')
+
+# f.write('x\n')
+# f.write('y')
+# f.write('\nz')
+
+# # Output:
+# # write in file names.txt:
+# # x
+# # y
+# # z
+
+
+
+# Solution 4 - read
 f = open('names.txt', 'w')
 
-f.write('hello')
+f.write('john\n')
+f.write('zahra')
+f.write('\nbaran')
+f.write('\nali')
+f.write('\nhamed')
+f.write('\ndavid')
+
+
+f = open('names.txt', 'r')
+
+a= f.read()
+
+print(type(a))
+print(a)
 
 
 # Output:
-# in file names.txt write hello
+# <class 'str'>
+# john
+# zahra
+# baran
+# ali
+# hamed
+# david
