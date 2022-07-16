16e273ce48361faa6c44e6ccbbebe70dd7fb4dee
Amir Motefaker <amotef@gmail.com>
2022-07-16 16:10:06 +0430

    Update working_with_files.py
    Added solution 7-12

--- working_with_files.py
+++ working_with_files.py
@@ -80,13 +80,130 @@
 # # ['john', 'zahra', 'baran', 'ali', 'hamed', 'david']
 
 
-# Solution 6 - .split
+# # Solution 6 - .split
+# f = open('names.txt', 'r')
+
+# a = f.read()
+
+# for line in a.split('\n'):
+#     print(line)
+
+# # Output:
+# # john
+# # zahra
+# # baran
+# # ali
+# # hamed
+# # david
+
+
+# # Solution 7 - readlines()
+# f = open('names.txt', 'r')
+
+# a = f.readlines()
+
+# print(a)
+
+# # Output:
+# # ['john\n', 'zahra\n', 'baran\n', 'ali\n', 'hamed\n', 'david']
+
+
+# # Solution 8 - read and . split
+# a = f.read()
+
+# for name in a.split('\n'):
+#     print(f'Hello {name.title()}')
+
+# # Output:
+# # Hello John
+# # Hello Zahra
+# # Hello Baran
+# # Hello Ali
+# # Hello Hamed
+# # Hello David
+
+
+# # Solution 9 - readline()
+# f = open('names.txt', 'r')
+
+# while True:
+#     line = f.readline()
+#     print(line) 
+#     if line == '':
+#         break
+
+# print('END')
+
+# # Output:
+# # john
+
+# # zahra
+
+# # baran
+
+# # ali
+
+# # hamed
+
+# # david
+
+# # END
+
+
+
+# # Solution 10 - readline() and readlines()
+# f = open('names.txt', 'r')
+
+# #print(f.readlines())
+
+# while True:
+#     line = f.readline()
+#     print(line, end='') 
+#     if line == '':
+#         break
+
+# print('END')
+
+# # Output:
+# # john
+# # zahra
+# # baran
+# # ali
+# # hamed
+# # davidEND
+
+
+
+# # Solution 11 - readline() and readlines()
+# f = open('names.txt', 'r')
+
+# print(f.readlines())
+
+# while True:
+#     line = f.readline()
+#     print(line, end='') 
+#     if line == '':
+#         break
+
+# print('END')
+
+# # Output:
+# # ['john\n', 'zahra\n', 'baran\n', 'ali\n', 'hamed\n', 'david']
+# # END
+
+
+
+# Solution 12 - readline()
 f = open('names.txt', 'r')
 
-a = f.read()
+while True:
+    line = f.readline()
+    if line == '':
+        break
 
-for line in a.split('\n'):
-    print(line)
+    print(line, end='')
+
+print('END')
 
 # Output:
 # john
@@ -94,4 +211,4 @@ for line in a.split('\n'):
 # baran
 # ali
 # hamed
-# david
+# davidEND
