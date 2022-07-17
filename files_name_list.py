ea9f7dc676930ded0d24d50f4d113123c62313bc
Amir Motefaker <amotef@gmail.com>
2022-07-17 12:12:14 +0430

    Update files_name_list.py
    Added solution 4 with reader.read()

--- files_name_list.py
+++ files_name_list.py
@@ -16,26 +16,63 @@
 
 
 
-# Solution 2
-names_file = open('names.txt', 'r')
+# # Solution 2
+# names_file = open('names.txt', 'r')
 
-names = names_file.read()
+# names = names_file.read()
 
-names = names.split('\n')
+# names = names.split('\n')
+
+# print(names)
+
+# while True:
+#     user_input = input('Enter name: ')
+#     if user_input == 'exit':
+#         break
+#     names.append(user_input)
+#     print(names)
+
+# # Output:
+# # ['john', 'zahra', 'baran', 'ali', 'hamed', 'david']
+# # Enter name: hadi
+# # ['john', 'zahra', 'baran', 'ali', 'hamed', 'david', 'hadi']
+# # Enter name: amir
+# # ['john', 'zahra', 'baran', 'ali', 'hamed', 'david', 'hadi', 'amir']
+# # Enter name: exit
+
+
+
+# # Solution 3 - reader
+# with open('names.txt', 'r') as reader:
+#     print(reader.read())
+#     print('in hear')
+
+# print('END')
+
+# # Output:
+# # john
+# # zahra
+# # baran
+# # ali
+# # hamed
+# # david
+# # in hear
+# # END
+
+
+
+# Solution 4 - reader.read()
+names = []
+
+with open('names.txt', 'r') as reader:
+    for line in reader.read().split('\n'):
+        names.append(line)
+    print('in hear')
 
 print(names)
 
-while True:
-    user_input = input('Enter name: ')
-    if user_input == 'exit':
-        break
-    names.append(user_input)
-    print(names)
+print('END')
 
 # Output:
 # ['john', 'zahra', 'baran', 'ali', 'hamed', 'david']
-# Enter name: hadi
-# ['john', 'zahra', 'baran', 'ali', 'hamed', 'david', 'hadi']
-# Enter name: amir
-# ['john', 'zahra', 'baran', 'ali', 'hamed', 'david', 'hadi', 'amir']
-# Enter name: exit
+# END
