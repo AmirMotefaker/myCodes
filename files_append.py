1d696646e9c27afb72a4ed55009844db802c9d12
Amir Motefaker <amotef@gmail.com>
2022-07-17 13:39:42 +0430

    Create files_append.py

---
+++ files_append.py
@@ -0,0 +1,34 @@
+# Code by @AmirMotefaker
+
+# Working with files - append
+
+# # Solution 1
+f = open('names.txt', 'a') # w: write, r: read, a:append
+
+f.write('\nanother line')
+
+# # Output(in files names.txt):
+# # john
+# # zahra
+# # baran
+# # ali
+# # hamed
+# # davidanother line
+# # another line
+
+
+
+# Solution 2 - with 
+with open('names.txt', 'a') as names_file:
+    names_file.write('\nThis is new')
+
+# Output(in files names.txt):
+# john
+# zahra
+# baran
+# ali
+# hamed
+# davidanother line
+# another line
+# This is new
+# This is new
