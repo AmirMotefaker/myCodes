795b23b40ffddb61e57a921bdb93e7e1f252f3f9
Amir Motefaker <amotef@gmail.com>
2022-07-17 13:07:51 +0430

    Create files_wirte_to_file.py

---
+++ files_wirte_to_file.py
@@ -0,0 +1,46 @@
+# Code by @AmirMotefaker
+
+# Working with files - write to files
+
+# # Solution 1
+# f = open('words.txt', 'w')
+
+# f.write('123\n')
+# f.write('456\n')
+
+# f.writelines(['hello\n', 'hi'])
+
+# # Output in words.txt:
+# # 123
+# # 456
+# # hello
+# # hi
+
+
+
+# # Solution 2 - writelines - map - lambda
+# f = open('words.txt', 'w')
+
+# names = ['john', 'joe', 'david']
+
+# f.writelines(map(lambda name: name+'\n', names))
+
+# # # Output in words.txt:
+# # john
+# # joe
+# # david
+
+
+
+# Solution 3 - write -  .join()
+f = open('words.txt', 'w')
+
+names = ['john', 'joe', 'david']
+
+f.write('\n'.join(names))
+# f.writelines(map(lambda name: name+'\n', names))
+
+# # Output in words.txt:
+# john
+# joe
+# david
