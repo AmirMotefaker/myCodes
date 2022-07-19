c60d8fdfb58ffaf9358c237cd8b8545a3a38b9d1
Amir Motefaker <amotef@gmail.com>
2022-07-19 15:21:27 +0430

    Update GUI_03_getting_input_from_user.py
    Added solution 4,5 with .pack() and from tkinter import LEFT, BOTTOM

--- GUI_03_getting_input_from_user.py
+++ GUI_03_getting_input_from_user.py
@@ -22,7 +22,7 @@ window.mainloop()
 
 
 
-# Solution 2 - connect Entry and Label
+# # Solution 2 - connect Entry and Label
 import tkinter as tk
 
 window = tk.Tk()
@@ -47,4 +47,95 @@ user_input.pack()
 window.mainloop()
 
 
-# 
+
+# # Solution 3 - .pack(side=tk.LEFT)
+import tkinter as tk
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+user_input.pack(side=tk.LEFT)
+text.pack(side=tk.LEFT)
+
+
+window.mainloop()
+
+
+
+# # Solution 4 - .pack()
+import tkinter as tk
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+name_text = tk.Label(
+    master=window,
+    text='First Name: ',
+)
+
+name_text.pack(side=tk.LEFT)
+user_input.pack(side=tk.LEFT)
+text.pack(side=tk.LEFT)
+
+
+window.mainloop()
+
+
+
+# Solution 5 - from tkinter import LEFT, BOTTOM
+import tkinter as tk
+from tkinter import LEFT, BOTTOM
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+name_text = tk.Label(
+    master=window,
+    text='First Name: ',
+)
+
+name_text.pack(side=LEFT)
+user_input.pack(side=LEFT)
+text.pack(side=LEFT)
+
+
+window.mainloop()
