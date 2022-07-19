bbbf7bec33acf63c2e53f2b5d2f4507d583513c3
Amir Motefaker <amotef@gmail.com>
2022-07-19 15:40:13 +0430

    Create GUI_04_button_making.py

---
+++ GUI_04_button_making.py
@@ -0,0 +1,62 @@
+# Code by @AmirMotefaker
+
+# GUI - Button making
+
+# # Solution 1 - Create Button
+import tkinter as tk
+
+window = tk.Tk()
+
+button = tk.Button(
+    master=window,
+    text='Click me!',
+)
+
+button.pack()
+
+window.mainloop()
+
+
+
+# # Solution 2 - def button_clicked()
+import tkinter as tk
+
+window = tk.Tk()
+
+def button_clicked():
+    print('Clicked!')
+
+button = tk.Button(
+    master=window,
+    text='Click me!',
+    command=button_clicked,
+)
+
+button.pack()
+
+window.mainloop()
+
+# Output:
+# Clicked!
+
+
+
+# Solution 3 - lambda Instead of def button_clicked()
+import tkinter as tk
+
+window = tk.Tk()
+
+button = tk.Button(
+    master=window,
+    text='Click me!',
+    command=lambda: print('Clicked!'),
+)
+
+button.pack()
+
+window.mainloop()
+
+# Output:
+# Clicked!
+# Clicked!
+# Clicked!
