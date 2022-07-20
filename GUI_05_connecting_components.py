2c26fda3892d7e0c6b4a8a2072d482c5b75003ad
Amir Motefaker <amotef@gmail.com>
2022-07-20 16:30:12 +0430

    Update GUI_05_connecting_components.py
    Remove #

--- GUI_05_connecting_components.py
+++ GUI_05_connecting_components.py
@@ -3,63 +3,63 @@
 # GUI - Connecting components to each other - submit
 
 # # Solution 1
-# import tkinter as tk
+import tkinter as tk
 
-# window = tk.Tk()
+window = tk.Tk()
 
-# def button_clicked():
-#     print('Clicked!')
+def button_clicked():
+    print('Clicked!')
 
-# name_entry = tk.Entry(
-#     window,
-# )
+name_entry = tk.Entry(
+    window,
+)
 
-# submit_button = tk.Button(
-#     window,
-#     text = 'Submit',
-#     command=button_clicked,
-# )
+submit_button = tk.Button(
+    window,
+    text = 'Submit',
+    command=button_clicked,
+)
 
-# show_name_label = tk.Label(
-#     window,
-#     text = 'Test',
-# )
+show_name_label = tk.Label(
+    window,
+    text = 'Test',
+)
 
-# name_entry.pack()
-# submit_button.pack()
-# show_name_label.pack()
+name_entry.pack()
+submit_button.pack()
+show_name_label.pack()
 
-# window.mainloop()
+window.mainloop()
 
 
 
 # # Solution 2 - name_entry.get()
-# import tkinter as tk
+import tkinter as tk
 
-# window = tk.Tk()
+window = tk.Tk()
 
-# name_entry = tk.Entry(
-#     window,
-# )
-# show_name_label = tk.Label(
-#     window,
-#     text = 'Test',
-# )
+name_entry = tk.Entry(
+    window,
+)
+show_name_label = tk.Label(
+    window,
+    text = 'Test',
+)
 
-# def button_clicked():
-#     print(name_entry.get())
+def button_clicked():
+    print(name_entry.get())
 
-# submit_button = tk.Button(
-#     window,
-#     text = 'Submit',
-#     command=button_clicked,
-# )
+submit_button = tk.Button(
+    window,
+    text = 'Submit',
+    command=button_clicked,
+)
 
-# name_entry.pack()
-# submit_button.pack()
-# show_name_label.pack()
+name_entry.pack()
+submit_button.pack()
+show_name_label.pack()
 
-# window.mainloop()
+window.mainloop()
 
 
 
