47bfe51232c48fd5469501c1355b78200d94375b
Amir Motefaker <amotef@gmail.com>
2022-07-21 13:20:33 +0430

    Update GUI_08_sticky.py
    Added solution 3 with sticky with Tuple - sticky=(E, W)

--- GUI_08_sticky.py
+++ GUI_08_sticky.py
@@ -64,8 +64,66 @@
 
 
 
-# Solution 2  - sticky
+# # Solution 2  - sticky
+# import tkinter as tk
+
+# window = tk.Tk()
+
+# label_title = tk.Label(
+#     window,
+#     text='Enter you data',
+# )
+# label_title.grid(row=0, column=0, columnspan=2)
+
+# label_input_name = tk.Label(
+#     window,
+#     text = 'First Name: ',
+# )
+# entry_name = tk.Entry(
+#     window,
+# )
+
+
+# label_input_name.grid(row=1, column=0)
+# entry_name.grid(row=1, column=1)
+
+# label_input_last_name = tk.Label(
+#     window,
+#     text = 'Last Name: ',
+# )
+# entry_last_name = tk.Entry(
+#     window,
+# )
+
+# label_input_last_name.grid(row=2, column=0)
+# entry_last_name.grid(row=2, column=1)
+
+# label_input_age = tk.Label(
+#     window,
+#     text='Age: ',
+# )
+# entry_age = tk.Entry(
+#     window,
+# )
+
+
+# label_input_age.grid(row=3, column=0)
+# entry_age.grid(row=3, column=1)
+
+
+# submit_button = tk.Button(
+#     window,
+#     text='Submit'
+# )
+# submit_button.grid(row=4, column=1, sticky='ew')
+
+# window.mainloop()
+
+
+
+# Solution 3  - sticky with Tuple - sticky=(E, W)
 import tkinter as tk
+from tkinter import E, W, N, S
 
 window = tk.Tk()
 
@@ -84,7 +142,7 @@ entry_name = tk.Entry(
 )
 
 
-label_input_name.grid(row=1, column=0)
+label_input_name.grid(row=1, column=0, sticky=(W, ))
 entry_name.grid(row=1, column=1)
 
 label_input_last_name = tk.Label(
@@ -95,7 +153,7 @@ entry_last_name = tk.Entry(
     window,
 )
 
-label_input_last_name.grid(row=2, column=0)
+label_input_last_name.grid(row=2, column=0, sticky=(W, ))
 entry_last_name.grid(row=2, column=1)
 
 label_input_age = tk.Label(
@@ -107,7 +165,7 @@ entry_age = tk.Entry(
 )
 
 
-label_input_age.grid(row=3, column=0)
+label_input_age.grid(row=3, column=0, sticky=(W, ))
 entry_age.grid(row=3, column=1)
 
 
@@ -115,6 +173,6 @@ submit_button = tk.Button(
     window,
     text='Submit'
 )
-submit_button.grid(row=4, column=1, sticky='ew')
+submit_button.grid(row=4, column=1, sticky=(E, W))
 
 window.mainloop()
