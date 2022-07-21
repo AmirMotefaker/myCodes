2695b6cc092f3ad0badb4de21cc6da9162dff050
Amir Motefaker <amotef@gmail.com>
2022-07-21 12:20:34 +0430

    Update GUI_06_grid.py
    Remove # from all codes

--- GUI_06_grid.py
+++ GUI_06_grid.py
@@ -3,77 +3,77 @@
 # GUI - Grid
 
 # # Solution 1
-# import tkinter as tk
+import tkinter as tk
 
-# window = tk.Tk()
+window = tk.Tk()
 
-# label_input_name = tk.Label(
-#     window,
-#     text = 'First Name: ',
-# )
-# entry_name = tk.Entry(
-#     window,
-# )
+label_input_name = tk.Label(
+    window,
+    text = 'First Name: ',
+)
+entry_name = tk.Entry(
+    window,
+)
 
-# label_input_name.pack()
-# entry_name.pack()
+label_input_name.pack()
+entry_name.pack()
 
-# window.mainloop()
+window.mainloop()
 
 
 
 # # Solution 2 - Grid
-# import tkinter as tk
+import tkinter as tk
 
-# window = tk.Tk()
+window = tk.Tk()
 
-# label_input_name = tk.Label(
-#     window,
-#     text = 'First Name: ',
-# )
-# entry_name = tk.Entry(
-#     window,
-# )
+label_input_name = tk.Label(
+    window,
+    text = 'First Name: ',
+)
+entry_name = tk.Entry(
+    window,
+)
 
-# label_input_name.grid(row=0, column=0)
-# entry_name.grid(row=0, column=1)
-# # label_input_name.pack()
-# # entry_name.pack()
+label_input_name.grid(row=0, column=0)
+entry_name.grid(row=0, column=1)
+# label_input_name.pack()
+# entry_name.pack()
 
-# window.mainloop()
+window.mainloop()
 
 
 
 # # Solution 3 - Grid
-# import tkinter as tk
+import tkinter as tk
 
-# window = tk.Tk()
+window = tk.Tk()
 
-# label_input_name = tk.Label(
-#     window,
-#     text = 'First Name: ',
-# )
-# entry_name = tk.Entry(
-#     window,
-# )
-
-
-# label_input_name.grid(row=0, column=0)
-# entry_name.grid(row=0, column=1)
-
-# label_input_last_name = tk.Label(
-#     window,
-#     text = 'Last Name: ',
-# )
-# entry_last_name = tk.Entry(
-#     window,
-# )
-
-# label_input_last_name.grid(row=1, column=0)
-# entry_last_name.grid(row=1, column=1)
+label_input_name = tk.Label(
+    window,
+    text = 'First Name: ',
+)
+entry_name = tk.Entry(
+    window,
+)
 
 
-# window.mainloop()
+label_input_name.grid(row=0, column=0)
+entry_name.grid(row=0, column=1)
+
+label_input_last_name = tk.Label(
+    window,
+    text = 'Last Name: ',
+)
+entry_last_name = tk.Entry(
+    window,
+)
+
+label_input_last_name.grid(row=1, column=0)
+entry_last_name.grid(row=1, column=1)
+
+
+window.mainloop()
 
 
 
