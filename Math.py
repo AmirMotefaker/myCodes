bc2cfa396ae0ce76dcd739955821efafc041a947
Amir Motefaker <amotef@gmail.com>
2022-07-29 00:14:58 +0430

    Update Math.py
    Added solutions

--- Math.py
+++ Math.py
@@ -25,10 +25,47 @@ print(x)
 
 
 
-# Solution 3
+# # Solution 3
 x = pow(4, 3)
 
 print(x)
 
+# # Output:
+# # 64
+
+
+# # Solution 4 - math
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+# # Output:
+# # 8.0
+
+
+# # Solution 5
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+# # Output:
+# # 2
+# # 1
+
+
+
+# Solution 6
+import math
+
+x = math.pi
+
+print(x)
+
 # Output:
-64
+# 3.141592653589793
