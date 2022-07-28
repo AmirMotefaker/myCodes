632fa2398e9be03ff395f0f80c1c33889c060a82
Amir Motefaker <amotef@gmail.com>
2022-07-29 00:11:58 +0430

    Create Math.py

---
+++ Math.py
@@ -0,0 +1,34 @@
+# Code by @AmirMotefaker
+
+# Math
+
+# # Solution 1
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# # Output:
+# # 5
+# # 25
+
+
+
+# # Solution 2
+x = abs(-7.25)
+
+print(x)
+
+# # Output:
+# # 7.25
+
+
+
+# Solution 3
+x = pow(4, 3)
+
+print(x)
+
+# Output:
+64
