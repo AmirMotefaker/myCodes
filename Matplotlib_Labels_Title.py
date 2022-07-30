2fbdb7a73784610cfcff7e1959cad92187c804ab
Amir Motefaker <amotef@gmail.com>
2022-07-30 11:52:03 +0430

    Create Matplotlib_Labels_Title.py

---
+++ Matplotlib_Labels_Title.py
@@ -0,0 +1,74 @@
+# Code by @AmirMotefaker
+
+# Matplotlib - Labels and Title
+
+# # Solution 1 - Create Labels for a Plot
+# # With Pyplot, you can use the xlabel() and ylabel() functions to set a label for the x- and y-axis
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+
+
+# # Solution 2  - Create a Title for a Plot
+# # Add a plot title and labels for the x- and y-axis
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+
+
+# # Solution 3  - Set Font Properties for Title and Labels
+# # You can use the fontdict parameter in xlabel(), ylabel(),
+# # and title() to set font properties for the title and labels.
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+
+plt.title("Sports Watch Data", fontdict = font1)
+plt.xlabel("Average Pulse", fontdict = font2)
+plt.ylabel("Calorie Burnage", fontdict = font2)
+
+plt.plot(x, y)
+plt.show()
+
+
+
+# Solution 4  - Position the Title
+# You can use the loc parameter in title() to position the title.
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data", loc = 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+plt.show()
