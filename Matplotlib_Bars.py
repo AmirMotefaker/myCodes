1fb19d16eb21eea9a18ed39cdbc030cd177f8d8c
Amir Motefaker <amotef@gmail.com>
2022-07-31 12:05:11 +0430

    Create Matplotlib_Bars.py

---
+++ Matplotlib_Bars.py
@@ -0,0 +1,97 @@
+# Code by @AmirMotefaker
+
+# Matplotlib - Matplotlib Bars
+
+# # Solution 1 - Creating Bars - Draw 4 bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x,y)
+plt.show()
+
+
+
+# # Solution 2 - Creating Bars - Three lines to make our compiler able to draw
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = ["APPLES", "BANANAS"]
+y = [400, 350]
+
+plt.bar(x, y)
+plt.show()
+
+
+
+# # Solution 3 - Horizontal Bars - Draw 4 horizontal bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y)
+plt.show()
+
+
+
+# # Solution 4 - Bar Color - Draw 4 red bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "red")
+plt.show()
+
+
+
+# # Solution 5 - Color Names - Draw 4 "hot pink" bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "hotpink")
+plt.show()
+
+
+
+# # Solution 6 - Color Hex - Draw 4 bars with a beautiful green color
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "#4CAF50")
+plt.show()
+
+
+
+# # Solution 7 - Bar Width - Draw 4 very thin bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width = 0.1)
+plt.show()
+
+
+
+# Solution 8 - Bar Height - Draw 4 very thin bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y, height = 0.1)
+plt.show()
