d2980752ac9923a2b0a6bbf8ce0312d22bc639af
Amir Motefaker <amotef@gmail.com>
2022-08-01 10:30:59 +0430

    Create MySQL_Connection.py

---
+++ MySQL_Connection.py
@@ -0,0 +1,23 @@
+# Code by @AmirMotefaker
+
+# MySQL - Matplotlib Pie Charts
+
+# # Solution 1 - Test MySQL Connector
+# import mysql.connector
+
+
+
+# Solution 2 - Create Connection 
+# Use the username and password from your MySQL database
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="P@ssw0rd"
+)
+
+print(mydb)
+
+# Output:
+# <mysql.connector.connection_cext.CMySQLConnection object at 0x00000258A3E746D0>
