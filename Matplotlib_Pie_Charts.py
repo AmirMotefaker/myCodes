71de6ace7bd7f7576b56a457c476a7d122543cce
Amir Motefaker <amotef@gmail.com>
2022-08-01 09:02:10 +0430

    Create Matplotlib_Pie_Charts.py

---
+++ Matplotlib_Pie_Charts.py
@@ -0,0 +1,101 @@
+# Code by @AmirMotefaker
+
+# Matplotlib - Matplotlib Pie Charts
+
+# # Solution 1 - Creating Pie Charts - A simple pie chart
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+
+plt.pie(y)
+plt.show() 
+
+
+
+# # Solution 2 - Labels - A simple pie chart
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels)
+plt.show() 
+
+
+
+# # Solution 3 - Start Angle - Start the first wedge at 90 degrees
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels, startangle = 90)
+plt.show() 
+
+
+
+# # Solution 4 - Explode - Pull the "Apples" wedge 0.2 from the center of the pie
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode)
+plt.show() 
+
+
+
+# # Solution 5 - Shadow - Add a shadow
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode, shadow = True)
+plt.show() 
+
+
+
+# # Solution 6 - Colors - Specify a new color for each wedge
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+mycolors = ["black", "hotpink", "b", "#4CAF50"]
+
+plt.pie(y, labels = mylabels, colors = mycolors)
+plt.show() 
+
+
+
+# # Solution 7 - Legend - Add a legend
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels)
+plt.legend()
+plt.show() 
+
+
+
+# Solution 8 - Legend With Header - Add a legend with a header
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels)
+plt.legend(title = "Four Fruits:")
+plt.show() 
