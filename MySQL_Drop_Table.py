236c6c128aa2ab083eec700e76adaf79529eb8fb
Amir Motefaker <amotef@gmail.com>
2022-08-02 09:09:52 +0430

    Create MySQL_Drop_Table.py

---
+++ MySQL_Drop_Table.py
@@ -0,0 +1,39 @@
+# Code by @AmirMotefaker
+
+# MySQL - MySQL Drop Table
+
+# # Solution 1 - Delete a Table
+# # Delete the table "customers"
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="P@ssw0rd",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE customers"
+
+mycursor.execute(sql)
+
+
+
+# Solution 2 - Drop Only if Exist
+# Delete the table "customers" if it exists
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="P@ssw0rd",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE IF EXISTS customers"
+
+mycursor.execute(sql)
