668896aff8991d441bb6e2f925e790b9d0986007
Amir Motefaker <amotef@gmail.com>
2022-08-03 09:41:53 +0430

    Update ML_Data_Distribution.py
    Added solution 4 with Normal Data Distribution

--- ML_Data_Distribution.py
+++ ML_Data_Distribution.py
@@ -141,3 +141,14 @@ x = numpy.random.uniform(0.0, 5.0, 100000)
 plt.hist(x, 100)
 plt.show()
 
+
+
+# Solution 4 - Normal Data Distribution
+# A typical normal data distribution
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
