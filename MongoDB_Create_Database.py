8491f204d20f008d0113cdaa4e61b57a5f6d38a3
Amir Motefaker <amotef@gmail.com>
2022-08-12 00:57:33 +0430

    Create MongoDB_Create_Database.py

---
+++ MongoDB_Create_Database.py
@@ -0,0 +1,27 @@
+# Code by @AmirMotefaker
+
+# MongoDB 
+
+# # Solution 1 - Create Database
+# import pymongo
+
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# mydb = myclient["mydatabase"]
+
+
+
+# Solution 2 - Check if Database Exists
+import pymongo
+
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+
+print(myclient.list_database_names())
+
+
+
+# Solution 3 - Check if "mydatabase" exists
+dblist = myclient.list_database_names()
+if "mydatabase" in dblist:
+  print("The database exists.")
+
