e656928ac78162fdc521884e5e9b5a82d1bcafc9
Amir Motefaker <amotef@gmail.com>
2022-08-13 16:14:54 +0430

    Update multiply_two_matrices.py
    Added multiply two matrices with NumPy

--- multiply_two_matrices.py
+++ multiply_two_matrices.py
@@ -3,50 +3,82 @@
 # Multiply Two Matrices
 
 
-# Solution 1
+# # Solution 1 - Simple Nested Loops
+# # X = 3x3 matrix
+# # Y = 3x4 matrix
+# # result is 3x4
 
-# X = 3x3 matrix
-# Y = 3x4 matrix
-# result is 3x4
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+Y = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+for i in range(len(X)):
+   for j in range(len(Y[0])):
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
+
+# # Output:
+# # [114, 160, 60, 27]
+# # [74, 97, 73, 14]
+# # [119, 157, 112, 23]
 
 
-# X = [[12,7,3],
-#     [4 ,5,6],
-#     [7 ,8,9]]
-# Y = [[5,8,1,2],
-#     [6,7,3,0],
-#     [4,5,9,1]]
-# result = [[0,0,0,0],
-#          [0,0,0,0],
-#          [0,0,0,0]]
 
-# for i in range(len(X)):
-#    for j in range(len(Y[0])):
-#        for k in range(len(Y)):
-#            result[i][j] += X[i][k] * Y[k][j]
+# # Solution 2 - list comprehension
+# # Matrix Multiplication Using Nested List Comprehension
+# # Program to multiply two matrices using list comprehension
 
-# for r in result:
-#    print(r)
-
-
-# Solution 2
-# Matrix Multiplication Using Nested List Comprehension
-
-# Program to multiply two matrices using list comprehension
-
-# 3x3 matrix
 X = [[12,7,3],
     [4 ,5,6],
     [7 ,8,9]]
 
-# 3x4 matrix
 Y = [[5,8,1,2],
     [6,7,3,0],
     [4,5,9,1]]
 
-# result is 3x4
 result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row in X]
 
 for r in result:
    print(r)
 
+# # Output:
+# # [114, 160, 60, 27]
+# # [74, 97, 73, 14]
+# # [119, 157, 112, 23]
+
+
+
+
+# Solution 3 - NumPy
+import numpy as np
+
+A = [[12, 7, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+ 
+B = [[5, 8, 1, 2],
+     [6, 7, 3, 0],
+     [4, 5, 9, 1]]
+ 
+result= [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+ 
+result = np.dot(A,B)
+ 
+for r in result:
+    print(r)
+
+# Output:
+# [114 160  60  27]
+# [74 97 73 14]
+# [119 157 112  23]
