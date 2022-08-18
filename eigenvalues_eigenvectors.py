05ed3e2e51704061b6c7611d3d2c4a0d26e849f1
Amir Motefaker <amotef@gmail.com>
2022-08-18 11:44:04 +0430

    Update eigenvalues_eigenvectors.py
    Added 3 examples

--- eigenvalues_eigenvectors.py
+++ eigenvalues_eigenvectors.py
@@ -22,10 +22,11 @@
 # [ 0.52573111 -0.85065081]] 
 
 
+
 # linalg.eig(a): Compute the eigenvalues and right eigenvectors of a square array.
 
 
-# Solution 1
+# # Solution 1
 import numpy as np
   
 m = np.array([[5, 1],
@@ -43,15 +44,15 @@ print("Printing Right eigenvectors of the given square array:\n",
       v)
 
 
-# Output:
-# Printing the Original square array:
-#  [[5 1]
-#  [4 2]]
-# Printing the Eigen values of the given square array:
-#  [6. 1.]
-# Printing Right eigenvectors of the given square array:
-#  [[ 0.70710678 -0.24253563]
-#  [ 0.70710678  0.9701425 ]]
+# # Output:
+# # Printing the Original square array:
+# #  [[5 1]
+# #  [4 2]]
+# # Printing the Eigen values of the given square array:
+# #  [6. 1.]
+# # Printing Right eigenvectors of the given square array:
+# #  [[ 0.70710678 -0.24253563]
+# #  [ 0.70710678  0.9701425 ]]
 
 
 
@@ -84,3 +85,71 @@ print("Printing Right eigenvectors of the given square array:\n",
 # #  [[-0.33333333 -0.88174923 -0.03562293]
 # #  [-0.66666667 -0.01857963 -0.84529398]
 # #  [-0.66666667  0.47135241  0.53311265]]
+
+
+
+# # Solution 3
+import numpy as np
+from numpy import linalg as LA
+
+w, v = LA.eig(np.diag((1, 2, 3)))
+print(w)
+print(v)
+
+# # Output:
+# # [1. 2. 3.]
+# # [[1. 0. 0.]
+# #  [0. 1. 0.]
+# #  [0. 0. 1.]]
+
+
+
+# # Solution 4
+import numpy as np
+from numpy import linalg as LA
+
+w, v = LA.eig(np.array([[1, -1], [1, 1]]))
+
+print(w)
+print(v)
+
+# # Output:
+# # [1.+1.j 1.-1.j]
+# # [[0.70710678+0.j         0.70710678-0.j        ]
+# #  [0.        -0.70710678j 0.        +0.70710678j]]
+
+
+
+# # Solution 5
+import numpy as np
+from numpy import linalg as LA
+
+a = np.array([[1, 1j], [-1j, 1]])
+
+w, v = LA.eig(a)
+
+print(w)
+print(v)
+
+# # Output:
+# # [2.+0.j 0.+0.j]
+# # [[ 0.        +0.70710678j  0.70710678+0.j        ]
+# #  [ 0.70710678+0.j         -0.        +0.70710678j]]
+
+
+
+# Solution 6
+import numpy as np
+from numpy import linalg as LA
+
+a = np.array([[1 + 1e-9, 0], [0, 1 - 1e-9]])
+
+w, v = LA.eig(a)
+
+print(w)
+print(v)
+
+# Output:
+# [1. 1.]
+# [[1. 0.]
+#  [0. 1.]]
