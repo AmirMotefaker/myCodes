60a43ce8a30fbcd3acfe7c0fb26800d730b88a56
Amir Motefaker <amotef@gmail.com>
2022-08-19 09:04:21 +0430

    Create GUI_02_display_text_in_window.py

---
+++ GUI/GUI_02_display_text_in_window.py
@@ -0,0 +1,68 @@
+# Code by @AmirMotefaker
+
+# GUI - Display text in window
+
+# # Solution 1
+import tkinter as tk
+
+window = tk.Tk()
+
+# label - .pack()
+text = tk.Label(text='Hello World!')
+text.pack()
+
+window.mainloop()
+
+
+
+# # Solution 2
+import tkinter as tk
+
+window = tk.Tk()
+
+# label - .pack()
+text1 = tk.Label(text='Hello World1!')
+text1.pack()
+
+text2 = tk.Label(text='Hello World2!')
+text2.pack()
+
+window.mainloop()
+
+
+
+# # Solution 3
+import tkinter as tk
+
+window = tk.Tk()
+
+# label - .pack()
+text1 = tk.Label(text='Hello World1!')
+text2 = tk.Label(text='Hello World2!')
+
+text2.pack()
+text1.pack()
+
+window.mainloop()
+
+
+
+# Solution 3 - master
+import tkinter as tk
+
+window = tk.Tk()
+
+# label - 
+text1 = tk.Label(
+    master=window,
+    text='Hello World1!',
+)
+text2 = tk.Label(
+    master=window,
+    text='Hello World2!',
+)
+
+text2.pack()
+text1.pack()
+
+window.mainloop()
