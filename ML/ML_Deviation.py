8f7120709edb294516e19b7456ca99c21d431d8b
Amir Motefaker <amotef@gmail.com>
2022-08-19 09:23:19 +0430

    Create ML_Deviation.py

---
+++ ML/ML_Deviation.py
@@ -0,0 +1,60 @@
+# Code by @AmirMotefaker
+
+# Machine Learning - Standard Deviation
+
+# # Solution 1 
+# # Use the NumPy std() method to find the standard deviation
+import numpy
+
+speed = [86,87,88,86,87,85,86]
+
+x = numpy.std(speed)
+
+print(x)
+
+# # Output:
+# # 0.9035079029052513
+
+
+
+# # Solution 2
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
+
+# # Output:
+# # 37.84501153334721
+
+
+
+# # Solution 3- Variance
+# # Use the NumPy var() method to find the variance
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.var(speed)
+
+print(x)
+
+# # Output:
+# # 1432.2448979591834
+
+
+
+# Solution 4 - Standard Deviation
+# Use the NumPy std() method to find the standard deviation
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
+
+# Output:
+# 37.84501153334721
