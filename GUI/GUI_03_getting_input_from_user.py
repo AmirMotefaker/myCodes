c4ecdcc1319842daf78f41ee7d2fec29851c3158
Amir Motefaker <amotef@gmail.com>
2022-08-19 09:05:03 +0430

    Create GUI_03_getting_input_from_user.py

---
+++ GUI/GUI_03_getting_input_from_user.py
@@ -0,0 +1,141 @@
+# Code by @AmirMotefaker
+
+# GUI - Getting input from the user - Entry
+
+# # Solution 1 - Entry
+import tkinter as tk
+
+window = tk.Tk()
+
+text = tk.Label(
+    master=window,
+    text='Hello World!',
+)
+
+# Entry
+user_input = tk.Entry(window)
+
+text.pack()
+user_input.pack()
+
+window.mainloop()
+
+
+
+# # Solution 2 - connect Entry and Label
+import tkinter as tk
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+text.pack()
+user_input.pack()
+
+window.mainloop()
+
+
+
+# # Solution 3 - .pack(side=tk.LEFT)
+import tkinter as tk
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+user_input.pack(side=tk.LEFT)
+text.pack(side=tk.LEFT)
+
+
+window.mainloop()
+
+
+
+# # Solution 4 - .pack()
+import tkinter as tk
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+name_text = tk.Label(
+    master=window,
+    text='First Name: ',
+)
+
+name_text.pack(side=tk.LEFT)
+user_input.pack(side=tk.LEFT)
+text.pack(side=tk.LEFT)
+
+
+window.mainloop()
+
+
+
+# Solution 5 - from tkinter import LEFT, BOTTOM
+import tkinter as tk
+from tkinter import LEFT, BOTTOM
+
+window = tk.Tk()
+
+text_var = tk.StringVar()
+
+# Label
+text = tk.Label(
+    master=window,
+    textvariable=text_var,
+)
+
+# Entry
+user_input = tk.Entry(
+    window,
+    textvariable=text_var,
+)
+
+name_text = tk.Label(
+    master=window,
+    text='First Name: ',
+)
+
+name_text.pack(side=LEFT)
+user_input.pack(side=LEFT)
+text.pack(side=LEFT)
+
+
+window.mainloop()
