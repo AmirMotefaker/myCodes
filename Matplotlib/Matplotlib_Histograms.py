00f17d43185cca7b2d7c34f16a7dfb1d6726d999
Amir Motefaker <amotef@gmail.com>
2022-08-20 16:41:10 +0430

    Create Matplotlib_Histograms.py

---
+++ Matplotlib/Matplotlib_Histograms.py
@@ -0,0 +1,22 @@
+# Code by @AmirMotefaker
+
+# Matplotlib - Matplotlib Histograms
+
+
+# # Solution 1 - Create Histogram
+import numpy as np
+
+x = np.random.normal(170, 10, 250)
+
+print(x)
+
+
+
+# Solution 2 - A simple histogram
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.normal(170, 10, 250)
+
+plt.hist(x)
+plt.show() 
