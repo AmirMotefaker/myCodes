87be53360fe20b20ac55a2ec341fcd543c5acd6d
Amir Motefaker <amotef@gmail.com>
2022-08-20 16:43:00 +0430

    Create Matplotlib_Markers.py

---
+++ Matplotlib/Matplotlib_Markers.py
@@ -0,0 +1,103 @@
+# Code by @AmirMotefaker
+
+# Matplotlib - Markers 
+
+# # Solution 1 - Mark each point with a circle
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+
+
+# # Solution 2 - Mark each point with a star
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = '*')
+plt.show()
+
+
+
+# # Solution 3 - Mark each point with a circle
+# # This parameter is also called fmt, and is written with this syntax:
+# # marker|line|color
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, 'o:r')
+plt.show()
+
+
+
+# # Solution 4 - Set the size of the markers to 20
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20)
+plt.show()
+
+
+
+# # Solution 5 - Set the EDGE color to red
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r')
+plt.show()
+
+
+
+# # Solution 6 - Set the FACE color to red
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mfc = 'r')
+plt.show()
+
+
+
+# # Solution 7 - Use both the mec and mfc arguments to color of the entire marker
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.show()
+
+
+
+# # Solution 8 - Mark each point with a beautiful green color
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
+plt.show()
+
+
+
+# Solution 9 - Mark each point with the color named "hotpink"
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'hotpink', mfc = 'hotpink')
+plt.show()
+
