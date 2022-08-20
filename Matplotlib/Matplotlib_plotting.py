c581e6411ab0fecb6dfaf52a353e672654bf0725
Amir Motefaker <amotef@gmail.com>
2022-08-20 16:45:12 +0430

    Create Matplotlib_plotting.py

---
+++ Matplotlib/Matplotlib_plotting.py
@@ -0,0 +1,49 @@
+# Code by @AmirMotefaker
+
+# Matplotlib Plotting
+
+# # Solution 1 - Draw a line in a diagram from position to position
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+
+# # Solution 2 - Draw two points in the diagram
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+
+
+# # Solution 3 - Draw a line in a diagram from position to position
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+
+# Solution 4 - Plotting without x-points
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoints)
+plt.show()
+
