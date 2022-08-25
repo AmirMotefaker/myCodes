eedb91d1271270d25ed76a613a414129861d796c
Amir Motefaker <amotef@gmail.com>
2022-08-25 09:14:14 +0430

    Create string _slicing.py

---
+++ String/string _slicing.py
@@ -0,0 +1,14 @@
+# Code by @AmirMotefaker
+
+# Get a Substring of a String
+
+my_string = "I love python."
+
+# prints "love"
+print(my_string[2:6])
+
+# prints "love python."
+print(my_string[2:])
+
+# prints "I love python"
+print(my_string[:-1])
