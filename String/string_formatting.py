06b04c0e1fbebe933f89c6a97a277a3f6a163d8a
Amir Motefaker <amotef@gmail.com>
2022-08-25 09:20:52 +0430

    Update string_formatting.py

--- String/string_formatting.py
+++ String/string_formatting.py
@@ -4,8 +4,10 @@
 
 color = 'yellow'
 print("{name}'s {object} is {color}".format(color = "blue", object = "bag", name = "john"))
+# john's bag is blue
 
 print("My score is {score:10.3f}". format(score = 10 / 3))
+# My score is      3.333
 
 
 # f-strings
@@ -14,4 +16,5 @@ item = 'car'
 color = 'blue'
 
 print(f"{name}'s {item} is {color}.")
+# John's car is blue.
 
