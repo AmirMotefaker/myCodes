720f958ec3c18e2443577d42b70a88870ddbf2f9
Amir Motefaker <amotef@gmail.com>
2022-09-06 11:27:46 +0430

    Update find_ascii_value.py

--- find_ascii_value.py
+++ find_ascii_value.py
@@ -5,3 +5,13 @@
 
 c = str(input("Enter character: "))
 print("The ASCII value of '" + c + "' is", ord(c))
+
+# Output:
+# Enter character: @
+# The ASCII value of '@' is 64
+
+# Enter character: a
+# The ASCII value of 'a' is 97
+
+# Enter character: |  
+# The ASCII value of '|' is 124
