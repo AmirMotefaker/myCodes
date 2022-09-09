25edd3dd90be9306e7b5a1486d52caee7b3c0617
Amir Motefaker <amotef@gmail.com>
2022-09-09 19:26:56 +0430

    Update list_empty.py

--- list_empty.py
+++ list_empty.py
@@ -2,22 +2,22 @@
 
 #  Check If a List is Empty
 
-# # Solution 1 - Using Boolean operation
-# my_list = []
-# if not my_list:
-#     print("the list is empty")
+# Solution 1 - Using Boolean operation
+my_list = []
+if not my_list:
+    print("the list is empty")
 
-# # Output:
-# # the list is empty
+# Output:
+# the list is empty
 
 
-# # Solution 2 - Using len()
-# my_list = []
-# if not len(my_list):
-#     print("the list is empty")
+# Solution 2 - Using len()
+my_list = []
+if not len(my_list):
+    print("the list is empty")
 
-# # Output:
-# # the list is empty
+# Output:
+# the list is empty
 
 
 # Solution 3 - Comparing with []
