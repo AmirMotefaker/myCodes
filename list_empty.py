6440ee59df51acce119f3bc8db3d1dad2e180018
Amir Motefaker <amotef@gmail.com>
2022-09-09 19:26:32 +0430

    Update list_empty.py

--- list_empty.py
+++ list_empty.py
@@ -7,14 +7,23 @@
 # if not my_list:
 #     print("the list is empty")
 
+# # Output:
+# # the list is empty
 
-# Solution 2 - Using len()
+
+# # Solution 2 - Using len()
 # my_list = []
 # if not len(my_list):
 #     print("the list is empty")
+
+# # Output:
+# # the list is empty
 
 
 # Solution 3 - Comparing with []
 my_list = []
 if my_list == []:
     print("The list is empty")
+
+# Output:
+# The list is empty
