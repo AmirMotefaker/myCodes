dd196aa15d0a90a25a91bf9180f12aa2748f68bd
Amir Motefaker <amotef@gmail.com>
2022-09-10 14:40:50 +0430

    Update merge_two_dictionaries.py

--- merge_two_dictionaries.py
+++ merge_two_dictionaries.py
@@ -2,23 +2,29 @@
 
 # Merge Two Dictionaries
 
-# Solution 1 - Using the | Operator
+# # Solution 1 - Using the | Operator
 
-# dict_1 = {1: 'a', 2: 'b'}
-# dict_2 = {2: 'c', 4: 'd'}
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
 
-# print(dict_1 | dict_2)
+print(dict_1 | dict_2)
 
-
-# # Solution 2 - Using the ** Operator
-
-# dict_1 = {1: 'a', 2: 'b'}
-# dict_2 = {2: 'c', 4: 'd'}
-
-# print({**dict_1, **dict_2})
+# # Output:
+# # {1: 'a', 2: 'c', 4: 'd'}
 
 
-# Solution 3 - Using copy() and update()
+# # # Solution 2 - Using the ** Operator
+
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print({**dict_1, **dict_2})
+
+# # Output:
+# # {1: 'a', 2: 'c', 4: 'd'}
+
+
+# # Solution 3 - Using copy() and update()
 
 dict_1 = {1: 'a', 2: 'b'}
 dict_2 = {2: 'c', 4: 'd'}
@@ -27,3 +33,6 @@ dict_3 = dict_2.copy()
 dict_3.update(dict_1)
 
 print(dict_3)
+
+# Output:
+# {2: 'b', 4: 'd', 1: 'a'}
