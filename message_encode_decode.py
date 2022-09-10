c8376f2594a886f470e0e35a23e5ae3b7dab9555
Amir Motefaker <amotef@gmail.com>
2022-09-10 14:43:02 +0430

    Update message_encode_decode.py

--- message_encode_decode.py
+++ message_encode_decode.py
@@ -2,68 +2,76 @@
 
 # Message Encode - Decode
 
-# Solution 1 - basic
-# def encode(key, string):
-#     encoded_chars = []
-#     for i in range(len(string)):
-#         key_c = key[i % len(key)]
-#         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
-#         encoded_chars.append(encoded_c)
-#     encoded_string = ''.join(encoded_chars)
-#     return encoded_string
+# # Solution 1 - basic
+def encode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    return encoded_string
 
-# def decode(key, string):
-#     encoded_chars = []
-#     for i in range(len(string)):
-#         key_c = key[i % len(key)]
-#         encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
-#         encoded_chars.append(encoded_c)
-#     encoded_string = ''.join(encoded_chars)
-#     return encoded_string
-
-
-# e = encode('Amir', 'Motefaker')
-# d = decode('Amir', e)
-# print([e])
-# print([d])
+def decode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    return encoded_string
 
 
+e = encode('Amir', 'Motefaker')
+d = decode('Amir', e)
+print([e])
+print([d])
 
-# Solution 2 - advanced
-
-# import six, base64
-# # six: Provides simple utilities for wrapping over differences between Python 2 and Python 3.
-# # base64: This module provides functions for encoding binary data to printable ASCII characters and decoding such encodings back to binary data.
-
-# def encode(key, string):
-#     encoded_chars = []
-#     for i in range(len(string)):
-#         key_c = key[i % len(key)]
-#         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
-#         encoded_chars.append(encoded_c)
-#     encoded_string = ''.join(encoded_chars)
-#     encoded_string = encoded_string.encode('latin') if six.PY3 else encoded_string
-#     return base64.urlsafe_b64encode(encoded_string).rstrip(b'=')
-
-# def decode(key, string):
-#     string = base64.urlsafe_b64decode(string + b'===')
-#     string = string.decode('latin') if six.PY3 else string
-#     encoded_chars = []
-#     for i in range(len(string)):
-#         key_c = key[i % len(key)]
-#         encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
-#         encoded_chars.append(encoded_c)
-#     encoded_string = ''.join(encoded_chars)
-#     return encoded_string
-
-# e = encode('Amir', 'Motefaker')
-# d = decode('Amir', e)
-# print([e])
-# print([d])
+# # Output:
+# # ['\x8eÜÝ×§ÎÔ×³']
+# # ['Motefaker']
 
 
 
-# Solution 3 - advanced++
+# # Solution 2 - advanced
+
+import six, base64
+# six: Provides simple utilities for wrapping over differences between Python 2 and Python 3.
+# base64: This module provides functions for encoding binary data to printable ASCII characters and decoding such encodings back to binary data.
+
+def encode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    encoded_string = encoded_string.encode('latin') if six.PY3 else encoded_string
+    return base64.urlsafe_b64encode(encoded_string).rstrip(b'=')
+
+def decode(key, string):
+    string = base64.urlsafe_b64decode(string + b'===')
+    string = string.decode('latin') if six.PY3 else string
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    return encoded_string
+
+e = encode('Amir', 'Motefaker')
+d = decode('Amir', e)
+print([e])
+print([d])
+
+# # Output:
+# # [b'jtzd16fO1Nez']
+# # ['Motefaker']
+
+
+
+# # Solution 3 - advanced++
 
 def encode_decode(key, string, act='e'):
    
@@ -83,3 +91,7 @@ e = encode_decode('Amir', 'Motefaker')
 d = encode_decode('Amir', e,'d')
 print(e)
 print(d)
+
+# Output:
+# ÜÝ×§ÎÔ×³
+# Motefaker
