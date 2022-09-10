21aa8f33df1d28fdd13bb0cf58ef6749c8d4a718
Amir Motefaker <amotef@gmail.com>
2022-09-10 13:53:05 +0430

    Update list_randomly_select.py

--- list_randomly_select.py
+++ list_randomly_select.py
@@ -8,9 +8,15 @@ import random
 my_list = [1, 'a', 32, 'c', 'd', 31]
 print(random.choice(my_list))
 
+# Output:
+# c
+
 
 # Solution 2 - Using secrets module
 import secrets
 
 my_list = [1, 'a', 32, 'c', 'd', 31]
 print(secrets.choice(my_list))
+
+# Output:
+# d
