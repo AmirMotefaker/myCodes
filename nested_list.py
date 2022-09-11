e78741a6dbdca27b61f3ab8e90e2d1a9e1be0eb5
Amir Motefaker <amotef@gmail.com>
2022-09-11 13:03:36 +0430

    Update nested_list.py

--- nested_list.py
+++ nested_list.py
@@ -1,40 +1,40 @@
-# Code by @AmirMotefaker
+Code by AmirMotefaker
 
 # Flatten a Nested List
 
 # Solution 1 - Using List Comprehension
-# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-# flat_list = [num for sublist in my_list for num in sublist]
-# print(flat_list)
+flat_list = [num for sublist in my_list for num in sublist]
+print(flat_list)
 
 
 # Solution 2 - Using Nested for Loops (non pythonic way)
-# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-# flat_list = []
-# for sublist in my_list:
-#     for num in sublist:
-#         flat_list.append(num)
+flat_list = []
+for sublist in my_list:
+    for num in sublist:
+        flat_list.append(num)
 
-# print(flat_list)
+print(flat_list)
 
 
 # Solution 3 - Using itertools package
-# import itertools
+import itertools
 
-# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-# flat_list = list(itertools.chain(*my_list))
-# print(flat_list)
+flat_list = list(itertools.chain(*my_list))
+print(flat_list)
 
 
 
 # Solution 4 - Using sum()
-# my_list = [[1], [2, 3], [4, 5, 6, 7]]
+my_list = [[1], [2, 3], [4, 5, 6, 7]]
 
-# flat_list = sum(my_list, [])
-# print(flat_list)
+flat_list = sum(my_list, [])
+print(flat_list)
 
 
 
