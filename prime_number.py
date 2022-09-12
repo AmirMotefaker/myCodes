32ebf13b86dd711e278ff0633bf12e1e58c9ca57
Amir Motefaker <amotef@gmail.com>
2022-09-12 13:49:27 +0430

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,22 +1,25 @@
-# Code by amotef@gmail.com
+# Code by @AmirMotefaker
 
-# Prime Number
-
-# Solution 1 
-# n=int(input("Enter a number: "))
-# if n > 1:
-#     for i in range(2, n//2):
-#         if(n%i) == 0:
-#             print(n,"is not a prime number")
-#             break
-#     else:
-#         print(n,"is a prime number")
-# else:
-#     print(n,"is neither prime nor composite")
+# # Solution 1 
+n=int(input("Enter a number: "))
+if n > 1:
+    for i in range(2, n//2):
+        if(n%i) == 0:
+            print(n,"is not a prime number")
+            break
+    else:
+        print(n,"is a prime number")
+else:
+    print(n,"is neither prime nor composite")
 
 
+# # Output:
+# # Enter a number: 127
+# # 127 is a prime number
 
-# Solution 2 - CodingYar
+
+
+# # Solution 2 - CodingYar
 user_number = int(input('Enter number: '))
 
 is_prime = True
@@ -57,29 +60,33 @@ else:
 
 
 # Solution Final
-# import time
-# start_time = time.time()   #Time at the start of program execution
+import time
+start_time = time.time()   #Time at the start of program execution
 
-# def is_prime(n):
-#     avval = True
-#     for i in range(2, int(n**0.5)+1):
-#         if n % i == 0 :
-#             avval = False
-#             break
-#     return avval
+def is_prime(n):
+    avval = True
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0 :
+            avval = False
+            break
+    return avval
 
-# prime_count=0
-# last_prime_number=0
+prime_count=0
+last_prime_number=0
 
-# for i in range(1,10001):
-#     if is_prime(i):
-#         last_prime_number = i
-#         prime_count = prime_count + 1
+for i in range(1,10001):
+    if is_prime(i):
+        last_prime_number = i
+        prime_count = prime_count + 1
 
-# print("")
-# print ("we had", prime_count, "prime numbers")
-# print("last prime number was", last_prime_number)
+print("")
+print ("we had", prime_count, "prime numbers")
+print("last prime number was", last_prime_number)
 
-# end_time = time.time()   #Time at the end of execution
-# print ("Time of program execution:", (end_time - start_time))   # Time of program execution
+end_time = time.time()   #Time at the end of execution
+print ("Time of program execution:", (end_time - start_time))   # Time of program execution
 
+# Output:
+# we had 1230 prime numbers
+# last prime number was 9973
+# Time of program execution: 0.06999874114990234
