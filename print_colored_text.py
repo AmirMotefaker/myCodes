9b4e9886f63d4610bd3110c81a0961e3f3c8ea23
Amir Motefaker <amotef@gmail.com>
2022-09-12 13:53:29 +0430

    Update print_colored_text.py

--- print_colored_text.py
+++ print_colored_text.py
@@ -1,13 +1,18 @@
-# Code by AmirMotefaker
+# Code by @AmirMotefaker
 
 # Print Colored Text to the Terminal
 
-# Solution 1 - Using ANSI escape sequences
-print('\x1b[38;2;5;86;243m' + 'Programiz' + '\x1b[0m')
+# # Solution 1 - Using ANSI escape sequences
+# print('\x1b[38;2;5;86;243m' + 'Programiz' + '\x1b[0m')
 
+# # Output:
+# # Programiz
 
 
 # Solution 2 - Using python module termcolor
 from termcolor import colored
 
 print(colored('Programiz', 'blue'))
+# Code by AmirMotefaker
+
+# Print Colored Text to the Terminal
