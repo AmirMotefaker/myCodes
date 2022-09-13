03dbf13f0474074ee76bcf2116e71cbc4bd5a61b
Amir Motefaker <amotef@gmail.com>
2022-09-13 14:43:20 +0430

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,4 +1,4 @@
-# Code by AmirMotefaker
+# Code by @AmirMotefaker
 
 # Solve Quadratic Equation
 
@@ -23,3 +23,6 @@ sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
 print('The solution are {0} and {1}'.format(sol1,sol2))
+
+# Output:
+# The solution are (-3+0j) and (-2+0j)
