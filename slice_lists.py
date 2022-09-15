3866f56f1aaf48fa99276d7d8f356d0320a43920
Amir Motefaker <amotef@gmail.com>
2022-09-15 13:35:46 +0430

    Update slice_lists.py

--- slice_lists.py
+++ slice_lists.py
@@ -3,28 +3,28 @@
 # Slice Lists
 
 # Solution 1 - Get all the Items
-# my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5]
 
-# print(my_list[:])
+print(my_list[:])
 
 
 # Solution 2 - Get all the Items After a Specific Position
-# my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5]
 
-# print(my_list[2:])
+print(my_list[2:])
 
 
 
 # Solution 3 - Get all the Items Before a Specific Position 2
-# my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5]
 
-# print(my_list[:2])
+print(my_list[:2])
 
 
 # Solution 4 - Get all the Items from One Position to Another Position
-# my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5]
 
-# print(my_list[2:4])
+print(my_list[2:4])
 
 
 # Solution 5 - Get the Items at Specified Intervals
