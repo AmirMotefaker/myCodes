cc37ac66ac7951ce5469225bb5e4c74b872a9c09
Amir Motefaker <amotef@gmail.com>
2022-09-16 10:04:02 +0430

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,26 +3,24 @@
 # Convert Text to Speech
 
 # Solution 1
-
-# from gtts import gTTS
-# from playsound import playsound
+from gtts import gTTS
+from playsound import playsound
 # gTTS (Google Text-to-Speech), a Python library and CLI tool to interface with Google Translate's text-to-speech API.
 # playsound: Pure Python, cross platform, single function module with no dependencies for playing sounds.
 
 # in english
-# tts = gTTS('hello world')
-# tts.save('hello.mp3')
-# playsound("hello.mp3")
+tts = gTTS('hello world')
+tts.save('hello.mp3')
+playsound("hello.mp3")
 
 # in spanish
-# tts = gTTS('Hola Mundo', lang='es')
-# tts.save('hola.mp3')
-# playsound("hola.mp3")
+tts = gTTS('Hola Mundo', lang='es')
+tts.save('hola.mp3')
+playsound("hola.mp3")
 
 
 
 # Solution 2
-
 from gtts import gTTS
 import os   # os: play the converted audio
   
