5349ee784a0b820e39da9e7c18161843513f7f8b
Amir Motefaker <amotef@gmail.com>
2022-09-16 10:05:07 +0430

    Update unlimited_number_inputs_function.py

--- unlimited_number_inputs_function.py
+++ unlimited_number_inputs_function.py
@@ -2,14 +2,14 @@
 
 # Get an unlimited number of inputs in the function
 
-# def sum_numbers(*args):
-#     result_sum = 0
-#     for n in args:
-#         result_sum += n
+def sum_numbers(*args):
+    result_sum = 0
+    for n in args:
+        result_sum += n
 
-#     return result_sum
+    return result_sum
 
-# print(sum_numbers(1,2,3,4,5,9))
+print(sum_numbers(1,2,3,4,5,9))
 
 # Output:
 # 24
