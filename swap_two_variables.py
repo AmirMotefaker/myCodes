41b49e208b22bd4fa9dc366fec5c7e0ec965cc83
Amir Motefaker <amotef@gmail.com>
2022-09-16 10:00:21 +0430

    Update swap_two_variables.py

--- swap_two_variables.py
+++ swap_two_variables.py
@@ -2,21 +2,27 @@
 
 # Swap Two Variables
 
-
-# x = 5
-# y = 10
+# Solution 1 - Using Temporary Variable
+x = 5
+y = 10
 
 # To take inputs from the user
-#x = input('Enter value of x: ')
-#y = input('Enter value of y: ')
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
 
 # create a temporary variable and swap the values
-# temp = x
-# x = y
-# y = temp
+temp = x
+x = y
+y = temp
 
-# print('The value of x after swapping: {}'.format(x))
-# print('The value of y after swapping: {}'.format(y))
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
+# Output:
+# Enter value of x: 5
+# Enter value of y: 10
+# The value of x after swapping: 10
+# The value of y after swapping: 5
 
 
 
@@ -29,4 +35,6 @@ x, y = y, x
 print("x =", x)
 print("y =", y)
 
-# 
+# Output
+# x = 10
+# y = 5
