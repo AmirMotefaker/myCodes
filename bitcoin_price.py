1f637102b785f2eebf1d63cd73d9cadcb3c4d82e
Amir Motefaker <amotef@gmail.com>
2022-10-03 08:37:16 +0330

    Update bitcoin_price.py

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,4 +1,4 @@
-# Code by @AmirMotefaker
+# Code by AmirMotefaker
 
 # Bitcoin online prices
 
@@ -6,33 +6,33 @@
 # run with VPN
 
 # Code#1
-# import requests
+import requests
 
-# response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
-# print (response.text)
+response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
+print (response.text)
 
 
-# # Code#2
-# import requests
+# Code#2
+import requests
 
-# response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
-# print ('at this moment, Bitcoin is %i dollars' % float(response.json()['data']['amount']))
+response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
+print ('at this moment, Bitcoin is %i dollars' % float(response.json()['data']['amount']))
 
 
 
 # # Code#3
-# import requests
+import requests
 
-# def inform_amir():
-#     print("hi there, price is good.")
+def inform_amir():
+    print("hi there, price is good.")
 
-# my_good_price = 18000
-# response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
-# price = float(response.json()['data']['amount'])
+my_good_price = 18000
+response = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
+price = float(response.json()['data']['amount'])
 
-# if price < my_good_price:
-#     inform_amir()
-# print (response.text)
+if price < my_good_price:
+    inform_amir()
+print (response.text)
 
 
 # # Code 4
