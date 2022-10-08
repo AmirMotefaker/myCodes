1c95f5e658df25b96d54b2b78643f5f42a6d62b1
Amir Motefaker <amotef@gmail.com>
2022-10-08 14:31:06 +0330

    Update factorial_recursion.py

--- factorial_recursion.py
+++ factorial_recursion.py
@@ -3,21 +3,21 @@
 # Find Factorial of Number Using Recursion
 
 # # Solution 1 
-# def factorial_recursion(n):
-#    if n == 1:
-#        return n
-#    else:
-#        return n*factorial_recursion(n-1)
+def factorial_recursion(n):
+   if n == 1:
+       return n
+   else:
+       return n*factorial_recursion(n-1)
 
-# num = int(input("Enter number: "))
+num = int(input("Enter number: "))
 
-# # check if the number is negative
-# if num < 0:
-#    print("Sorry, factorial does not exist for negative numbers")
-# elif num == 0:
-#    print("The factorial of 0 is 1")
-# else:
-#    print("The factorial of", num, "is", factorial_recursion(num))
+# check if the number is negative
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print("The factorial of", num, "is", factorial_recursion(num))
 
 # # Output:
 # # Enter number: 8
