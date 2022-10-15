874613353993e7231d81d5d8a07763a4d1e3ebd2
Amir Motefaker <amotef@gmail.com>
2022-10-15 10:58:58 +0330

    Update mad_libs_game.py

--- mad_libs_game.py
+++ mad_libs_game.py
@@ -1,4 +1,4 @@
-# Code by AmirMotefaker
+# Code by @AmirMotefaker
 
 # Mad Libs Game
 
@@ -13,17 +13,17 @@
 
 # Solution 1 - simple
 
-# food = input("Enter a type of food: ")
-# girl = input("Enter a girl's name: ")
-# adj1 = input("Enter an adjective: ")
-# bird = input("Enter a noun: ")
-# verb1 = input("Enter a verb in the past tense: ")
-# verb2 = input("Enter another verb in the past tense: ")
-# verb3 = input("Enter a third verb in the past tense: ")
-# noun1 = input("Enter a noun: ")
-# story = "It was " + food + " day at school, and " + girl + " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + girl + " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and " + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped! Luckily, " + girl + "'s friends were willing to share their " + food + " with her."
+food = input("Enter a type of food: ")
+girl = input("Enter a girl's name: ")
+adj1 = input("Enter an adjective: ")
+bird = input("Enter a noun: ")
+verb1 = input("Enter a verb in the past tense: ")
+verb2 = input("Enter another verb in the past tense: ")
+verb3 = input("Enter a third verb in the past tense: ")
+noun1 = input("Enter a noun: ")
+story = "It was " + food + " day at school, and " + girl + " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + girl + " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and " + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped! Luckily, " + girl + "'s friends were willing to share their " + food + " with her."
  
-# print(story)
+print(story)
 
 
 # Solution 2 - advanced(Tkinter)
